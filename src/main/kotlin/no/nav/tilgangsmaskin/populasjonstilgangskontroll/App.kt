fc702315d447c9b40451db4a84e4d7d62d3d53bb
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-01-24 09:02:18 +0100

    client reg

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/App.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/App.kt
@@ -1,20 +1,25 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll
 
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.AbstractRestClientAdapter
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Cluster.Companion.profiler
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory.getLogger
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    exclude = [ErrorMvcAutoConfiguration::class],
-)
+@SpringBootApplication
 @ConfigurationPropertiesScan
 
 class App
 
+@Value("\${azure.app.client.jwk}")
+lateinit var azureAppClientJwk: String
 fun main(args: Array<String>) {
     runApplication<App>(*args) {
+        getLogger(App::class.java).info("Azure app client jwk: $azureAppClientJwk")
         setAdditionalProfiles(*profiler)
     }
 }

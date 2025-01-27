a2a176813e0136a0e83a2c0318954ef0c4eed070
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-01-27 14:58:41 +0100

    test

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
@@ -3,10 +3,8 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.OAuth2ClientRequestInterceptor
-import org.springframework.boot.web.client.RestClientCustomizer
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 @Configuration
 class FellesBeanConfig {
 

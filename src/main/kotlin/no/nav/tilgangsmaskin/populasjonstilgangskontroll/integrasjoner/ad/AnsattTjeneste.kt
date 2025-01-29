ab37d17e8e94ce4ccc363df4429f486595f86884
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-01-29 14:34:23 +0100

    error handling  + caching

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/ad/AnsattTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/ad/AnsattTjeneste.kt
@@ -1,9 +1,12 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.ad
 
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.ad.MSGraphConfig.Companion.GRAPH
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import java.util.*
 
 @Service
+@Cacheable(GRAPH)
 class AnsattTjeneste(private val adapter: MSRestClientAdapter) {
 
     fun ansattAzureId(ident: NavId) = adapter.hentUUIDforNavIdent(ident.verdi)

fdd01478232292a107aa91717eb5ac026d72fb57
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-01-29 22:43:18 +0100

    ping ms graph

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/ad/MSGraphClientBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/ad/MSGraphClientBeanConfig.kt
@@ -3,6 +3,7 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.ad
 import no.nav.security.token.support.client.spring.oauth2.OAuth2ClientRequestInterceptor
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.ad.MSGraphConfig.Companion.GRAPH
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.ad.MSGraphConfig.Companion.HEADER_CONSISTENCY_LEVEL
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.AbstractPingableHealthIndicator
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.AbstractRestClientAdapter.Companion.headerAddingRequestInterceptor
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.context.annotation.Bean
@@ -20,5 +21,9 @@ class MSGraphClientBeanConfig {
                 it.addFirst(oAuth2ClientRequestInterceptor)
                 it.add(headerAddingRequestInterceptor(HEADER_CONSISTENCY_LEVEL))
             }.build()
+
+    @Bean
+    fun graphHealthIndicator(a: MSRestClientAdapter) = object : AbstractPingableHealthIndicator(a) {}
+
 
 }

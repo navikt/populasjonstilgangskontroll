3d921c834a2881e7d2edf1267187c8eac499fb20
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-01-29 15:19:51 +0100

    Endret AdGruppe.kt

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/AdGruppe.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/AdGruppe.kt
@@ -1,5 +1,9 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import java.util.UUID
 
-data class AdGruppe(val id: UUID, val name: String)
+data class AdGruppe(
+    @JsonProperty("@odata.context") val context: String,
+                    val value: List<String>
+)

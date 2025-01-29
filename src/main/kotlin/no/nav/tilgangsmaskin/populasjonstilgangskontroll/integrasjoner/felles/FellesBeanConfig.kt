c4c761f85aa24bf5a056dd35a0119ae678cb1f46
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-01-29 19:57:44 +0100

    logging

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
 
+import com.github.benmanes.caffeine.cache.Caffeine
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.OAuth2ClientRequestInterceptor
@@ -8,6 +9,9 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class FellesBeanConfig {
 
+    @Bean
+    fun caffeine() =Caffeine.newBuilder().recordStats()
+    }
     @Bean
     fun oAuth2ClientRequestInterceptor(properties: ClientConfigurationProperties, service: OAuth2AccessTokenService) = OAuth2ClientRequestInterceptor(properties, service)
 }

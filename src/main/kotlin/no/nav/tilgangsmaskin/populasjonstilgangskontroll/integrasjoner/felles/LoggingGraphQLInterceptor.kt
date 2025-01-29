050a248fc2b0dc167ab960908313c8c2f6b98da7
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-01-29 10:58:58 +0100

    clean up

---
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/LoggingGraphQLInterceptor.kt
@@ -0,0 +1,15 @@
+package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
+
+import org.slf4j.LoggerFactory
+import org.springframework.graphql.client.ClientGraphQlRequest
+import org.springframework.graphql.client.SyncGraphQlClientInterceptor
+
+class LoggingGraphQLInterceptor : SyncGraphQlClientInterceptor {
+
+    private val log = LoggerFactory.getLogger(LoggingGraphQLInterceptor::class.java)
+
+    override fun intercept(req: ClientGraphQlRequest, chain: SyncGraphQlClientInterceptor.Chain) =
+        chain.next(req).also {
+            log.trace("Eksekverer {} med variabler {}", req.document, req.variables)
+        }
+}

27c6272fa58533709a7154fc36a5a9738e623304
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-03 15:26:57 +0100

    rename

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/AdGruppe.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/AdGruppe.kt
@@ -1,16 +1,18 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import java.util.UUID
 import java.net.URI
+import java.util.*
 
 data class EntraGrupper(
     @JsonProperty("@odata.context") val context: String,
     @JsonProperty("@odata.nextLink") val next: URI? = null,
     val value: List<EntraGruppeInfo> = emptyList()
 )
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class EntraGruppeInfo(
-    @JsonProperty("@odata.type") val odataType: String,
+  //  @JsonProperty("@odata.type") val odataType: String,
     val id: UUID,
     val displayName: String
 )

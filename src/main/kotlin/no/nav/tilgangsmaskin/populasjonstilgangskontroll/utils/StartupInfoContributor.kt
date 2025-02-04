d0dc714c689e866239bd1bcc605c3711d369ed9c
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-04 10:35:41 +0100

    infoContributor

---
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/StartupInfoContributor.kt
@@ -0,0 +1,24 @@
+package no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils
+
+import org.springframework.boot.actuate.info.InfoContributor
+import org.springframework.context.ApplicationContext
+import org.springframework.boot.actuate.info.Info.Builder
+import org.springframework.core.SpringVersion
+import org.springframework.boot.SpringBootVersion
+import org.springframework.stereotype.Component
+import java.time.LocalDateTime
+import java.time.ZoneId
+import java.time.format.DateTimeFormatter
+
+@Component
+class StartupInfoContributor(private val ctx : ApplicationContext) : InfoContributor {
+
+    override fun contribute(builder : Builder) {
+        builder.withDetail("extra-info", mapOf("Startup time" to ctx.startupDate.local(),
+            "Spring Boot version" to SpringBootVersion.getVersion(),
+            "Spring Framework version" to SpringVersion.getVersion()))
+    }
+
+    private fun Long.local(fmt : String = "yyyy-MM-dd HH:mm:ss") = LocalDateTime.ofInstant(Instant.ofEpochMilli(this),
+        ZoneId.of("Europe/Oslo")).format(DateTimeFormatter.ofPattern(fmt))
+}

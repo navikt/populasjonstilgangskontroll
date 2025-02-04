9bf95b3a0ab6a0b4b24521bd9c6ea20005dd152b
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-04 10:56:08 +0100

    infoContributor

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/Cluster.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/Cluster.kt
@@ -5,25 +5,11 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.DEV_GCP
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.GCP
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.LOCAL
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.NAIS_CLUSTER_NAME
-import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.NAIS_CLUSTER_TYPE
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.PROD
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.PROD_GCP
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.TEST
 import java.lang.System.*
 import kotlin.also
-import kotlin.collections.firstOrNull
-
-internal object Constants {
-    internal const val LOCAL = "local"
-    internal const val GCP = "gcp"
-    internal const val TEST = "test"
-    internal const val DEV = "dev"
-    internal const val PROD = "prod"
-    internal  const val DEV_GCP = "${DEV}-${GCP}"
-    internal const val PROD_GCP = "${PROD}-${GCP}"
-    internal const val NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME"
-    internal const val NAIS_CLUSTER_TYPE = "NAIS_CLUSTER_TYPE"
-}
 
 internal enum class Cluster(private val clusterName: String) {
     TEST_CLUSTER(TEST),
@@ -35,7 +21,6 @@ internal enum class Cluster(private val clusterName: String) {
 
         val current = (getenv(NAIS_CLUSTER_NAME) ?: LOCAL).let { e -> entries.first { it.clusterName == e } }
         val isProd = current == PROD_GCP_CLUSTER
-        val isDevOrLocal = !isProd
         val profiler =
             when (current) {
                 TEST_CLUSTER, LOCAL_CLUSTER ->
---
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/Constants.kt
@@ -0,0 +1,12 @@
+package no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils
+
+internal object Constants {
+    internal const val LOCAL = "local"
+    internal const val GCP = "gcp"
+    internal const val TEST = "test"
+    internal const val DEV = "dev"
+    internal const val PROD = "prod"
+    internal  const val DEV_GCP = "${DEV}-${GCP}"
+    internal const val PROD_GCP = "${PROD}-${GCP}"
+    internal const val NAIS_CLUSTER_NAME = "NAIS_CLUSTER_NAME"
+}

dc8b09d35fd89316f22675e29c20ec61f38e7f14
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-04 10:30:11 +0100

    ikke vis details

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/AbstractRestConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/AbstractRestConfig.kt
@@ -1,13 +1,11 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
 
-import org.slf4j.LoggerFactory
 import org.springframework.web.util.DefaultUriBuilderFactory
 import java.net.URI
 
 abstract class AbstractRestConfig(val baseUri: URI, private val pingPath: String, val name: String = baseUri.host, val isEnabled: Boolean)  {
 
     protected fun builder() = DefaultUriBuilderFactory("$baseUri").builder()
-    protected val log = LoggerFactory.getLogger(javaClass)
 
     val pingEndpoint =   builder().path(pingPath).build()
     override fun toString() = "name=$name, pingPath=$pingPath,enabled=$isEnabled,baseUri=$baseUri"

ec42d447be55000b9f30abc037d6c0fbffcbcc63
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-02-05 16:49:12 +0100

    testing

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/Saksbehandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/Saksbehandler.kt
@@ -6,5 +6,5 @@ import java.util.UUID
 class Saksbehandler(val  attributter: SaksbehandlerAttributter, val gruoper: List<EntraGrupperBolk.EntraGruppe>) {
     data class SaksbehandlerAttributter(val id: UUID, val navId: NavId, val fornavn: String, val etternavn: String, val enhetsNummer: Enhetsnummer)
 
-    fun kanBehandle(gruppe: FortroligGruppe) =  gruoper.any { it.gruppeNavn == gruppe.gruppeNavn }
+    fun kanBehandle(gruppe: FortroligGruppe) =  gruoper.any { it.displayName == gruppe.gruppeNavn }
 }

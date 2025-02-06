307dfe93f055bd2e762bc4e4b73d33c9fd4a8cde
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-06 10:18:27 +0100

    info

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/entra/EntraTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/entra/EntraTjeneste.kt
@@ -3,7 +3,6 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.Saksbehandler
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra.EntraConfig.Companion.GRAPH
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import java.util.*
@@ -16,9 +15,7 @@ class EntraTjeneste(private val adapter: EntraClientAdapter) {
     fun saksbehandler(ident: NavId) : Saksbehandler {
         val attributter = adapter.attributterForIdent(ident.verdi)
         val grupper = adapter.grupperForUUID(attributter.id)
-        return Saksbehandler(attributter, grupper).also {
-            log.info("Saksbehandler: $it")
-        }
+        return Saksbehandler(attributter, grupper)
     }
 
     fun ansattAzureId(ident: NavId) = adapter.attributterForIdent(ident.verdi)

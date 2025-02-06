8c284d62efcfe086eae0aa1b6b75cb5791d3b3ae
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-06 10:29:59 +0100

    info

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/StartupInfoContributor.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/StartupInfoContributor.kt
@@ -17,7 +17,7 @@ class StartupInfoContributor(private val ctx : ConfigurableApplicationContext) :
 
     override fun contribute(builder : Builder) {
         builder.withDetail("extra-info", mapOf("Startup time" to ctx.startupDate.local(),
-            "Client ID" to ctx to ctx.environment.getProperty("azure.app.client.id"),
+            "Client ID" to ctx.environment.getProperty("azure.app.client.id"),
             "Name" to ctx.environment.getProperty("spring.application.name"),
             "Spring Boot version" to SpringBootVersion.getVersion(),
             "Spring Framework version" to SpringVersion.getVersion()))

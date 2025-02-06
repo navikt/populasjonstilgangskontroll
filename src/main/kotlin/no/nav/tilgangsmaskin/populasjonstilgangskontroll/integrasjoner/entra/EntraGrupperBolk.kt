25c5c41ebd11c86214a35a98483974fbef56b9b5
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-02-06 09:48:56 +0100

    displayName er obligatorisk

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/entra/EntraGrupperBolk.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/entra/EntraGrupperBolk.kt
@@ -5,5 +5,5 @@ data class EntraGrupperBolk(
     @com.fasterxml.jackson.annotation.JsonProperty("@odata.nextLink") val next: java.net.URI? = null,
     val value: List<EntraGruppe> = emptyList())   {
     @com.fasterxml.jackson.annotation.JsonIgnoreProperties(ignoreUnknown = true)
-    data class EntraGruppe(val id: java.util.UUID, val displayName: String?)
+    data class EntraGruppe(val id: java.util.UUID, val displayName: String)
 }

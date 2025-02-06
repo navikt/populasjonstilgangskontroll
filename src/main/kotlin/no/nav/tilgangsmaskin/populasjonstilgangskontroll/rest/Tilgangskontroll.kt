2a1785655826278f2fdf3baa6c7c5cee89d80042
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-02-06 13:23:16 +0100

    protected tjeneste og swagger-conf

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/rest/Tilgangskontroll.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/rest/Tilgangskontroll.kt
@@ -1,11 +1,15 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.rest
 
 import no.nav.security.token.support.spring.ProtectedRestController
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra.EntraTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.service.TilgangsTjeneste
+import org.springframework.web.bind.annotation.GetMapping
 
 @ProtectedRestController(value = ["/api/v1"], issuer = "azuread", claimMap = [])
-class Tilgangskontroll(val service : TilgangsTjeneste) {
+class Tilgangskontroll(val service : TilgangsTjeneste, val ansatt: EntraTjeneste,) {
 
-
+    @GetMapping("ansatt")
+    fun hentAnsatt(ident: NavId) = ansatt.ansattAzureId(ident)
 }
 

2dd0f67678379e4328b4f5f1bcf408517a92658b
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-02-06 14:36:57 +0100

    lagt til SecirutyScheme i controller

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/rest/Tilgangskontroll.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/rest/Tilgangskontroll.kt
@@ -1,11 +1,19 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.rest
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.security.SecurityScheme
 import no.nav.security.token.support.spring.ProtectedRestController
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra.EntraTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.service.TilgangsTjeneste
 import org.springframework.web.bind.annotation.GetMapping
 
+@SecurityScheme(
+    bearerFormat = "JWT",
+    name = "bearer-key",
+    scheme = "bearer",
+    type = SecuritySchemeType.HTTP,
+)
 @ProtectedRestController(value = ["/api/v1"], issuer = "azuread", claimMap = [])
 class Tilgangskontroll(val service : TilgangsTjeneste, val ansatt: EntraTjeneste,) {
 

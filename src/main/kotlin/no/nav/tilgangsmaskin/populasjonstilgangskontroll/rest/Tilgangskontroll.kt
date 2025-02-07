d872a39f5734e265e19cccfa3bd1b19f4b12fcf0
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-02-07 15:19:09 +0100

    bruke TokenUtil

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/rest/Tilgangskontroll.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/rest/Tilgangskontroll.kt
@@ -3,12 +3,16 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.rest
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.security.SecurityScheme
+import no.nav.security.token.support.core.context.TokenValidationContextHolder
+import no.nav.security.token.support.core.jwt.JwtTokenClaims
 import no.nav.security.token.support.spring.ProtectedRestController
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.Fødselsnummer
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra.EntraTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.service.TilgangTjeneste
-import org.springframework.context.annotation.Configuration
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.GetMapping
+import java.util.*
 
 @SecurityScheme(
     bearerFormat = "JWT",
@@ -17,10 +21,44 @@ import org.springframework.web.bind.annotation.GetMapping
     type = SecuritySchemeType.HTTP,
 )
 @ProtectedRestController(value = ["/api/v1"], issuer = "azuread", claimMap = [])
-class Tilgangskontroll(val service : TilgangTjeneste, val ansatt: EntraTjeneste,) {
+class Tilgangskontroll(val service : TilgangTjeneste, val ansatt: EntraTjeneste, private val tokenUtil: TokenUtil) {
 
     @GetMapping("ansatt")
     @SecurityRequirement(name = "bearerAuth")
     fun hentAnsatt(ident: NavId) = ansatt.ansattAzureId(ident)
+
+    @GetMapping("tilgang")
+    @SecurityRequirement(name="bearerAuth")
+    fun validerTilgang(kandidatIdent: Fødselsnummer) :Unit {
+        val saksbehandlerNavIdent = tokenUtil.getNavIdentFromToken()
+        return  service.sjekkTilgang(saksbehandlerNavIdent, kandidatIdent);
+
+    }
+
+
+}
+@Component
+class TokenUtil{
+    val AAD_ISSUER: String = "aad"
+    private var contextHolder: TokenValidationContextHolder? = null
+
+    fun TokenUtil(contextHolder: TokenValidationContextHolder?) {
+        this.contextHolder = contextHolder
+    }
+    fun getSubject(): String {
+        return Optional.of(claimSet(AAD_ISSUER)).map { cs -> cs.getStringClaim("pid") }.orElse(null)
+    }
+    fun getIdentFromToken(): UUID {
+        return Optional.of(claimSet(AAD_ISSUER)).map { cs -> UUID.fromString(cs.getStringClaim("oid")) }.orElse(null)
+    }
+    fun getNavIdentFromToken(): NavId {
+        return  Optional.of(claimSet(AAD_ISSUER)).map { cs -> cs.getStringClaim("NAVident") as NavId }.orElse(null)
+    }
+
+    private fun claimSet(issuer: String): JwtTokenClaims {
+        return Optional.ofNullable(contextHolder!!.getTokenValidationContext())
+            .map { c -> c.getClaims(issuer) }
+            .orElse(null)
+    }
 }
 

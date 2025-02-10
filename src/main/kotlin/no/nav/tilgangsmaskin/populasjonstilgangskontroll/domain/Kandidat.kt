9a1e23839db6e43f4bf0e9981fb3e3584adcddf1
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-10 15:56:26 +0100

    use infix

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/Kandidat.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/Kandidat.kt
@@ -4,6 +4,6 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.GlobalGruppe
 
 class Kandidat(val ident: Fødselsnummer, vararg private val beskyttelse: GlobalGruppe) {
 
-    fun  kreverGruppe(gruppe: GlobalGruppe) = beskyttelse.contains(gruppe)
+    fun  kreverGruppe(gruppe: GlobalGruppe) = gruppe in beskyttelse
     override fun toString() = "${javaClass.simpleName} [ident=$ident, beskyttelse=$beskyttelse]"
 }

08872b42fa630667a0f874d721ab089c8f391080
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-10 10:28:02 +0100

    feilhåndtering

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/errors/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/errors/ExceptionHandler.kt
@@ -1,10 +1,12 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.errors
 
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelException
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.BAD_REQUEST
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON
 import org.springframework.http.ResponseEntity.status
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -17,11 +19,11 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 class ExceptionHandler : ResponseEntityExceptionHandler() {
     private val log = getLogger(javaClass)
 
-    @ExceptionHandler(Exception::class)
-    fun catchAll(e: Exception, req: NativeWebRequest) = createProblem(e, req, BAD_REQUEST)
-    private fun createProblem(e: Exception, req: NativeWebRequest, status: HttpStatus) =
+    @ExceptionHandler(RegelException::class)
+    fun catchAll(e: RegelException, req: NativeWebRequest) = createProblem(e, req, e.statusCode)
+    private fun createProblem(e: Exception, req: NativeWebRequest, status: HttpStatusCode) =
         status(status)
             .headers(HttpHeaders().apply { contentType = APPLICATION_PROBLEM_JSON })
             .body(createProblemDetail(e, status, e.message ?: e.javaClass.simpleName, null, null, req).apply {
-            }.also { log.error("OOPS $req $it ${status.reasonPhrase}: ${e.message}", e) })
+            }.also { log.error("OOPS $req $it ${status.value()}: ${e.message}", e) })
 }
--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/RegelException.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/RegelException.kt
@@ -5,9 +5,10 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.errors.IrrecoverableException
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.Regel.RegelForklaring
 import org.springframework.http.HttpStatus.FORBIDDEN
+import java.lang.String.format
 
 class RegelException(kandidatId: Fødselsnummer, saksbehandlerId: NavId, forklaring: RegelForklaring) : IrrecoverableException(
-    FORBIDDEN, String.format(forklaring.feilmelding,saksbehandlerId.verdi, kandidatId),mapOf(
+    FORBIDDEN, format(forklaring.feilmelding,saksbehandlerId.verdi, kandidatId.verdi),mapOf(
         "kandidat" to kandidatId.verdi,
         "saksbehandler" to saksbehandlerId.verdi,
         "kode" to forklaring.kode,

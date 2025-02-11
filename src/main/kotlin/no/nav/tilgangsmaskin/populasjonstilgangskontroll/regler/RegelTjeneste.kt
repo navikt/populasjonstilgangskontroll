621ab2b6ea593230c7b32922a34032ceffd0c26f
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-11 10:11:50 +0100

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/RegelTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/RegelTjeneste.kt
@@ -2,7 +2,26 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler
 
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.Fødselsnummer
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
+import org.springframework.stereotype.Service
 
-interface RegelTjeneste {
-   fun sjekkTilgang(sakebehandler: NavId, kandidat: Fødselsnummer)
+@Service
+class DefaultRegelTjeneste(private val regelMotor: RegelMotor, private val kandidatTjeneste: KandidatTjeneste, private val saksbehandlerTjeneste: SaksbehandlerTjeneste) : RegelTjeneste {
+
+    override fun sjekkTilgang(saksbehandlerId: NavId, kandidatId: Fødselsnummer) =
+        regelMotor.vurderTilgang(kandidatTjeneste.kandidat(kandidatId), saksbehandlerTjeneste.saksbehandler(saksbehandlerId))
+        /**
+        Prioritert utslagskriterier:
+        Harde regler:
+        Kode 6: Strengt fortrolig adresse
+        Kode 19: trengt fortrolig adresse utland
+        Kode 7 : Fortrolig adresse
+        Egen ansatt: Skjerming
+        Familie: (mangler datasettene for dette)
+        Verge: (ikkje implementert og mangler datasettene for dette)
+        Oppslag på egen person :(mangler datasettene for dette) (hovuddel vil håndters via skjerming, men avskjermede ansatte dekkes ikkje av skjerming)
+        Overstyrbare regler:
+        Geogrfisk tilgang: (Mangler datasettene for dette)
+         **/
+
 }
+
--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/TokenAccessor.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/TokenAccessor.kt
@@ -12,7 +12,7 @@ class TokenUtil(private val contextHolder: TokenValidationContextHolder){
     val all get() = claimSet().allClaims
     val subject get()  = claimSet().getStringClaim("pid")
     val  identFromToken get()  = claimSet().let { UUID.fromString(it.getStringClaim("oid")) }
-    val navIdentFromToken get()  = claimSet().getStringClaim("NAVident")?.let { NavId(it) } ?: throw RuntimeException("NAVident claim not found in token")
+    val navIdent get()  = claimSet().getStringClaim("NAVident")?.let { NavId(it) } ?: throw RuntimeException("NAVident claim not found in token")
     private fun claimSet() = contextHolder.getTokenValidationContext().getClaims(AAD_ISSUER)
 
     companion object {

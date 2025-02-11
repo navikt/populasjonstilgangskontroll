e80e177166acc880c3f19e6c3f4857522c215770
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-11 13:15:19 +0100

    val

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/Kandidat.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/Kandidat.kt
@@ -3,7 +3,7 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.pdl.GTRespons
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.GlobalGruppe
 
-class Kandidat(val ident: Fødselsnummer, private val gt: GTRespons, private vararg val gruppeKrav: GlobalGruppe) {
+class Kandidat(val ident: Fødselsnummer, val gt: GTRespons, vararg val gruppeKrav: GlobalGruppe) {
 
     fun  kreverGruppe(gruppe: GlobalGruppe) = gruppe in gruppeKrav
     override fun toString() = "${javaClass.simpleName} [ident=$ident, gt=$gt,  gruppeKrav=${gruppeKrav.contentToString()}]"

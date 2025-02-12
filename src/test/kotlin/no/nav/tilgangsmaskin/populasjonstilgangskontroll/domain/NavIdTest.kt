ae47c70b6f1b6f60a74d399322cb019838459d3a
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-12 10:08:57 +0100

    test NAvId

---
+++ src/test/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/NavIdTest.kt
@@ -0,0 +1,32 @@
+package no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class NavIdTest {
+    @Test
+    @DisplayName("Ugyldig NavId OK")
+    fun validNavId() {
+        assertEquals("A123456", NavId("A123456").verdi)
+    }
+
+    @Test
+    @DisplayName("NavId med ygyldig lengde skal kaste IllegalArgumentException")
+    fun lengde() {
+        assertThrows<IllegalArgumentException> { NavId("A12345") }
+    }
+
+    @Test
+    @DisplayName("NavId uten stor bokstav først skal kaste IllegalArgumentException")
+    fun ikkeStprBokstav() {
+        assertThrows<IllegalArgumentException> { NavId("a123456") }
+    }
+
+    @Test
+    @DisplayName("NavId uten 6 tall etter første bokstav skal kaste IllegalArgumentException")
+    fun ikke6tall() {
+        assertThrows<IllegalArgumentException> { NavId("A12345a") }
+    }
+}

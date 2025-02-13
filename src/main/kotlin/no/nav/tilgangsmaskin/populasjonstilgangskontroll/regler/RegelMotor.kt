8fe8a78f9dbfc37a05affce866c4670d66695ad7
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-02-13 12:31:07 +0100

    Dokumentere flyt på GEO-regler

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/RegelMotor.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/RegelMotor.kt
@@ -30,5 +30,20 @@ Familie: (mangler datasettene for dette)
 Verge: (ikkje implementert og mangler datasettene for dette)
 Oppslag på egen person :(mangler datasettene for dette) (hovuddel vil håndters via skjerming, men avskjermede ansatte dekkes ikkje av skjerming)
 Overstyrbare regler:
-Geogrfisk tilgang: (Mangler datasettene for dette)
+Geogrfisk tilgang: (Flyt for gGEO-tilgang spesifisert under
  **/
+
+/** Flyt for GEO-sjekk
+ * (Kan overstyres av system)
+ *
+ * Ansatt har gruppe GE_GEO-NASJONAL
+ * Bruker har ikke registert GT --> Ansatte med GEO_Udefinert skal ha tilgang til personer uten GT/Udefnert GT
+ * Brukers GT stemmer med en av Ansattes liste over ENHET-GT _
+ * Brukers oppfølgingskontornr stemmer med en av Ansattes liste over ENHET-KONTORNR _(datasett må komme fra POAO (sansynligbvis)) team OBO
+ *Bruker har GT = landskode --> Sjekk mot ansattes gruper -GEO-Utland
+ *
+ * Avvik på tilganger som må dekkes:
+ * Bruker har kun kommunetilhørighet i GT, mens kommunene har bydelsoppdeling --> Diskusjon om ansatte i kommunene Oslo(0301, Bergen(4601), Trondheim(5001) og Stavanger(1103) skal ha tilgang til kommunenummeret i tilleggg til bydelen
+ *
+ *
+ */

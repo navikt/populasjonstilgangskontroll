6b6dd8e2a3a79c126975c4a11abe851681cad7bc
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-13 11:03:14 +0100

    test mapping av kombo egen ansatt og kode 6/7

--- src/test/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/pdl/PdlTilBrukerMapperTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/pdl/PdlTilBrukerMapperTest.kt
@@ -3,6 +3,7 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.pdl
 import com.neovisionaries.i18n.CountryCode.SE
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.Fødselsnummer
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.GeoTilknytning
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.GeoTilknytning.KommuneTilknytning
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.GeoTilknytning.UtenlandskTilknytning
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.pdl.PdlGeoTilknytning.GTKommune
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.pdl.PdlGeoTilknytning.GTLand
@@ -16,37 +17,63 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.pdl.PdlPe
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.pdl.PdlPerson.Bostedsadresse.VegAdresse.Koordinater
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.pdl.PdlPerson.Folkeregisteridentifikator
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.GlobalGruppe.*
-import org.junit.jupiter.api.Assertions.assertTrue
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
-import kotlin.test.assertEquals
 
 class PdlTilBrukerMapperTest {
 
-    private val fnr = Fødselsnummer("11111111111")
+    private val fnr = Fødselsnummer("08526835671")
 
     @Test
-    @DisplayName("Test at STRENGT_FORTROLIG_UTLAND fra Pdl krever medlemsskap i STRENGT_FORTROLIG_GRUPPEog at geotilknytning er UtenlandskTilknytning")
+    @DisplayName("Test at behandling av brukere med STRENGT_FORTROLIG_UTLAND  krever medlemsskap i STRENGT_FORTROLIG_GRUPPE fra ansatt og at geotilknytning er UtenlandskTilknytning")
     fun strengtFortroligUtland()   {
         with(PdlTilBrukerMapper.tilBruker(pdlPerson(fnr,STRENGT_FORTROLIG_UTLAND), geoUtland(), false)) {
-            assertEquals(ident, fnr)
-            assertEquals(gruppeKrav.single(), STRENGT_FORTROLIG_GRUPPE)
-            assertTrue(geoTilknytning is UtenlandskTilknytning)
+            assertThat(ident).isEqualTo(fnr)
+            assertThat(gruppeKrav).containsExactly(STRENGT_FORTROLIG_GRUPPE)
+            assertThat(geoTilknytning).isInstanceOf(UtenlandskTilknytning::class.java)
+        }
+    }
+    @Test
+    @DisplayName("Test at behandling av brukere med STRENGT_FORTROLIG vil kreve medlemsskap i STRENGT_FORTROLIG_GRUPPE for ansatt og at geotilknytning er KommuneTilknytning")
+    fun strengtFortroligKommune()   {
+        with(PdlTilBrukerMapper.tilBruker(pdlPerson(fnr,STRENGT_FORTROLIG), geoKommune(), false)) {
+            assertThat(ident).isEqualTo(fnr)
+            assertThat(gruppeKrav).containsExactly(STRENGT_FORTROLIG_GRUPPE)
+            assertThat(geoTilknytning).isInstanceOf(KommuneTilknytning::class.java)
+        }
+    }
+    @Test
+    @DisplayName("Test at behandling av brukere med EGEN_ANSATT vil kreve medlemsskap i EGEN_ANSATT_GRUPPE for ansatt")
+    fun egenAnsatt()   {
+        with(PdlTilBrukerMapper.tilBruker(pdlPerson(fnr), geoKommune(), true)) {
+            assertThat(gruppeKrav).containsExactly(EGEN_ANSATT_GRUPPE)
+        }
+    }
+    @Test
+    @DisplayName("Test at behandling av brukere med EGEN_ANSATT og STRENGT_FORTROLIG vil kreve medlemsskap i EGEN_ANSATT_GRUPPE og STRENGT_FORTROLIG_GRUPPE fir ansatt")
+    fun egenAnsattKode6()   {
+        with(PdlTilBrukerMapper.tilBruker(pdlPerson(fnr,STRENGT_FORTROLIG), geoKommune(), true)) {
+            assertThat(gruppeKrav).containsExactlyInAnyOrder(EGEN_ANSATT_GRUPPE,STRENGT_FORTROLIG_GRUPPE)
+        }
+    }
+    @Test
+    @DisplayName("Test at behandling av brukere med EGEN_ANSATT og FORTROLIG vil kreve medlemsskap i EGEN_ANSATT_GRUPPE og FORTROLIG_GRUPPE fir ansatt")
+    fun egenAnsattKode7()   {
+        with(PdlTilBrukerMapper.tilBruker(pdlPerson(fnr,FORTROLIG), geoKommune(), true)) {
+            assertThat(gruppeKrav).containsExactlyInAnyOrder(EGEN_ANSATT_GRUPPE,FORTROLIG_GRUPPE)
         }
     }
 
     private fun geoUtland() = PdlGeoTilknytning(UTLAND, gtLand = GTLand(SE.alpha3))
     private fun geoKommune() = PdlGeoTilknytning(KOMMUNE, gtKommune = GTKommune("1234"))
 
-    fun pdlPerson(fnr: Fødselsnummer,gradering: AdressebeskyttelseGradering) : PdlPerson {
-        val adressebeskyttelse = listOf(Adressebeskyttelse(gradering))
+    fun pdlPerson(fnr: Fødselsnummer,gradering: AdressebeskyttelseGradering? = null) : PdlPerson {
+        val adressebeskyttelse = gradering?.let{
+            listOf(Adressebeskyttelse(gradering))
+        }?: emptyList()
         val navn = listOf(PdlPerson.Navn("Ola", "Mellomnavn", "Nordmann"))
         return PdlPerson(adressebeskyttelse, navn, emptyList(), listOf(Folkeregisteridentifikator(fnr.verdi, "FNR")))
-    }
-    private fun pdlPerson1(fnr: Fødselsnummer,gradering: AdressebeskyttelseGradering) : PdlPerson {
-        return pdlPerson(fnr,gradering).copy(bostedsadresse = listOf(Bostedsadresse(LocalDate.of(2020, 1, 1), "c/o Navn",
-            VegAdresse(12345, "12", "A", "H0101", "Gateveien", "1234", "Tilleggsnavn", "1234", Koordinater(59.1234f, 10.1234f, null,1)),
-            "Gateveien 12A", UkjentBosted("Ukjent Kommune"))))
     }
 }

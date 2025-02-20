ee002af4ab4eeb4136385ffa44e90dc6396e2349
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-20 10:29:36 +0100

    prøv overstyr

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/Overstyring.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/Overstyring.kt
@@ -2,14 +2,17 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.overstyring
 
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType.IDENTITY
 import jakarta.persistence.Id
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 
 @Entity
+@EntityListeners(LoggingEntityListener::class, AuditingEntityListener::class)
 class Overstyring {
     @Column(name = "navid", length = 7)
     var navid: String? = null

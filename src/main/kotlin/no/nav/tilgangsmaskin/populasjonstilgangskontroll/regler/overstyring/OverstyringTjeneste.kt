cf75271487677cdd8ef88a57319791b9d306af2a
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-20 10:09:28 +0100

    prøv overstyr

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringTjeneste.kt
@@ -5,8 +5,23 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
 import org.springframework.stereotype.Component
 
 @Component
-class OverstyringTjeneste {
+class OverstyringTjeneste(private val adapter: JPAOverstyringAdapter) {
     fun harOverstyrtTilgang(id: NavId, fødselsnummer: Fødselsnummer) : Boolean {
         return false  // TODO
+    }
+
+    fun overstyr(ansattId: NavId, brukerId: Fødselsnummer) {
+         adapter.lagre(ansattId.verdi, brukerId.verdi)
+    }
+}
+
+@Component
+class JPAOverstyringAdapter(private val repository: OverstyringRepository)  {
+
+    fun lagre(ansattId: String, brukerId: String) {
+        repository.save(Overstyring().apply {
+            navid = ansattId
+            fnr = brukerId
+        })
     }
 }
--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/rest/DevTilgangController.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/rest/DevTilgangController.kt
@@ -7,13 +7,16 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.AnsattTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.BrukerTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelTjeneste
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.overstyring.OverstyringTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.DEV
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
 
 @UnprotectedRestController(value = ["/${DEV}"])
 @ConditionalOnNotProd
-class DevTilgangController(private val bruker : BrukerTjeneste, private val ansatt: AnsattTjeneste, private val regler: RegelTjeneste)
+class DevTilgangController(private val bruker : BrukerTjeneste, private val ansatt: AnsattTjeneste, private val regler: RegelTjeneste, private val overstyringTjeneste: OverstyringTjeneste)
 {
     @GetMapping("bruker")
     fun bruker(fnr: Fødselsnummer) = bruker.bruker(fnr)
@@ -26,5 +29,8 @@ class DevTilgangController(private val bruker : BrukerTjeneste, private val ansa
 
     @GetMapping("kjerneregler")
     fun kjerneregler(@RequestParam ansattId: NavId, @RequestParam brukerId: Fødselsnummer) = regler.kjerneregler(ansattId, brukerId)
+
+    @PostMapping("overstyr/{ansattId}/{brukerId}")
+    fun overstyr(@PathVariable ansattId: NavId, @PathVariable brukerId: Fødselsnummer) = overstyringTjeneste.overstyr(ansattId, brukerId)
 
 }

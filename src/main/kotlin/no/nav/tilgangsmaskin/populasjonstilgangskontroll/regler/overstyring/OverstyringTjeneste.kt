2f369e383eaa8f3817bd9148a851c337103ce8c7
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-20 10:20:52 +0100

    prøv overstyr

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringTjeneste.kt
@@ -2,26 +2,28 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.overstyring
 
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.Fødselsnummer
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.NavId
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.stereotype.Component
 
 @Component
 class OverstyringTjeneste(private val adapter: JPAOverstyringAdapter) {
+
+    private val log = getLogger(OverstyringTjeneste::class.java)
+
     fun harOverstyrtTilgang(id: NavId, fødselsnummer: Fødselsnummer) : Boolean {
         return false  // TODO
     }
 
-    fun overstyr(ansattId: NavId, brukerId: Fødselsnummer) {
-         adapter.lagre(ansattId.verdi, brukerId.verdi)
-    }
+    fun overstyr(ansattId: NavId, brukerId: Fødselsnummer) =
+         adapter.lagre(ansattId.verdi, brukerId.verdi).also { log.trace("Overstyring: {}", it) }
 }
 
 @Component
 class JPAOverstyringAdapter(private val repository: OverstyringRepository)  {
 
-    fun lagre(ansattId: String, brukerId: String) {
+    fun lagre(ansattId: String, brukerId: String) =
         repository.save(Overstyring().apply {
             navid = ansattId
             fnr = brukerId
-        })
-    }
+        }).id
 }

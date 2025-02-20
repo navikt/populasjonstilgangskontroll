4599c742c59eb6b1c6616d41727d93ffc14732c8
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-20 14:06:22 +0100

    removal listener

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
@@ -1,16 +1,24 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
 
 import com.github.benmanes.caffeine.cache.Caffeine
+import no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
 import no.nav.security.token.support.client.spring.oauth2.OAuth2ClientRequestInterceptor
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 @Configuration
 class FellesBeanConfig {
 
+    private val log = LoggerFactory.getLogger(FellesBeanConfig::class.java)
+
+
     @Bean
-    fun caffeine() = Caffeine.newBuilder().recordStats()
+    fun caffeine() = Caffeine.newBuilder()
+        .recordStats()
+        .removalListener { key: Any?, value: Any?, cause -> log.trace(CONFIDENTIAL,"Cache removal key={}, value={}, cause={}", key, value, cause)
+    }.build<Any, Any>()
     @Bean
     fun oAuth2ClientRequestInterceptor(properties: ClientConfigurationProperties, service: OAuth2AccessTokenService) = OAuth2ClientRequestInterceptor(properties, service)
 }

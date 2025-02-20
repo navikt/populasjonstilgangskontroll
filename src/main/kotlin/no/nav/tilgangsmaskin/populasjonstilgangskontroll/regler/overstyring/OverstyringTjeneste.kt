ef55cc7c0a6ee2db778d21560ea518fd039f102d
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-20 15:29:00 +0100

    log tid

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringTjeneste.kt
@@ -6,17 +6,19 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.AnsattTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.BrukerTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelException
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelMotor
-import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.overstyring.Overstyring.Companion.OVERSTYRING
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.ObjectUtil.format
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.ObjectUtil.mask
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.CachePut
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 import java.time.Instant
+import kotlin.compareTo
+import kotlin.rem
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.minutes
+import kotlin.time.toKotlinDuration
 
 @Component
 @Cacheable(OVERSTYRING)
@@ -26,7 +28,14 @@ class OverstyringTjeneste(private val ansatt: AnsattTjeneste, private val bruker
 
 
     fun erOverstyrt(id: NavId, brukerId: Fødselsnummer) =
-        nyesteOverstyring(id, brukerId)?.let { it.expires?.isAfter(Instant.now())  } == true
+        nyesteOverstyring(id, brukerId)?.let {
+            val isOverstyrt = it.expires?.isAfter(Instant.now()) == true
+            if (!isOverstyrt) {
+                val utgått = java.time.Duration.between(Instant.now(), it.expires).toKotlinDuration().format()
+                log.warn("Overstyring har gått ut på tid for $utgått siden for id=${id.verdi} and brukerId=${brukerId.mask()}")
+            }
+            isOverstyrt
+        } == true
 
     fun nyesteOverstyring(id: NavId, brukerId: Fødselsnummer) =
         adapter.nyesteOverstyring(id.verdi, brukerId.verdi)
@@ -43,7 +52,7 @@ class OverstyringTjeneste(private val ansatt: AnsattTjeneste, private val bruker
             }.getOrElse {
                 when (it) {
                     is RegelException -> {
-                        log.info("Regelkjøring feilet, sjekker om overstyring er mulig for ansatt '${ansattId.verdi}' og bruker '${brukerId.mask()}'")
+                        log.info("Regelkjøring feilet, sjekker om overstyring av regel '${it.regel.beskrivelse.kortNavn}' er mulig for ansatt '${ansattId.verdi}' og bruker '${brukerId.mask()}'")
                         if (it.regel.erOverstyrbar) {
                             log.info("${it.regel.beskrivelse.kortNavn} er overstyrbar, gir tilgang til ansatt '${ansattId.verdi}' og bruker '${brukerId.mask()}'")
                             adapter.lagre(ansattId.verdi, brukerId.verdi, varighet).also {
@@ -67,6 +76,7 @@ class OverstyringTjeneste(private val ansatt: AnsattTjeneste, private val bruker
 
     @CachePut(OVERSTYRING)
     private fun refresh(ansattId: NavId, brukerId: Fødselsnummer, varighet: Duration) : Any = Unit
+
 
 }
 
--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/ObjectUtil.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/utils/ObjectUtil.kt
@@ -1,6 +1,7 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils
 
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.Fødselsnummer
+import kotlin.time.Duration
 
 object ObjectUtil {
     fun requires(verdi: String,len: Int): Unit {
@@ -9,4 +10,18 @@ object ObjectUtil {
     }
 
     fun Fødselsnummer.mask() = verdi.replaceRange(6,11, "*****")
+
+    fun Duration.format(): String {
+        val days = inWholeDays
+        val hours = inWholeHours % 24
+        val minutes = inWholeMinutes % 60
+        val seconds = inWholeSeconds % 60
+
+        return buildString {
+            if (days > 0) append("${days} dager ")
+            if (hours > 0) append("${hours} timer ")
+            if (minutes > 0) append("${minutes} minutter ")
+            if (seconds > 0) append("${seconds} sekunder")
+        }.trim()
+    }
 }

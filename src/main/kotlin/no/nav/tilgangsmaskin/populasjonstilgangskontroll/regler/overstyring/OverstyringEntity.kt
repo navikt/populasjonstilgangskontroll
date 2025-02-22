a4d218395a899546df350b7a0acd0afc23d58ee5
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-22 22:53:09 +0100

    index d

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringEntity.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringEntity.kt
@@ -6,12 +6,17 @@ import jakarta.persistence.EntityListeners
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType.IDENTITY
 import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
 import org.springframework.data.annotation.CreatedDate
 import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.Instant
 
 @Entity(name = "overstyring")
+@Table(indexes = [
+    Index(name = "idx_overstyringentity_navid", columnList = "navid, fnr")
+])
 @EntityListeners(LoggingEntityListener::class, AuditingEntityListener::class)
 class OverstyringEntity(@Column(length = 7, nullable = false) val navid: String,
                         @Column(length = 11, nullable = false) val fnr: String,

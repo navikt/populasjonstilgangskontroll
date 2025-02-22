cf6d34b816892836b1190c211815765c79fd3d58
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-22 22:55:19 +0100

    rename

--- src/test/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/BrukerIdTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/domain/BrukerIdTest.kt
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
-class FødselsnummerTest {
+class BrukerIdTest {
 
     @Test
     @DisplayName("Gyldig Fødselsnummer skal opprettes uten problemer")

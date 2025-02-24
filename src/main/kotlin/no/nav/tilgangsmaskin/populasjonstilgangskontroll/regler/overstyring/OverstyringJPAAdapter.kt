dbadd50bf2ea45a657959a88ddf8110f56057ac6
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-24 12:38:37 +0100

    gyldighet inkluderer hele dagen oppgitt som dato

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringJPAAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/overstyring/OverstyringJPAAdapter.kt
@@ -1,13 +1,15 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.overstyring
 
 import org.springframework.stereotype.Component
-import java.time.ZoneId
+import java.time.ZoneId.systemDefault
+import kotlin.time.Duration.Companion.days
+import kotlin.time.toJavaDuration
 
 @Component
 class OverstyringJPAAdapter(private val repository: OverstyringRepository)  {
 
     fun overstyr(ansattId: String, brukerId: String, metadata: OverstyringMetadata) =
-        repository.save(OverstyringEntity(ansattId, brukerId,metadata.begrunnelse,metadata.varighet.atStartOfDay(ZoneId.systemDefault()).toInstant()))
+        repository.save(OverstyringEntity(ansattId, brukerId,metadata.begrunnelse,metadata.varighet.atStartOfDay(systemDefault()).toInstant().plus(1.days.toJavaDuration())))
 
     fun gjeldendeOverstyringGyldighetDato(ansattId: String, brukerId: String) = repository.finnGjeldendeOverstyring(ansattId, brukerId)?.expires
 }

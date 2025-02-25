9cac43cbeaf425a2049f6392d36fccefd91114aa
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-25 09:14:05 +0100

    bolk

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/pdl/PdlPipRestClientAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/pdl/PdlPipRestClientAdapter.kt
@@ -17,8 +17,8 @@ import org.springframework.web.client.RestClient.ResponseSpec.ErrorHandler
 @Cacheable(PDLPIP)
 class PdlPipRestClientAdapter(@Qualifier(PDLPIP) restClient: RestClient, private val cf : PdlPipConfig, errorHandler: ErrorHandler): AbstractRestClientAdapter(restClient, cf, errorHandler) {
 
-    fun person(brukerId: String) = get<Map<String,Object>>(cf.personURI(), mapOf("ident" to brukerId))
-    fun bolk(brukerIds: List<String>) = post<List<Map<String,Object>>>(cf.personBolkURI(), brukerIds)
+    fun person(brukerId: String) = get<Map<String,Any>>(cf.personURI(), mapOf("ident" to brukerId))
+    fun bolk(brukerIds: List<String>) = post<Any>(cf.personBolkURI(), brukerIds)
 }
 
 

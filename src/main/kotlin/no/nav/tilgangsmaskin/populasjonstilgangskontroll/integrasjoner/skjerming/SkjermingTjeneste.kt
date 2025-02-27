1414ab84c37ab333fac088ae6d1a036e5f7df5ae
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-02-27 10:56:24 +0100

    test retries skjerming

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/skjerming/SkjermingTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/skjerming/SkjermingTjeneste.kt
@@ -5,6 +5,7 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.errors.RecoverableRestE
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.RetryingOnRecoverableCacheableService
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.skjerming.SkjermingConfig.Companion.SKJERMING
 import org.slf4j.LoggerFactory
+import org.springframework.retry.ExhaustedRetryException
 import org.springframework.retry.annotation.Recover
 import kotlin.arrayOf
 
@@ -13,10 +14,14 @@ class SkjermingTjeneste(private val adapter: SkjermingRestClientAdapter) {
 
     private val log = LoggerFactory.getLogger(SkjermingTjeneste::class.java)
 
-    fun erSkjermet(brukerId: BrukerId) = adapter.erSkjermet(brukerId.verdi)
+
+    fun erSkjermet(brukerId: BrukerId)  =  adapter.erSkjermet(brukerId.verdi)
 
     @Recover
-    fun fallback(e: RecoverableRestException, brukerId: BrukerId) = false.also {
-        log.warn("Recovery fallback: Kunne ikke hente skjerming for bruker ${brukerId.verdi} ", e)
+    fun fallback(e: Throwable, brukerId: BrukerId) =
+        when (e) {
+            is RecoverableRestException -> true
+            is ExhaustedRetryException -> throw e.cause ?: e
+            else -> throw e
     }
 }
---
+++ src/test/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/skjerming/SkjermingRetryTest.kt
@@ -0,0 +1,70 @@
+package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.skjerming
+
+import com.ninjasquad.springmockk.MockkBean
+import io.mockk.every
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.errors.IrrecoverableRestException
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.errors.RecoverableRestException
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.TestData.brukerId
+
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.Constants.TEST
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.assertThrows
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
+import org.springframework.retry.annotation.EnableRetry
+import org.springframework.test.context.ActiveProfiles
+import org.springframework.test.context.ContextConfiguration
+import java.net.URI
+import kotlin.test.Test
+
+@ContextConfiguration(classes = [SkjermingTjeneste::class])
+@ExtendWith(MockKExtension::class)
+@EnableRetry
+@ActiveProfiles(TEST)
+@SpringBootTest
+internal class SkjermingRetryTest {
+
+    val uri =  URI.create("https://www.vg.no")
+
+    @MockkBean
+    lateinit var adapter: SkjermingRestClientAdapter
+
+    @Autowired
+    lateinit  var tjeneste: SkjermingTjeneste
+
+    @Test
+    @DisplayName("Returner true etter at antall forsøk er oppbrukt")
+    fun erSkjermetEtterTreMislykkedeForsøk() {
+        every { adapter.erSkjermet(brukerId.verdi) } throws RecoverableRestException(INTERNAL_SERVER_ERROR, uri)
+        assertThat(tjeneste.erSkjermet(brukerId)).isTrue
+        verify(exactly = 3) {
+            tjeneste.erSkjermet(brukerId)
+        }
+    }
+
+    @Test
+    @DisplayName("Test retry tar seg inn etter først å ha feilet")
+    fun testRetryOK() {
+        every { adapter.erSkjermet(brukerId.verdi) } throws RecoverableRestException(INTERNAL_SERVER_ERROR, uri) andThen false
+        assertThat(tjeneste.erSkjermet(brukerId)).isFalse
+        verify(exactly = 2) {
+            tjeneste.erSkjermet(brukerId)
+        }
+    }
+    @Test
+    @DisplayName("Andre exceptions fører ikke til retry, og kastes umiddlelbart videre")
+    fun andreExceptions() {
+        every { adapter.erSkjermet(brukerId.verdi) } throws IrrecoverableRestException(INTERNAL_SERVER_ERROR, uri)
+        assertThrows<IrrecoverableRestException>  {
+            tjeneste.erSkjermet(brukerId)
+        }
+        verify(exactly = 1) {
+            tjeneste.erSkjermet(brukerId)
+        }
+    }
+}

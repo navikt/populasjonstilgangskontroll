c314901f9052845c4e479b9222477effdcc9295e
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-03-02 11:12:57 +0100

    rename

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/RegelTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/RegelTjeneste.kt
@@ -1,8 +1,8 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler
 
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelType.KOMPLETT
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.BrukerId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.AnsattId
-import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelSpec.RegelType.ALLE
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.overstyring.OverstyringSjekker
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -24,14 +24,13 @@ class RegelTjeneste(private val motor: RegelMotor, private val brukerTjeneste: B
 
     fun bulkRegler(ansattId: AnsattId, vararg specs: RegelSpec) {
         log.trace("Eksekverer ${specs.size} regler for ansatt $ansattId")
-        val (alle,kjerne) = specs.partition { it.type == ALLE }
-        log.trace("Eksekverer ${alle.size} komplette regler for ansatt $ansattId")
-        log.trace("Eksekverer ${kjerne.size} kjerneregler for ansatt $ansattId")
-
+        val (komplett,kjerne) = specs.partition { it.type == KOMPLETT }
         val ansatt = ansattTjeneste.ansatt(ansattId)
         val exceptions = mutableListOf<RegelException>()
+        log.trace("Eksekverer ${kjerne.size} kjerneregler for ansatt $ansattId")
         kjerne.forEach { eksekver({ motor.kjerneregler(ansatt, brukerTjeneste.bruker(it.brukerId)) }, exceptions) }.also { log.trace("Antall feil etter kjerneregler er ${exceptions.size}") }
-        alle.forEach { eksekver({ motor.alleRegler(ansatt, brukerTjeneste.bruker(it.brukerId)) }, exceptions) }.also { log.trace("Antall feil etter alle regler er ${exceptions.size}") }
+        log.trace("Eksekverer ${komplett.size} komplette regler for ansatt $ansattId")
+        komplett.forEach { eksekver({ motor.alleRegler(ansatt, brukerTjeneste.bruker(it.brukerId)) }, exceptions) }.also { log.trace("Antall feil etter alle regler er ${exceptions.size}") }
         if (exceptions.isNotEmpty()) throw BulkRegelException(exceptions).also {
             log.trace("Det er ${exceptions.size} feil i bulk-regler for ansatt $ansattId")
         }

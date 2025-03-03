0d9b17567b31d1b2882a592afc15bf56e4de5daf
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-03-03 17:11:59 +0100

    igmor unknown

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/pdl/PdlPerson.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/pdl/PdlPerson.kt
@@ -1,9 +1,11 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.pdl
 
 import com.fasterxml.jackson.annotation.JsonEnumDefaultValue
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import java.time.LocalDate
 
 data class PdlPerson(val adressebeskyttelse: List<Adressebeskyttelse>, val folkeregisteridentifikator: List<Folkeregisteridentifikator>)  {
+    @JsonIgnoreProperties(ignoreUnknown = true)
     data class Folkeregisteridentifikator(val identifikasjonsnummer: String, val type: String)
     data class Adressebeskyttelse(val gradering: AdressebeskyttelseGradering)  {
         enum class AdressebeskyttelseGradering { STRENGT_FORTROLIG_UTLAND, STRENGT_FORTROLIG, FORTROLIG, @JsonEnumDefaultValue

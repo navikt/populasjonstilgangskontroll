f9cbeaf9b1a4e76b6727f4c446e707f5f434d7ff
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-03-03 16:45:50 +0100

    selctively ignore unknown properties

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
@@ -1,24 +1,33 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+import com.fasterxml.jackson.annotation.JsonInclude
 import com.github.benmanes.caffeine.cache.Caffeine
 import no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL
+import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenService
 import no.nav.security.token.support.client.spring.ClientConfigurationProperties
-import no.nav.security.token.support.client.spring.oauth2.ClientConfigurationPropertiesMatcher
 import no.nav.security.token.support.client.spring.oauth2.OAuth2ClientRequestInterceptor
 import org.slf4j.LoggerFactory
-import org.springframework.cache.CacheManager
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.cache.annotation.CachingConfigurer
 import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.cache.interceptor.KeyGenerator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ReloadableResourceBundleMessageSource
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder
 
 @Configuration
 class FellesBeanConfig : CachingConfigurer {
 
     private val log = LoggerFactory.getLogger(FellesBeanConfig::class.java)
+
+    @Bean
+    fun jsonCustomizer() = Jackson2ObjectMapperBuilderCustomizer {it.mixIn(OAuth2AccessTokenResponse::class.java, IgnoreUnknownMixin::class.java) }
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    private interface IgnoreUnknownMixin
 
     @Bean
     fun errorMessageSource() = ReloadableResourceBundleMessageSource().apply {

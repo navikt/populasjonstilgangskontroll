9197a6763398ff13c2cb3960b6fa270fd79c6119
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-03-07 08:56:36 +0100

    log

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/errors/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/errors/ExceptionHandler.kt
@@ -1,23 +1,30 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.errors
 
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.AbstractRestClientAdapter
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.BulkRegelException
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.Regel.Companion.TYPE_URI
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelException
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.http.HttpStatusCode
 import org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON
 import org.springframework.http.ProblemDetail
-import org.springframework.http.ProblemDetail.*
+import org.springframework.http.ProblemDetail.forStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.status
+import org.springframework.http.converter.HttpMessageNotWritableException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
 @Order(-1)
 class ExceptionHandler : ResponseEntityExceptionHandler() {
 
+    protected val log = getLogger(ExceptionHandler::class.java)
 
 
     @ExceptionHandler(BulkRegelException::class)
@@ -26,8 +33,14 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
             .headers(HttpHeaders().apply { contentType = APPLICATION_PROBLEM_JSON })
             .body<ProblemDetail>(bulkDetail(e))
 
-    @ExceptionHandler(Throwable::class)
-    fun catchAll(e: Throwable) = logger.error(e.message, e)
+    override fun handleHttpMessageNotWritable(
+        ex: HttpMessageNotWritableException,
+        headers: HttpHeaders,
+        status: HttpStatusCode,
+        request: WebRequest): ResponseEntity<in Any>? {
+        log.error("Feil ved skriving av melding", ex)
+        return super.handleHttpMessageNotWritable(ex, headers, status, request)
+    }
 
     fun bulkDetail(e: BulkRegelException) = forStatus(FORBIDDEN).apply {
         title = e.message

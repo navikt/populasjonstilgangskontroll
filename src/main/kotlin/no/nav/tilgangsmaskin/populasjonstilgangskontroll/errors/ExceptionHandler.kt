adefcceef2fd92e689a5407751675c9f6b42bba3
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-03-07 08:43:30 +0100

    log

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/errors/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/errors/ExceptionHandler.kt
@@ -6,6 +6,7 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.regler.RegelException
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON
 import org.springframework.http.ProblemDetail
 import org.springframework.http.ProblemDetail.*
@@ -18,11 +19,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @Order(-1)
 class ExceptionHandler : ResponseEntityExceptionHandler() {
 
+
+
     @ExceptionHandler(BulkRegelException::class)
     fun bulkExceptionHandler(e: BulkRegelException) =
         status(FORBIDDEN)
             .headers(HttpHeaders().apply { contentType = APPLICATION_PROBLEM_JSON })
             .body<ProblemDetail>(bulkDetail(e))
+
+    @ExceptionHandler(Throwable::class)
+    fun catchAll(e: Throwable) =   logger.error(e.message, e) }
 
     fun bulkDetail(e: BulkRegelException) = forStatus(FORBIDDEN).apply {
         title = e.message

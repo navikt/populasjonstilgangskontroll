4e5aca747c60419f12f4c9062281af892c5d8383
Jan-Olav Kjørås Eide <Jan-Olav.Eide@Eide-sin-MacBook-Pro.local>
2025-03-10 16:07:06 +0100

    log

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/entra/EntraTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/entra/EntraTjeneste.kt
@@ -3,20 +3,24 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.Ansatt
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.AnsattId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra.EntraConfig.Companion.GRAPH
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.FellesRetryListener
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.RetryingOnRecoverableCacheableService
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.utils.TokenAccessor
+import org.slf4j.LoggerFactory
 import java.util.UUID
 
 @RetryingOnRecoverableCacheableService(cacheNames = [GRAPH])
-class EntraTjeneste(
-    private val adapter: EntraClientAdapter,
-    private val accessor: TokenAccessor) {
+class EntraTjeneste(private val adapter: EntraClientAdapter, private val accessor: TokenAccessor) {
+
+    private val log = LoggerFactory.getLogger(EntraTjeneste::class.java)
 
     fun ansatt(ident: AnsattId) =
         run {
             oid()?.let {
+                log.info("Henter gruppemedlemsskap via oid fra token '$it'")
                 EntraResponse(adapter.grupper("$it"))
             } ?: run {
+                log.info("Henter gruppemedlemsskap via navident '$ident'")
                 val attributter = adapter.attributter(ident.verdi).attributter.first()
                 val grupper = adapter.grupper(attributter.id.toString())
                 EntraResponse(grupper, attributter.tilAttributter())

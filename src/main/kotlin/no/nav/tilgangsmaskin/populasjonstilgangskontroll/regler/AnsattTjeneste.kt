4256bd1f6fe7dd72b93c8883a28c1ef69f8854aa
Jan-Olav Kjørås Eide <Jan-Olav.Eide@Eide-sin-MacBook-Pro.local>
2025-03-11 10:50:02 +0100

    Ansatt log

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/AnsattTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regler/AnsattTjeneste.kt
@@ -4,16 +4,20 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.Ansatt
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.AnsattId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra.EntraTjeneste
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.nom.NomTjeneste
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 @Service
 class AnsattTjeneste(private val entra: EntraTjeneste, private val nom: NomTjeneste, private val pdl: BrukerTjeneste) {
+    private val log = LoggerFactory.getLogger(AnsattTjeneste::class.java)
 
 
     fun ansatt(ansattId: AnsattId) : Ansatt {
         val entraData = entra.ansatt(ansattId)
         val fnr = nom.fnrForAnsatt(ansattId)
         val  ansattBruker = fnr?.let { pdl.bruker(it) }
-        return Ansatt(ansattBruker, ansattId,entraData.attributter,*entraData.grupper.toTypedArray())
+        return Ansatt(ansattBruker, ansattId,entraData.attributter,*entraData.grupper.toTypedArray()).also {
+            log.info("Ansatt er $it")
+        }
     }
 }

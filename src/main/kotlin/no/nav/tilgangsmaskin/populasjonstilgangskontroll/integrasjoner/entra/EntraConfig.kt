06d1f658af58177f8434f23d5693cb5947c228b9
Jan-Olav Kjørås Eide <Jan-Olav.Eide@Eide-sin-MacBook-Pro.local>
2025-03-12 08:43:06 +0100

    batch størrelse er konfigurerbar

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/entra/EntraConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/entra/EntraConfig.kt
@@ -6,7 +6,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import java.net.URI
 
 @ConfigurationProperties(GRAPH)
-class EntraConfig(baseUri: URI, pingPath: String = DEFAULT_PING_PATH, enabled: Boolean = true) :
+class EntraConfig(baseUri: URI, pingPath: String = DEFAULT_PING_PATH, private val size: Int = DEFAULT_BATCH_SIZE, enabled: Boolean = true) :
     AbstractRestConfig(baseUri, pingPath, GRAPH, enabled) {
 
     fun userURI(navIdent: String) = builder().path(USERS_PATH)
@@ -18,22 +18,23 @@ class EntraConfig(baseUri: URI, pingPath: String = DEFAULT_PING_PATH, enabled: B
     fun grupperURI(ansattId: String) = builder().path(GRUPPER_PATH)
         .queryParam(PARAM_NAME_SELECT, PARAM_VALUE_SELECT_GROUPS)
         .queryParam(PARAM_NAME_COUNT, "true")
-        .queryParam(PARAM_NAME_TOP, "250")
+        .queryParam(PARAM_NAME_TOP, size)
         .build(ansattId)
 
     override fun toString() = "$javaClass.simpleName [baseUri=$baseUri, pingEndpoint=$pingEndpoint]"
 
 
     companion object {
-        val HEADER_CONSISTENCY_LEVEL = "ConsistencyLevel" to "eventual"
         const val GRAPH = "graph"
-        private const val USERS_PATH: String = "/users"
+        val HEADER_CONSISTENCY_LEVEL = "ConsistencyLevel" to "eventual"
+        private const val DEFAULT_BATCH_SIZE = 250
+        private const val USERS_PATH = "/users"
         private const val GRUPPER_PATH = "/users/{ansattId}/memberOf"
-        private const val PARAM_NAME_SELECT: String = "\$select"
-        private const val PARAM_NAME_FILTER: String = "\$filter"
-        private const val PARAM_NAME_COUNT: String = "\$count"
-        private const val PARAM_VALUE_SELECT_USER: String = "id"
-        private const val PARAM_VALUE_SELECT_GROUPS: String = "id,displayName"
+        private const val PARAM_NAME_SELECT = "\$select"
+        private const val PARAM_NAME_FILTER = "\$filter"
+        private const val PARAM_NAME_COUNT  = "\$count"
+        private const val PARAM_VALUE_SELECT_USER  = "id"
+        private const val PARAM_VALUE_SELECT_GROUPS  = "id,displayName"
         private const val DEFAULT_PING_PATH = "/organization"
         private const val PARAM_NAME_TOP = "\$top"
     }

c12c87becd183686cb4e35b467582b912be005eb
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-14 12:58:16 +0100

    string it is

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomHendelse.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomHendelse.kt
@@ -2,13 +2,12 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.nom
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
-import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.BrukerId
 import java.time.LocalDate
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class NomHendelse(
     @JsonProperty("personident")
-    val brukerId: BrukerId?,
+    val brukerId: String?,
     @JsonProperty("navident")
     val ansattId: String?,
     val startdato: LocalDate,

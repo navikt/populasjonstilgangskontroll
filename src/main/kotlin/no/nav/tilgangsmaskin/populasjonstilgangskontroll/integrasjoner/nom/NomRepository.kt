4513914f0b394288c81596320f741e09057b1e11
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-15 18:43:50 +0100

    bruk query

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomRepository.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomRepository.kt
@@ -1,7 +1,9 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.nom
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface NomRepository : JpaRepository<NomEntity, Long> {
+  @Query("SELECT n.fnr FROM NomEntity n WHERE n.gyldigtil IS NULL OR n.gyldigtil >= CURRENT_DATE")
   fun findByNavid(navId: String): NomEntity?
 }

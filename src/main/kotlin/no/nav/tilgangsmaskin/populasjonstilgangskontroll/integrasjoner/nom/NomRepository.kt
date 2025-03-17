03c63643ab27b928dc57c301c9d5e6c95d4b783d
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-17 11:43:36 +0100

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomRepository.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query
 import java.time.Instant
 
 interface NomRepository : JpaRepository<NomEntity, Long> {
-  @Query("SELECT n.fnr FROM NomEntity n WHERE n.navid = :navId  AND n.gyldigtil IS NULL OR n.gyldigtil >= CURRENT_DATE")
+  @Query("SELECT n.fnr FROM NomEntity n WHERE n.navid = :navId AND (n.gyldigtil IS NULL OR n.gyldigtil >= CURRENT_DATE)")
   fun finnGyldigAnsattFnr(navId: String): String?
 
   fun  deleteByGyldigtilBefore(before: Instant) : Int

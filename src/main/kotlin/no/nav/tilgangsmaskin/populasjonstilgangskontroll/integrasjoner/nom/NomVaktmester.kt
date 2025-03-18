719ce1483e18abff287d349e9a895840673857e2
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-18 10:51:37 +0100

    bruk builder, fjern cfg

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomVaktmester.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomVaktmester.kt
@@ -7,22 +7,18 @@ import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit.MINUTES
 
 @Component
-class NomVaktmester(private val nom: NomTjeneste, private val elector: LederUtvelger) {
+class NomVaktmester(private val nom: NomTjeneste, private val lederUtvelger: LederUtvelger) {
 
     private val log = getLogger(NomVaktmester::class.java)
 
     @Scheduled(fixedRate = 60, timeUnit = MINUTES)
     fun ryddOpp() {
-        if ((elector.erLeder)) {
-            log.info("Vaktmester fjerner utgått informasjon")
+        if ((lederUtvelger.erLeder)) {
             nom.ryddOpp().also {
                 if (it > 0) {
                     log.info("Vaktmester fjernet $it rad(er) med utgått informasjon")
                 }
             }
-        }
-        else {
-            log.trace("Vaktmester er ikke leder")
         }
     }
 }

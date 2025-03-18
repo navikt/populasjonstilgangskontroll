3ab71b5136d9c9dde6ea8c45e67247fee30a2a87
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-18 15:05:05 +0100

    rare data valideres fød lagring

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomHendelseKonsument.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomHendelseKonsument.kt
@@ -4,14 +4,12 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.nom
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.AnsattId
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.domain.BrukerId
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.beans.factory.aspectj.AnnotationBeanConfigurerAspect
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
 
 @Component
 class NomHendelseKonsument(private val nom: NomTjeneste) {
 
-    private final val annotationBeanConfigurerAspect: AnnotationBeanConfigurerAspect = TODO("initialize me")
     private val log = getLogger(NomHendelseKonsument::class.java)
     @KafkaListener(topics = ["#{'\${nom.topic}'}"])
     fun listen(hendelse : NomHendelse) {

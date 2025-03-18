3cfe5d0a8d5617e1875f9bb6f26f0d74a4641291
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-18 10:45:03 +0100

    bruk builder, fjern cfg

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/LeaderElector.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/LeaderElector.kt
@@ -1,22 +1,20 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
 
-import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.nom.NomConfig.Companion.NOM
-import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestClient
+import org.springframework.web.client.RestClient.Builder
 import java.net.InetAddress
 import java.net.URI
 import java.time.LocalDateTime
 
 @Service
 class LeaderElector(private val adapter: LeaderElectorClientAdapter) {
-    val erLeder get() = adapter.lederHostname() == InetAddress.getLocalHost().hostName
+    val erLeder get() = adapter.leder() == InetAddress.getLocalHost().hostName
 }
 @Component
-class LeaderElectorClientAdapter(@Qualifier(NOM) client: RestClient, cf : LeaderElectorConfig) : AbstractRestClientAdapter(client, cf) {
-    fun lederHostname() = get<LeaderElectorRespons>(cfg.baseUri).name
+class LeaderElectorClientAdapter(builder: Builder, cf : LeaderElectorConfig) : AbstractRestClientAdapter(builder.build(), cf) {
+    fun leder() = get<LeaderElectorRespons>(cfg.baseUri).name
 }
 private data class LeaderElectorRespons(val name: String, val last_update: LocalDateTime)
 @Component

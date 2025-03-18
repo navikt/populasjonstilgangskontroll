752f6a308b21c9c30d97794b9e9910b8a578470f
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-18 11:45:36 +0100

    sse test sse

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomVaktmester.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomVaktmester.kt
@@ -4,16 +4,17 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.Le
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import java.util.concurrent.TimeUnit.MINUTES
+import java.util.concurrent.TimeUnit.SECONDS
 
 @Component
 class NomVaktmester(private val nom: NomTjeneste, private val lederUtvelger: LederUtvelger) {
 
     private val log = getLogger(NomVaktmester::class.java)
 
-    @Scheduled(fixedRate = 60, timeUnit = MINUTES)
+    @Scheduled(fixedRate = 60, timeUnit = SECONDS)
     fun ryddOpp() {
         if ((lederUtvelger.erLeder)) {
+            log.info("Jeg er leder og rydder opp")
             nom.ryddOpp().also {
                 if (it > 0) {
                     log.info("Vaktmester fjernet $it rad(er) med utgått informasjon")

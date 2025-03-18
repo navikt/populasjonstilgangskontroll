8358e7e8d0d2ef52a9a9126d4a70f0791981d948
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-18 14:27:10 +0100

    polish

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/LederUtvelger.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/LederUtvelger.kt
@@ -1,15 +1,12 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
 
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.ApplicationEvent
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.ApplicationListener
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
-import org.springframework.web.client.RestClient.Builder
-import org.springframework.web.reactive.function.client.WebClient
-import reactor.core.publisher.Flux
+import org.springframework.web.reactive.function.client.WebClient.Builder
 import java.net.InetAddress
 import java.net.URI
 import java.time.LocalDateTime
@@ -17,48 +14,38 @@ import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.Le
 
 
 @Service
-class LederUtvelger(private val adapter: LederUtvelgerClientAdapter) : ApplicationListener<LeaderChangedEvent> {
-    private val log = getLogger(LederUtvelger::class.java)
+class LederUtvelger : ApplicationListener<LeaderChangedEvent> {
 
-    var leder : String? = null
-    val erLeder get() = leder  == InetAddress.getLocalHost().hostName
+    var erLeder : Boolean = false
 
     override fun onApplicationEvent(event: LeaderChangedEvent) {
-        log.info("SSE spring event $event")
-        leder = event.leader
+        erLeder = event.leader == InetAddress.getLocalHost().hostName
     }
 }
-@Component
-class LederUtvelgerClientAdapter(builder: Builder, cf : LederUtvelgerConfig) : AbstractRestClientAdapter(builder.build(), cf) {
-    fun leder() = get<LederUtvelgerRespons>(cfg.baseUri).name
-}
-data class LederUtvelgerRespons(val name: String, val last_update: LocalDateTime)
-@Component
-class LederUtvelgerConfig(@Value("\${elector.get.url}")  base: URI): AbstractRestConfig(base)
 
 @Service
-class SseService(private val webClient: WebClient.Builder) {
+class SseService(private val webClient: Builder) {
 
-    fun subscribe(uri: URI) =
+    fun start(uri: URI) =
          webClient.build()
             .get()
             .uri(uri)
             .retrieve()
             .bodyToFlux(LederUtvelgerRespons::class.java)
+
+    data class LederUtvelgerRespons(val name: String, val last_update: LocalDateTime)
+
 }
 
 
 @Component
-class SseSubscriber(private val sseService: SseService, @Value("\${elector.sse.url}") private val uri: URI, val publisher: LeaderChangedEventPublisher) {
+private class SseSubscriber(private val sseService: SseService, @Value("\${elector.sse.url}") private val uri: URI, val publisher: LeaderChangedEventPublisher) {
     init {
-        startSubscription()
+        start()
     }
-    private val log = getLogger(SseSubscriber::class.java)
-    private final fun startSubscription() {
-        val eventStream: Flux<LederUtvelgerRespons> = sseService.subscribe(uri)
-        eventStream.subscribe { event ->
-            log.info("SSE  event: $event")
-            publisher.publish(event.name)
+    private final fun start() {
+        sseService.start(uri).subscribe {
+            publisher.publish(it.name)
         }
     }
 }

2ace75590838a5eee8f7a4ef02a735f21abda7f2
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-20 21:08:42 +0100

    ny tag

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomVaktmester.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomVaktmester.kt
@@ -1,5 +1,7 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.nom
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles.LederUtvelger
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.scheduling.annotation.Scheduled
@@ -7,15 +9,19 @@ import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit.SECONDS
 
 @Component
-class NomVaktmester(private val nom: NomOperasjoner, private val utvelger: LederUtvelger) {
+class NomVaktmester(private val nom: NomOperasjoner, private val utvelger: LederUtvelger, private val registry: MeterRegistry) {
 
     private val log = getLogger(NomVaktmester::class.java)
+    private val counter =  Counter.builder("vaktmester.rader.fjernet")
+    .description("Antall rader fjernet")
+    .register(registry)
 
     @Scheduled(fixedRate = 60, timeUnit = SECONDS)
     fun ryddOpp() =
         if (utvelger.erLeder) {
             nom.ryddOpp().also {
                 if (it > 0) {
+                    counter.increment(it.toDouble())
                     log.info("Vaktmester ryddet opp $it rad(er) med utgått informasjon om ansatte som ikke lenger jobber i Nav")
                 }
             }

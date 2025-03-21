6bf5b98e9d634f8988286709ab9ba7a6d238f9bf
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-21 10:56:36 +0100

    om hundrede år er allting glemt

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomJPAAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/nom/NomJPAAdapter.kt
@@ -19,11 +19,11 @@ class NomJPAAdapter(private val repo: NomRepository) {
     fun upsert(data: NomAnsattData) =
         upsert(data.ansattId, data.brukerId, data.gyldighet.start, data.gyldighet.endInclusive)
 
-    private fun upsert(ansattId: AnsattId, ansattFnr: BrukerId, start: LocalDate? = null,slutt: LocalDate? = null) =
+    private fun upsert(ansattId: AnsattId, ansattFnr: BrukerId, start: LocalDate,slutt: LocalDate) =
          repo.save(repo.findByNavid(ansattId.verdi)?.apply {
              this.fnr = fnr
-             startdato = start?.toInstant()
-             gyldigtil = slutt?.toInstant()
-        } ?: NomEntity(ansattId.verdi, ansattFnr.verdi, start?.toInstant(),slutt?.toInstant())).id!!
+             startdato = start.toInstant()
+             gyldigtil = slutt.toInstant()
+        } ?: NomEntity(ansattId.verdi, ansattFnr.verdi, start.toInstant(),slutt.toInstant())).id!!
     fun fnrForAnsatt(ansattId: String) = repo.ansattFødselsnummer(ansattId)?.let { AnsattFnr(it) }
     private fun LocalDate.toInstant(): Instant = atStartOfDay(systemDefault()).toInstant()}

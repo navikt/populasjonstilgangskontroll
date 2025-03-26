3986f1fac4f42f704a4a1d076ad8f4c325bba9a9
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-26 15:55:39 +0100

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regelmotor/overstyring/OverstyringResultatHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/regelmotor/overstyring/OverstyringResultatHandler.kt
@@ -12,17 +12,17 @@ import org.springframework.stereotype.Component
 class OverstyringResultatHandler {
     private val log = LoggerFactory.getLogger(OverstyringResultatHandler::class.java)
     fun gyldig(ansattId: AnsattId, brukerId: BrukerId, diff: String) = true.also {
-        log.trace("Overstyring er gyldig i $diff til for ansatt '${ansattId.verdi}' og bruker '${brukerId.maskFnr()}'")
+        log.trace("Overstyring er gyldig i $diff til for ${ansattId} og bruker $brukerId")
     }
     fun utgått(ansattId: AnsattId, brukerId: BrukerId, diff: String) = false.also {
-        log.warn("Overstyring har gått ut på tid for $diff siden for ansatt '${ansattId.verdi}' og bruker '${brukerId.maskFnr()}'")
+        log.warn("Overstyring har gått ut på tid for $diff siden for  $ansattId og bruker $brukerId")
     }
     fun ingen(ansattId: AnsattId, brukerId: BrukerId) = false.also {
-        log.trace("Ingen overstyring for ansatt '${ansattId.verdi}' og bruker '${brukerId.maskFnr()}' ble funnet i databasen")
+        log.trace("Ingen overstyring for ansatt $ansattId og bruker $brukerId ble funnet i databasen")
     }
     fun avvist(ansattId: AnsattId, brukerId: BrukerId) =
-        log.error("Overstyring er avvist av kjerneregler for '${ansattId.verdi}' og bruker '${brukerId.maskFnr()})")
+        log.error("Overstyring er avvist av kjerneregler for $ansattId og bruker $brukerId")
 
     fun overstyrt(ansattId: AnsattId, brukerId: BrukerId) =
-        log.info("Overstyring er gjort for ansatt '${ansattId.verdi}' og bruker '${brukerId.maskFnr()}'")
+        log.info("Overstyring er gjort for ansatt $ansattId og bruker $brukerId")
 }

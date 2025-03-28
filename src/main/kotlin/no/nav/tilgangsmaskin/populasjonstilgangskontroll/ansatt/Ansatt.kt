5776b281fee88e41769d3a3b0309c4d2915b271e
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-03-28 10:24:17 +0100

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/ansatt/Ansatt.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/ansatt/Ansatt.kt
@@ -2,6 +2,7 @@ package no.nav.tilgangsmaskin.populasjonstilgangskontroll.ansatt
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.bruker.Bruker
+import no.nav.tilgangsmaskin.populasjonstilgangskontroll.bruker.Familie
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.bruker.GeografiskTilknytning.BydelTilknytning
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.bruker.GeografiskTilknytning.KommuneTilknytning
 import no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.entra.EntraGruppe
@@ -31,10 +32,10 @@ data class Ansatt(val identifikatorer: AnsattIdentifikatorer, val grupper: List<
 
     infix fun kanBehandle(id: UUID) = grupper.any { it.id == id }
 
-    infix fun erForeldreEllerBarnTil(bruker: Bruker) = bruker.brukerId in foreldreOgBarn.map { it.brukerId }
+    infix fun erForeldreEllerBarnTil(bruker: Bruker) = erFamiliemedlemTil(bruker.brukerId,foreldreOgBarn)
+    infix fun erSøskenTil(bruker: Bruker) = erFamiliemedlemTil(bruker.brukerId,søsken)
 
-    infix fun erSøskenTil(bruker: Bruker) = bruker.brukerId in søsken.map { it.brukerId }
-
+    private fun erFamiliemedlemTil(brukerId: AnsattFnr, medlemmer: List<Familie.FamilieMedlem>) = brukerId in medlemmer.map { it.brukerId } + søsken.map { it.brukerId }
     data class AnsattIdentifikatorer(val ansattId: AnsattId, val oid: UUID, val ansattFnr: AnsattFnr? = null)
 
 }

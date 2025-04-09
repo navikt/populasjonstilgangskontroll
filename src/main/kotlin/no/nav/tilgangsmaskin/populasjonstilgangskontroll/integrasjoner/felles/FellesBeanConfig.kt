93ec2f69f9aede84ff26e58e54eda088997ea1b3
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-04-09 10:50:46 +0200

    mapper

--- src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/FellesBeanConfig.kt
@@ -1,43 +1,27 @@
 package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.databind.ObjectMapper
 import jakarta.servlet.http.HttpServletRequest
 import no.nav.boot.conditionals.ConditionalOnNotProd
 import no.nav.security.token.support.client.core.oauth2.OAuth2AccessTokenResponse
 import no.nav.security.token.support.client.spring.oauth2.OAuth2ClientRequestInterceptor
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.boot.actuate.health.Health
-import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.boot.actuate.web.exchanges.HttpExchangeRepository
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository
 import org.springframework.boot.actuate.web.exchanges.Include.defaultIncludes
 import org.springframework.boot.actuate.web.exchanges.servlet.HttpExchangesFilter
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer
 import org.springframework.boot.web.client.RestClientCustomizer
-import org.springframework.cache.CacheManager
-import org.springframework.cache.annotation.CachingConfigurer
-import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.interceptor.KeyGenerator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.support.ReloadableResourceBundleMessageSource
-import org.springframework.data.redis.cache.RedisCacheConfiguration
-import org.springframework.data.redis.cache.RedisCacheManager
-import org.springframework.data.redis.connection.RedisConnectionFactory
-import org.springframework.data.redis.core.RedisConnectionUtils
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializationContext
-import org.springframework.data.redis.serializer.StringRedisSerializer
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
-import java.time.Duration
 
 
 @Configuration
-@EnableCaching
-class FellesBeanConfig(private val ansattIdAddingInterceptor: ConsumerAwareHandlerInterceptor, private val cf: RedisConnectionFactory) : CachingConfigurer, WebMvcConfigurer {
+class FellesBeanConfig(private val ansattIdAddingInterceptor: ConsumerAwareHandlerInterceptor):  WebMvcConfigurer {
 
     private val log = getLogger(javaClass)
 
@@ -74,44 +58,7 @@ class FellesBeanConfig(private val ansattIdAddingInterceptor: ConsumerAwareHandl
         override fun shouldNotFilter(request: HttpServletRequest) = request.servletPath.contains("monitoring")
     }
 
-    @Bean
-    fun redisHealthIndicator(cf: RedisConnectionFactory) = object : HealthIndicator  {
-        override fun health() =
-            RedisConnectionUtils.getConnection(cf).use { connection ->
-                runCatching {
-                    if (connection.ping().equals("PONG", ignoreCase = true)) {
-                        Health.up().withDetail("Redis", "Connection is healthy").build()
-                    } else {
-                        Health.down().withDetail("Redis", "Ping failed").build()
-                    }
-                }.fold(
-                    onSuccess = { it },
-                    onFailure = { Health.down(it).withDetail("Redis", "Connection failed").build() }
-                )
-            }
-    }
-
-    override fun cacheManager(): CacheManager {
-        val config = RedisCacheConfiguration.defaultCacheConfig()
-            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
-            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer()))
-
-        return RedisCacheManager.builder(cf)
-            .cacheDefaults(config)
-            .build()
-    }
-
-
     override fun addInterceptors(registry: InterceptorRegistry) {
         registry.addInterceptor(ansattIdAddingInterceptor)
-    }
-
-
-    override fun keyGenerator() = KeyGenerator { target, method, params ->
-        buildString {
-            append(target::class)
-            append(method.name)
-            params.forEach { append(it) }
-        }
     }
 }
---
+++ src/main/kotlin/no/nav/tilgangsmaskin/populasjonstilgangskontroll/integrasjoner/felles/RedisConfiguration.kt
@@ -0,0 +1,63 @@
+package no.nav.tilgangsmaskin.populasjonstilgangskontroll.integrasjoner.felles
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.boot.actuate.health.Health
+import org.springframework.boot.actuate.health.HealthIndicator
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.CachingConfigurer
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.interceptor.KeyGenerator
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisConnectionUtils
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+
+@Configuration
+@EnableCaching
+class RedisConfiguration(private val cf: RedisConnectionFactory, private val mapper: ObjectMapper) : CachingConfigurer {
+
+    @Bean
+    fun redisHealthIndicator(cf: RedisConnectionFactory) = object : HealthIndicator {
+        override fun health() =
+            RedisConnectionUtils.getConnection(cf).use { connection ->
+                runCatching {
+                    if (connection.ping().equals("PONG", ignoreCase = true)) {
+                        Health.up().withDetail("Redis", "Connection is healthy").build()
+                    } else {
+                        Health.down().withDetail("Redis", "Ping failed").build()
+                    }
+                }.fold(
+                    onSuccess = { it },
+                    onFailure = { Health.down(it).withDetail("Redis", "Connection failed").build() }
+                )
+            }
+    }
+
+    override fun cacheManager(): CacheManager {
+        val config = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(
+                    GenericJackson2JsonRedisSerializer(mapper)
+                )
+            )
+
+        return RedisCacheManager.builder(cf)
+            .cacheDefaults(config)
+            .build()
+    }
+
+
+    override fun keyGenerator() = KeyGenerator { target, method, params ->
+        buildString {
+            append(target::class)
+            append(method.name)
+            params.forEach { append(it) }
+        }
+    }
+}

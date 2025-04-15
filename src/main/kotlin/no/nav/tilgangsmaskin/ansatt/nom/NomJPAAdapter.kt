8101a102e95dd0975ea0bfaa58091e8c1c18ca12
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-04-15 09:39:13 +0200

    flyttet til test

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomJPAAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomJPAAdapter.kt
@@ -3,7 +3,6 @@ package no.nav.tilgangsmaskin.ansatt.nom
 import jakarta.persistence.EntityManager
 import jakarta.persistence.PersistenceContext
 import no.nav.boot.conditionals.ConditionalOnGCP
-import no.nav.boot.conditionals.ConditionalOnLocalOrTest
 import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.felles.utils.extensions.TimeExtensions.toInstant
@@ -43,38 +42,6 @@ class NomJPAAdapter(val repo: NomRepository, @PersistenceContext protected val e
     }
 
     fun fnrForAnsatt(ansattId: String) = repo.ansattFødselsnummer(ansattId)?.let { BrukerId(it) }
-
-}
-
-
-@ConditionalOnLocalOrTest
-class NomJPATestAdapter(repo: NomRepository, @PersistenceContext val em: EntityManager) : NomJPAAdapter(repo, em) {
-
-
-    override fun upsert(data: NomAnsattData) =
-        with(data) {
-            upsert(ansattId, brukerId, gyldighet.start.toInstant(), gyldighet.endInclusive.toInstant())
-        }
-
-    private fun upsert(ansattId: AnsattId, ansattFnr: BrukerId, start: Instant, slutt: Instant): Long {
-        val query =
-            """
-                MERGE INTO Ansatte (navid, fnr, startdato, gyldigtil, created, updated)
-                KEY (navid)
-                VALUES (:navid, :fnr, :startdato, :gyldigtil, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)
-            """
-        entityManager.createNativeQuery(query)
-            .setParameter("navid", ansattId.verdi)
-            .setParameter("fnr", ansattFnr.verdi)
-            .setParameter("startdato", start)
-            .setParameter("gyldigtil", slutt)
-            .executeUpdate()
-        // Fetch the ID separately
-        val idQuery = "SELECT id FROM Ansatte WHERE navid = :navid"
-        return entityManager.createNativeQuery(idQuery)
-            .setParameter("navid", ansattId.verdi)
-            .singleResult as Long
-    }
 
 }
 

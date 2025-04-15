27e5144f644b6b098e415dd3893a05c2872bb68a
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-04-15 19:51:07 +0200

    navn

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/Regel.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/Regel.kt
@@ -15,7 +15,7 @@ interface Regel {
     fun sjekkGruppeRegel(predicate: () -> Boolean, ansatt: Ansatt, id: UUID) =
         if (predicate.invoke()) ansatt kanBehandle id else true
 
-    fun nektOgTellHvis(predicate: () -> Boolean, teller: HabilitetsTeller, ok: Boolean = false) =
+    fun avslåOgTellHvis(predicate: () -> Boolean, teller: HabilitetsTeller, ok: Boolean = false) =
         if (predicate.invoke()) teller.registrerOppslag(ok) else true
 
 }

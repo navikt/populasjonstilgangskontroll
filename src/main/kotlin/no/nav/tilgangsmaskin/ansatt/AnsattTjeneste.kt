28eb22eea678f63708f3035f20b41ab85329098a
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-04-23 13:15:32 +0200

    force

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/AnsattTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/AnsattTjeneste.kt
@@ -19,13 +19,13 @@ class AnsattTjeneste(
     private val log = getLogger(javaClass)
     fun ansatt(ansattId: AnsattId) =
         entra.ansatt(ansattId).let { ansatt ->
-            log.trace("Ansatt {} er medlem av følgende globale grupper {}", ansattId, accessor.globaleGrupper)
+            log.info("Ansatt {} er medlem av følgende globale grupper {}", ansattId, accessor.globaleGrupper)
             val ansattFnr = ansatte.fnrForAnsatt(ansattId)
             val ansattBruker = ansattFnr?.let {
                 runCatching {
                     brukere.utvidetFamilie(it.verdi)
                 }.getOrNull()
-               }
+            }
             Ansatt(AnsattIdentifikatorer(ansattId, ansatt.oid), ansatt.grupper, ansattBruker)
         }
 }

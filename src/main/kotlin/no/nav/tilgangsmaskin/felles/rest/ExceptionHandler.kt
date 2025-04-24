a2fb4fc3579bdfa2765c131ad01f360130e58fb8
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-04-24 15:10:36 +0200

    disanle test

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ExceptionHandler.kt
@@ -1,5 +1,6 @@
 package no.nav.tilgangsmaskin.felles.rest
 
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus.FORBIDDEN
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -8,14 +9,19 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @RestControllerAdvice
 @Order(-1)
-class ExceptionHandler : ResponseEntityExceptionHandler()
+class ExceptionHandler : ResponseEntityExceptionHandler() {
 
-@ExceptionHandler(RuntimeException::class)
-fun recoverableFeilet(e: RuntimeException) = if (e is RecoverableRestException) {
-    e.body.apply {
-        println("XXXXXX")
-        setStatus(FORBIDDEN)
+    protected val log = getLogger(javaClass)
+
+
+    @ExceptionHandler(RuntimeException::class)
+    fun recoverableFeilet(e: RuntimeException) = if (e is RecoverableRestException) {
+        e.body.apply {
+            log.warn("XXXXXX ${e.body}")
+            setStatus(FORBIDDEN)
+        }
+    } else {
+        log.warn("Uventet feil", e)
+        throw e
     }
-} else {
-    throw e
 }

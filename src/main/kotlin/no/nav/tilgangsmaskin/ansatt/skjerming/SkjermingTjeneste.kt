b8cc82f70608d549bd7fc9f09746bd332e45853c
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-04-24 14:40:54 +0200

    simplify

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingTjeneste.kt
@@ -4,10 +4,6 @@ import io.micrometer.core.annotation.Timed
 import no.nav.tilgangsmaskin.ansatt.skjerming.SkjermingConfig.Companion.SKJERMING
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.felles.CacheableRetryingOnRecoverableService
-import no.nav.tilgangsmaskin.felles.rest.RecoverableRestException
-import org.springframework.core.NestedExceptionUtils.getMostSpecificCause
-import org.springframework.retry.ExhaustedRetryException
-import org.springframework.retry.annotation.Recover
 
 @CacheableRetryingOnRecoverableService(cacheNames = [SKJERMING])
 @Timed
@@ -17,11 +13,4 @@ class SkjermingTjeneste(private val adapter: SkjermingRestClientAdapter) {
 
     fun skjerminger(brukerId: List<BrukerId>) = adapter.skjerminger(brukerId.map { it.verdi })
 
-    @Recover
-    fun fallback(e: Throwable, brukerId: BrukerId) =
-        when (e) {
-            is RecoverableRestException -> true
-            is ExhaustedRetryException -> throw getMostSpecificCause(e)
-            else -> throw e
-        }
 }
--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ExceptionHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ExceptionHandler.kt
@@ -1,9 +1,17 @@
 package no.nav.tilgangsmaskin.felles.rest
 
 import org.springframework.core.annotation.Order
+import org.springframework.http.HttpStatus.FORBIDDEN
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 
 @RestControllerAdvice
 @Order(-1)
 class ExceptionHandler : ResponseEntityExceptionHandler()
+
+@ExceptionHandler(RecoverableRestException::class)
+fun recoverableFeilet(e: RecoverableRestException) =
+    e.body.apply {
+        setStatus(FORBIDDEN)
+    }

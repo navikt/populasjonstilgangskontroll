d5726beb290160278ec2344c62d5476b5a1d7fe3
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-05-02 09:00:29 +0200

    tell nasjonal gruppemedlemsskap

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/utils/extensions/DomainExtensions.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/utils/extensions/DomainExtensions.kt
@@ -1,7 +1,5 @@
 package no.nav.tilgangsmaskin.felles.utils.extensions
 
-import no.nav.tilgangsmaskin.bruker.BrukerId
-
 
 object DomainExtensions {
     fun requireDigits(verdi: String, len: Int) {
@@ -10,9 +8,7 @@ object DomainExtensions {
     }
 
     fun String.upcase() = this.replaceFirstChar { it.uppercaseChar() }
-    fun BrukerId.maskFnr() = verdi.maskFnr()
     fun String.maskFnr() = if (length == 11) replaceRange(6, 11, "*****") else this
-    fun String.pluralize(list: List<Any>) =
-        if (list.size == 1) this else if (this.endsWith('e')) "${this}r" else "${this}er"
+    fun String.pluralize(l: List<Any>) = if (l.size == 1) this else if (this.endsWith('e')) "${this}r" else "${this}er"
 
 }

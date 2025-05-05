341b70eebaa5ce9ea23cf1ef6bdca29805979d36
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-05-05 10:12:56 +0200

    utvidet familie mer lesbar

--- src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PDLTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PDLTjeneste.kt
@@ -2,7 +2,6 @@ package no.nav.tilgangsmaskin.bruker.pdl
 
 import io.micrometer.core.annotation.Timed
 import no.nav.tilgangsmaskin.bruker.BrukerId
-import no.nav.tilgangsmaskin.bruker.Familie
 import no.nav.tilgangsmaskin.bruker.Familie.FamilieMedlem
 import no.nav.tilgangsmaskin.bruker.Familie.FamilieMedlem.FamilieRelasjon.SØSKEN
 import no.nav.tilgangsmaskin.bruker.pdl.PdlConfig.Companion.PDL
@@ -15,14 +14,13 @@ import no.nav.tilgangsmaskin.felles.CacheableRetryingOnRecoverableService
 class PDLTjeneste(private val adapter: PdlRestClientAdapter, private val graphQL: PdlSyncGraphQLClientAdapter) {
 
     fun utvidetFamile(id: String) =
-        with(tilPerson(adapter.person(id))) {
-            copy(familie = Familie(foreldre, barn, søsken(this), partnere(id)))
+        with(nærmesteFamilie(id)) {
+            copy(familie = familie.copy(søsken = søsken(foreldre, brukerId), partnere = partnere(id)))
         }
 
     fun nærmesteFamilie(id: String) = tilPerson(adapter.person(id))
 
-    fun personer(brukerIds: Set<String>) =
-        adapter.personer(brukerIds).map { tilPerson(it.value) }
+    fun personer(brukerIds: Set<String>) = adapter.personer(brukerIds).map { tilPerson(it.value) }
 
     private fun partnere(id: String) =
         graphQL.sivilstand(id).sivilstand
@@ -32,11 +30,11 @@ class PDLTjeneste(private val adapter: PdlRestClientAdapter, private val graphQL
                 }
             }.toSet()
 
-    private fun søsken(person: Person) =
-        adapter.personer(person.foreldre.map { it.brukerId.verdi }.toSet())
+    private fun søsken(foreldre: Set<FamilieMedlem>, ansatt: BrukerId) =
+        adapter.personer(foreldre.map { it.brukerId.verdi }.toSet())
             .asSequence()
             .flatMap { tilPerson(it.value).barn }
-            .filter { it.brukerId != person.brukerId }
+            .filter { it.brukerId != ansatt }
             .map { FamilieMedlem(it.brukerId, SØSKEN) }
             .toSet()
 

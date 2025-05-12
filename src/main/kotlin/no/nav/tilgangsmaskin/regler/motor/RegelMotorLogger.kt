f9c769c9bb176601f4881702f0ed2a5c76182374
Jan-Olav Kjørås Eide <jan-olav.eide@nav.no>
2025-05-12 08:54:11 +0200

    team logs #1

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/RegelMotorLogger.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/RegelMotorLogger.kt
@@ -4,17 +4,19 @@ import io.micrometer.core.annotation.Counted
 import no.nav.tilgangsmaskin.ansatt.Ansatt
 import no.nav.tilgangsmaskin.bruker.Bruker
 import org.slf4j.LoggerFactory.getLogger
+import org.slf4j.MarkerFactory
 import org.springframework.stereotype.Component
 
 @Component
 @Counted
 class RegelMotorLogger {
 
-    private val secureLogger = getLogger("secureLog")
     private val log = getLogger(javaClass)
     fun avvist(ansatt: Ansatt, bruker: Bruker, regel: Regel) {
         log.warn("Tilgang avvist av regel '${regel.kortNavn}'. (${regel.begrunnelse}) for ${ansatt.ansattId}")
-        secureLogger.info("Tilgang til ${bruker.brukerId.verdi} avvist av regel '${regel.kortNavn}' for ${ansatt.ansattId}")
+        log.info(
+                SECURE,
+                "Tilgang til ${bruker.brukerId.verdi} avvist av regel '${regel.kortNavn}' for ${ansatt.ansattId}")
     }
 
     fun ok(ansatt: Ansatt, regelSett: RegelSett) {
@@ -23,5 +25,9 @@ class RegelMotorLogger {
 
     fun evaluerer(ansatt: Ansatt, bruker: Bruker, regel: Regel) {
         log.trace("Evaluerer regel: '${regel.kortNavn}' for ${ansatt.ansattId}  og ${bruker.brukerId}")
+    }
+
+    companion object {
+        private val SECURE = MarkerFactory.getMarker("TEAM_LOGS")
     }
 }

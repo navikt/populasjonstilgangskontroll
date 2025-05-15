fe0ead9b60b816c65e3030775618ca3e99794b57
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-15 12:35:58 +0200

    dust

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/RegelMotorLogger.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/RegelMotorLogger.kt
@@ -14,14 +14,14 @@ class RegelMotorLogger {
     private val secureLog = getLogger("secureLog")
     private val log = getLogger(javaClass)
     fun avvist(ansatt: Ansatt, bruker: Bruker, regel: Regel) {
+        MDC.put(BESLUTNING,regel.kode)
         log.warn("Tilgang avvist av regel '${regel.kortNavn}'. (${regel.begrunnelse}) for ${ansatt.ansattId}")
         secureLog.info("Tilgang til ${bruker.brukerId.verdi} avvist av regel '${regel.kortNavn}' for ${ansatt.ansattId}")
-        MDC.put(BESLUTNING,regel.kode)
     }
 
     fun ok(ansatt: Ansatt, regelSett: RegelSett) {
-        log.info("${regelSett.beskrivelse} ga tilgang for ${ansatt.ansattId}")
         MDC.put(BESLUTNING,"TILGANG_OK")
+        log.info("${regelSett.beskrivelse} ga tilgang for ${ansatt.ansattId}")
     }
 
     fun evaluerer(ansatt: Ansatt, bruker: Bruker, regel: Regel) {

9d75ae19e89dacc178abd6afe8a0ed593277c3c3
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-16 14:01:11 +0200

    dedup mer

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/CaffeineConfiguration.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/CaffeineConfiguration.kt
@@ -17,7 +17,7 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.concurrent.TimeUnit.HOURS
 
-@Configuration(proxyBeanMethods = true)
+@Configuration
 @EnableCaching
 @ConditionalOnGCP
 class CaffeineConfiguration(private vararg val cfgs: CachableRestConfig) : CachingConfigurer {

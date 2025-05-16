51c844f18653637cfae7de1bfec1751729e13b74
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-16 11:49:32 +0200

    log removals

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/CaffeineConfiguration.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/CaffeineConfiguration.kt
@@ -3,6 +3,10 @@ package no.nav.tilgangsmaskin.felles.rest.cache
 import com.github.benmanes.caffeine.cache.Caffeine
 import com.github.benmanes.caffeine.cache.RemovalCause
 import no.nav.boot.conditionals.ConditionalOnGCP
+import no.nav.tilgangsmaskin.ansatt.entra.EntraConfig.Companion.GRAPH
+import no.nav.tilgangsmaskin.ansatt.skjerming.SkjermingConfig.Companion.SKJERMING
+import no.nav.tilgangsmaskin.bruker.pdl.PdlConfig.Companion.PDL
+import no.nav.tilgangsmaskin.regler.overstyring.OverstyringEntity.Companion.OVERSTYRING
 import org.slf4j.LoggerFactory
 import org.springframework.cache.annotation.CachingConfigurer
 import org.springframework.cache.annotation.EnableCaching
@@ -18,12 +22,15 @@ class CaffeineConfiguration : CachingConfigurer {
     private val log = LoggerFactory.getLogger(javaClass)
 
 
-     private fun caffeine() = Caffeine.newBuilder().recordStats()
+     private fun caffeine() = Caffeine.newBuilder().recordStats().removalListener {
+        key: Any?, _: Any?, cause: RemovalCause ->
+         log.info("Cache entry removed: key={}, cause={}", key, cause)
+     }
 
 
     @Bean
      override fun cacheManager() =
-        CaffeineCacheManager("pdl","graph","skjerming","overstyring").apply {
+        CaffeineCacheManager(PDL,GRAPH,SKJERMING,OVERSTYRING).apply {
             setCaffeine(caffeine())
         }
 

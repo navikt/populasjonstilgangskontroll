4c736bd5b0ca2cb7117fc79c22bf7f5bb340ea6c
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-17 21:35:50 +0200

    bruk kode

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/RegelMotorLogger.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/RegelMotorLogger.kt
@@ -17,7 +17,7 @@ class RegelMotorLogger(private val teller: AvvisningTeller) {
         MDC.put(BESLUTNING,regel.kode)
         log.warn("Tilgang avvist av regel '${regel.kortNavn}'. (${regel.begrunnelse}) for ${ansatt.ansattId}")
         secureLog.info("Tilgang til ${bruker.brukerId.verdi} avvist av regel '${regel.kortNavn}' for ${ansatt.ansattId} fra ${MDC.get(CONSUMER_ID)}")
-        teller.tell(Tags.of("regel", regel.kode))
+        teller.tell(Tags.of("kode", regel.kode))
         MDC.remove(BESLUTNING)
     }
 

5c1d3b662983edae06364d44349ddb609fc67977
Jan Olav Eide <jan.olav.eide@gmail.com>
2025-05-19 14:06:42 +0200

    Update EntraOidResolver.kt

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entra/EntraOidResolver.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entra/EntraOidResolver.kt
@@ -5,8 +5,8 @@ import no.nav.tilgangsmaskin.tilgang.Token
 import org.springframework.stereotype.Component
 
 @Component
-class EntraOidResolver(private val adapter: EntraRestClientAdapter, private val accessor: Token) {
-    fun oidForAnsatt(ansattId: AnsattId) = accessor.oid ?: oidFraEntra(ansattId.verdi)
+class EntraOidResolver(private val adapter: EntraRestClientAdapter, private val token: Token) {
+    fun oidForAnsatt(ansattId: AnsattId) = token.oid ?: oidFraEntra(ansattId.verdi)
 
     private fun oidFraEntra(ansattId: String) = adapter.oidFraEntra(ansattId)
 }

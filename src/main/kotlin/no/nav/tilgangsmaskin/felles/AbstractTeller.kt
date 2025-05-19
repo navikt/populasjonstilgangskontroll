9a2261afa8f6576fc40e15cf0866a1c2dc66446e
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-19 10:20:03 +0200

    bytt navn på metrikken

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/AbstractTeller.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/AbstractTeller.kt
@@ -4,7 +4,6 @@ import io.micrometer.core.instrument.Counter
 import io.micrometer.core.instrument.MeterRegistry
 import io.micrometer.core.instrument.Tags
 import no.nav.tilgangsmaskin.tilgang.Token
-import org.slf4j.LoggerFactory.getLogger
 
 abstract class AbstractTeller(
         private val registry: MeterRegistry,
@@ -12,11 +11,9 @@ abstract class AbstractTeller(
         private val navn: String,
         private val beskrivelse: String) {
 
-    private val log = getLogger(javaClass)
 
     open fun tell(tags: Tags) =
         with(tags.and("system", token.system)) {
-           // log.info("Registrerer teller med navn: {} og tags: {}", navn, this)
             Counter.builder(navn)
                 .description(beskrivelse)
                 .tags(this)

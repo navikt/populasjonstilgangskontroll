8dfad8095fac1ebf3c2535fdb5e3ac06e9294ee6
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-20 10:39:28 +0200

    bulk med CCF i TilgangController

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/IdOgType.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/IdOgType.kt
@@ -1,9 +1,19 @@
 package no.nav.tilgangsmaskin.regler.motor
 
+import io.swagger.v3.oas.annotations.media.Schema
 import no.nav.tilgangsmaskin.regler.motor.RegelSett.RegelType
 import no.nav.tilgangsmaskin.regler.motor.RegelSett.RegelType.KOMPLETT_REGELTYPE
 
 
+
+@Schema(example = """
+[
+  {
+    "brukerId": "string",
+    "type": "KOMPLETT_REGELTYPE"
+  }
+]
+""")
 data class IdOgType(val brukerId: String, val type: RegelType = KOMPLETT_REGELTYPE)
 
 

3b3f5bd4e71c6b6dbfddac3dcf43b476874aa382
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-21 14:29:21 +0200

    logisk feil i erOBO

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/Token.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/Token.kt
@@ -22,7 +22,8 @@ class Token(private val contextHolder: TokenValidationContextHolder) {
     private fun claimSet() = runCatching { contextHolder.getTokenValidationContext().getClaims(AAD_ISSUER) }.getOrNull()
     val systemNavn get() = system.split(":").lastOrNull() ?: "N/A"
     val systemAndNs get() = runCatching { system.split(":").drop(1).joinToString(separator = ":") }.getOrElse { systemNavn }
-    val erObo get() = APP.equals(claimSet()?.getStringClaim(IDTYP))
+    val erObo get() = APP != claimSet()?.getStringClaim(IDTYP)
+    val erCC get() = !erObo
     companion object {
         const val AAD_ISSUER: String = "azuread"
         private const val APP = "app"

e92a890f4ceb21198cc9d8df2f49b25926c474f5
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-22 20:03:57 +0200

    expression

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/AnsattTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/AnsattTjeneste.kt
@@ -18,39 +18,40 @@ import no.nav.tilgangsmaskin.ansatt.GlobalGruppe.NASJONAL
 @Service
 @Timed
 @ConditionalOnGCP
-class AnsattTjeneste(private val entra: Entra, private val ansatte: Nom,
+class AnsattTjeneste(private val entra: Entra,
+                     private val ansatte: Nom,
                      private val brukere: BrukerTjeneste,
-                     private val token: Token, private val teller: NasjonalGruppeTeller) {
+                     private val token: Token,
+                     private val teller: NasjonalGruppeTeller) {
     private val log = getLogger(javaClass)
 
-    fun ansatt(ansattId: AnsattId) : Ansatt {
+    fun ansatt(ansattId: AnsattId) =
         if (token.erObo) {
             val grupper = token.globaleGrupper()
             if (grupper.girNasjonalTilgang()) {
-                log.info("OBO-flow: $ansattId har nasjonal tilgang, slår *ikke* opp GEO-grupper i Entra")
-                tell( true)
-                return ansatt(ansattId, grupper)
+                ansatt(ansattId, grupper).also {
+                    log.info("OBO-flow: $ansattId har nasjonal tilgang, slo *ikke* opp GEO-grupper i Entra")
+                    tell( true)
+                }
             }
             else {
-                log.info("OBO-flow: $ansattId har ikke nasjonal tilgang, slår opp GEO-grupper i Entra")
-                tell( false)
-                return ansatt(ansattId, grupper + entra.geoGrupper(ansattId))
+                ansatt(ansattId, grupper + entra.geoGrupper(ansattId)).also {
+                    log.info("OBO-flow: $ansattId har ikke nasjonal tilgang, slo opp GEO-grupper i Entra")
+                    tell( false)
+                }
             }
         }
         else {
-            log.info("CC-flow: slår opp globale og GEO-grupper i Entra")
-            return ansatt(ansattId, entra.geoOgGlobaleGrupper(ansattId)).also {
+            ansatt(ansattId, entra.geoOgGlobaleGrupper(ansattId)).also {
+                log.info("CC-flow: $ansattId slo opp globale og GEO-grupper i Entra")
                 tell (it erMedlemAv NASJONAL)
             }
         }
-    }
 
 
     private fun ansatt(ansattId: AnsattId, grupper: Set<EntraGruppe>): Ansatt {
         val ansattBruker = ansatte.fnrForAnsatt(ansattId)?.let {
-            runCatching {
-                brukere.utvidetFamilie(it.verdi)
-            }.getOrNull()
+            runCatching { brukere.utvidetFamilie(it.verdi) }.getOrNull()
         }
         return Ansatt(ansattId, ansattBruker, grupper)
     }

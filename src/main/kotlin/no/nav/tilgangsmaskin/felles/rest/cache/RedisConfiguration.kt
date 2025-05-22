9b5873ab146d6350bc9d4268046a4463552bbcad
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-22 21:34:48 +0200

    konfigurer cachene fra cfg

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/RedisConfiguration.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/RedisConfiguration.kt
@@ -2,14 +2,11 @@ package no.nav.tilgangsmaskin.felles.rest.cache
 
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY
 import com.fasterxml.jackson.core.JsonParser.Feature.INCLUDE_SOURCE_IN_LOCATION
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.ObjectMapper.DefaultTyping.EVERYTHING
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import no.nav.boot.conditionals.ConditionalOnDev
-import no.nav.tilgangsmaskin.ansatt.entra.EntraConfig.Companion.GRAPH
-import no.nav.tilgangsmaskin.ansatt.skjerming.SkjermingConfig.Companion.SKJERMING
-import no.nav.tilgangsmaskin.bruker.pdl.PdlConfig.Companion.PDL
+import no.nav.tilgangsmaskin.felles.rest.CachableRestConfig
 import org.springframework.boot.actuate.health.Health
 import org.springframework.boot.actuate.health.HealthIndicator
 import org.springframework.cache.annotation.CachingConfigurer
@@ -17,20 +14,20 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.interceptor.KeyGenerator
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
 import org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig
 import org.springframework.data.redis.cache.RedisCacheManager
 import org.springframework.data.redis.cache.RedisCacheWriter.nonLockingRedisCacheWriter
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.core.RedisConnectionUtils.getConnection
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
-import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
-
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer
 @Configuration
 @EnableCaching
 @ConditionalOnDev
-class RedisConfiguration(private val cf: RedisConnectionFactory) : CachingConfigurer {
+class RedisConfiguration(private val cf: RedisConnectionFactory,private vararg val cfgs: CachableRestConfig) : CachingConfigurer {
 
 
         @Bean
@@ -39,34 +36,23 @@ class RedisConfiguration(private val cf: RedisConnectionFactory) : CachingConfig
                 getConnection(cf).use { connection ->
                     runCatching {
                         if (connection.ping().equals("PONG", ignoreCase = true)) {
-                            Health.up().withDetail("Redis", "Connection is healthy").build()
+                            Health.up().withDetail("Redis", "Frisk og rask").build()
                         } else {
-                            Health.down().withDetail("Redis", "Ping failed").build()
+                            Health.down().withDetail("Redis", "Ikke helt i slag").build()
                         }
                     }.fold(
                         onSuccess = { it },
-                        onFailure = { Health.down(it).withDetail("Redis", "Connection failed").build() }
+                        onFailure = { Health.down(it).withDetail("Redis", "Ingen forbindelse").build() }
                     )
                 }
         }
 
     @Bean
-    override fun cacheManager(): RedisCacheManager {
-        val keySerializer = StringRedisSerializer()
-        val valueSerializer = GenericJackson2JsonRedisSerializer(mapper)
-        val customCacheConfig = defaultCacheConfig()
-          .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(keySerializer))
-            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(valueSerializer))
-            .entryTtl(Duration.ofHours(24)) // Example: 10 min TTL
-        val cacheConfigs = mapOf(PDL to customCacheConfig, SKJERMING to customCacheConfig, GRAPH to customCacheConfig)
-
-        return RedisCacheManager.builder(nonLockingRedisCacheWriter(cf))
-            .withInitialCacheConfigurations(cacheConfigs)
+    override fun cacheManager(): RedisCacheManager =
+        RedisCacheManager.builder(nonLockingRedisCacheWriter(cf))
+            .withInitialCacheConfigurations(cfgs.associate { it.navn to cacheConfig(it) })
             .enableStatistics()
             .build()
-    }
-
-
 
     override fun keyGenerator() = KeyGenerator { target, method, params ->
         buildString {
@@ -76,14 +62,20 @@ class RedisConfiguration(private val cf: RedisConnectionFactory) : CachingConfig
         }
     }
 
+    private fun cacheConfig(cfg: CachableRestConfig) =
+        defaultCacheConfig()
+            .entryTtl(Duration.ofHours(cfg.expireHours))
+            .serializeKeysWith(fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(fromSerializer(GenericJackson2JsonRedisSerializer(mapper)))
+
     private val mapper =
-        jacksonObjectMapper().registerModule(JavaTimeModule())
-       // m.copy()
+        jacksonObjectMapper()
+            .registerModule(JavaTimeModule())
             .apply {
                 configure(INCLUDE_SOURCE_IN_LOCATION, true)
                 activateDefaultTyping(polymorphicTypeValidator,
-            EVERYTHING,
-            PROPERTY
-        )
-    }
+                    EVERYTHING,
+                    PROPERTY
+                )
+            }
 }

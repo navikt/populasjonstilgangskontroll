9d1a3fb086349e30693b6c4d87cd1658e3081ca9
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-22 18:35:32 +0200

    @JsonValue

--- src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PdlGeografiskTilknytning.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PdlGeografiskTilknytning.kt
@@ -12,10 +12,10 @@ data class PdlGeografiskTilknytning(
     enum class GTType { BYDEL, KOMMUNE, UDEFINERT, UTLAND }
 
     //@JvmInline
-    data class GTKommune(val verdi: String)
+    data class GTKommune(@JsonValue val verdi: String)
 
     //@JvmInline
-    data class GTBydel(val verdi: String)
+    data class GTBydel(@JsonValue val verdi: String)
 
    // @JvmInline
    data class GTLand(@JsonValue val verdi: String)

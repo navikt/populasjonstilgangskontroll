de581c0672e464f37d3d82d50d643011e70bbf05
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-23 11:36:45 +0200

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/AnsattGruppeResolver.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/AnsattGruppeResolver.kt
@@ -1,8 +1,8 @@
 package no.nav.tilgangsmaskin.ansatt
 
+import no.nav.tilgangsmaskin.ansatt.GlobalGruppe.*
 import no.nav.tilgangsmaskin.ansatt.GlobalGruppe.Companion.globaleGrupper
 import no.nav.tilgangsmaskin.ansatt.entra.Entra
-import no.nav.tilgangsmaskin.ansatt.entra.EntraGruppe
 import no.nav.tilgangsmaskin.tilgang.Token
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
@@ -12,21 +12,20 @@ class AnsattGruppeResolver(private val entra: Entra, private val token: Token) {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
-    fun grupperForAnsatt(ansattId: AnsattId) : Set<EntraGruppe> {
+    fun grupperForAnsatt(ansattId: AnsattId) =
         if (token.erObo) {
             val grupper = token.globaleGrupper()
-            if (grupper.any { it.id == GlobalGruppe.NASJONAL.id }) {
+            if (grupper.any { it.id == NASJONAL.id }) {
                 log.info("OBO-flow: $ansattId har nasjonal tilgang, slår *ikke* opp GEO-grupper i Entra")
-                return grupper
+                grupper
             }
             else {
                 log.info("OBO-flow: $ansattId har ikke nasjonal tilgang, slår opp GEO-grupper i Entra")
-                return grupper + entra.geoGrupper(ansattId)
+                grupper + entra.geoGrupper(ansattId)
             }
         }
         else {
             log.info("CC-flow: $ansattId slår opp globale og GEO-grupper i Entra")
-            return entra.geoOgGlobaleGrupper(ansattId)
+            entra.geoOgGlobaleGrupper(ansattId)
         }
-    }
 }

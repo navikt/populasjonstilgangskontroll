3035dff6095d3735a369a37ca4ac7112a5e265ec
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-25 12:21:06 +0200

    log

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingTjeneste.kt
@@ -4,8 +4,10 @@ import io.micrometer.core.annotation.Timed
 import no.nav.tilgangsmaskin.ansatt.skjerming.SkjermingConfig.Companion.SKJERMING
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.felles.CacheableRetryingOnRecoverableService
+import org.springframework.stereotype.Service
 
 //@CacheableRetryingOnRecoverableService(cacheNames = [SKJERMING])
+@Service
 @Timed
 class SkjermingTjeneste(private val adapter: SkjermingRestClientAdapter) {
 

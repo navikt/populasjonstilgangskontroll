8188c25041a0a2732050462a3deebc36fc40d215
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-27 14:15:35 +0200

    rollbak to verify

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValkeyConfiguration.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValkeyConfiguration.kt
@@ -59,9 +59,11 @@ class ValkeyConfiguration(private val cf: RedisConnectionFactory, private vararg
             append(method.name)
             append(":")
             params.forEach {
-                if (it is BrukerId)
-                append(it.verdi)
-                else append(it)
+            //    if (it is BrukerId)
+             //   append(it.verdi)
+                // else
+                    append(it)
+
             }
         }
     }

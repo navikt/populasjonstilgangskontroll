8a6953c125a403efb97980fa377c4695319ac8e3
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-27 09:30:25 +0200

    log skjerming status

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingRestClientAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingRestClientAdapter.kt
@@ -5,6 +5,7 @@ import no.nav.tilgangsmaskin.ansatt.skjerming.SkjermingConfig.Companion.IDENTER
 import no.nav.tilgangsmaskin.ansatt.skjerming.SkjermingConfig.Companion.SKJERMING
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.felles.rest.AbstractRestClientAdapter
+import no.nav.tilgangsmaskin.felles.utils.extensions.DomainExtensions.maskFnr
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
 import org.springframework.web.client.RestClient
@@ -13,7 +14,9 @@ import org.springframework.web.client.RestClient
 class SkjermingRestClientAdapter(@Qualifier(SKJERMING) restClient: RestClient, private val cf: SkjermingConfig) :
     AbstractRestClientAdapter(restClient, cf) {
 
-    fun skjerming(ident: String) = post<Boolean>(cf.skjermingUri, mapOf(IDENT to ident))
+    fun skjerming(ident: String) = post<Boolean>(cf.skjermingUri, mapOf(IDENT to ident)).also {
+        log.info("${ident.maskFnr()} skjerming status: $it")
+    }
 
     fun skjerminger(identer: Set<String>): Map<BrukerId, Boolean> =
         post<Map<String, Boolean>>(cf.skjermingerUri, mapOf(IDENTER to identer))

5862a22b377d7944cade5f2ded944be7b4c580f3
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-05-30 12:05:51 +0200

    bulk redesign

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/RegelTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/RegelTjeneste.kt
@@ -1,17 +1,25 @@
 package no.nav.tilgangsmaskin.tilgang
 
 import io.micrometer.core.annotation.Timed
+import net.minidev.json.annotate.JsonIgnore
+import no.nav.tilgangsmaskin.ansatt.Ansatt
 import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.ansatt.AnsattTjeneste
+import no.nav.tilgangsmaskin.bruker.Bruker
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.bruker.BrukerTjeneste
 import no.nav.tilgangsmaskin.felles.utils.extensions.DomainExtensions.maskFnr
 import no.nav.tilgangsmaskin.regler.motor.IdOgType
+import no.nav.tilgangsmaskin.regler.motor.RegelException
 import no.nav.tilgangsmaskin.regler.motor.RegelMotor
 import no.nav.tilgangsmaskin.regler.motor.RegelMotor.BulkRegelResult.*
+import no.nav.tilgangsmaskin.regler.motor.RegelSett.RegelType
 import no.nav.tilgangsmaskin.regler.motor.RegelSett.RegelType.KOMPLETT_REGELTYPE
 import no.nav.tilgangsmaskin.regler.overstyring.OverstyringTjeneste
 import org.slf4j.LoggerFactory.getLogger
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatus.ACCEPTED
+import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import org.springframework.stereotype.Service
 import kotlin.time.measureTime
 
@@ -31,7 +39,10 @@ class RegelTjeneste(
             runCatching {
                 motor.kompletteRegler(ansatte.ansatt(ansattId), bruker)
             }.getOrElse {
-                overstyring.sjekk(ansattId, it)
+                if (overstyring.erOverstyrt(ansattId,bruker.brukerId)) {
+                    Unit
+                }
+                else throw it
             }
         }
         log.info("Tid brukt på komplett regelsett for $ansattId og ${brukerId.maskFnr()}: ${elapsedTime.inWholeMilliseconds}ms")
@@ -41,33 +52,20 @@ class RegelTjeneste(
         motor.kjerneregler(ansatte.ansatt(ansattId), brukere.utvidetFamilie(brukerId))
 
     fun bulkRegler(ansattId: AnsattId, idOgType: Set<IdOgType>): List<Pair<BrukerId, Any>> {
-        lateinit var resultater: List<Pair<BrukerId,Any>>
-        val elapsedTime = measureTime {
-            log.info("Eksekverer bulk regler for $ansattId og ${idOgType.map { it.brukerId }.map { it.maskFnr() }}")
-            resultater = motor.bulkRegler(ansatte.ansatt(ansattId), idOgType.brukerIdOgType()).map { spec ->
-                when (spec) {
-                    is Success -> {
-                        log.info("Regel for ${spec.brukerId} er OK")
-                        Pair(spec.brukerId,204)
-                    }
-                    is RegelFailure ->
-                        if (overstyring.erOverstyrt(ansattId, spec.brukerId)) {
-                            log.info("Regel for ${spec.brukerId} er overstyrt")
-                            Pair(spec.brukerId,204)
-                        } else {
-                            log.warn("Regel for ${spec.brukerId} er avvist med ${spec.exception.message}")
-                            Pair(spec.brukerId,spec.exception.body)
-                          //  Pair(spec.brukerId, spec.statusCode.value())
-                        }
-                    is InternalError ->{
-                        log.error("Regel for ${spec.brukerId} feilet med ${spec.exception.message}")
-                        Pair(spec.brukerId,500)
-                    }
-                }
+        val resultater = motor.bulkRegler(ansatte.ansatt(ansattId), idOgType.brukerIdOgType()).map {
+            when (it) {
+                is Success -> it.brukerId to "OK"
+                is RegelFailure -> if (overstyring.erOverstyrt(ansattId, it.brukerId))
+                    it.brukerId to "OK"
+                      else it.brukerId to "403"
+
+                is InternalError -> it.brukerId to "500"
             }
         }
-        log.info("Tid brukt på bulk regler for $ansattId og ${idOgType.map { it.brukerId }.map { it.maskFnr() }}: ${elapsedTime.inWholeMilliseconds}ms")
-        return resultater
+        val notFound = idOgType.map { it.brukerId }
+            .filterNot { brukerId -> resultater.any { it.first.verdi == brukerId } }
+            .map { BrukerId(it) to "404" }
+        return resultater + notFound
     }
 
     private fun Set<IdOgType>.brukerIdOgType() =

ede2f117baf1e5fe434f668ef0beadf402286272
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-01 22:01:14 +0200

    constants

--- src/test/kotlin/no/nav/tilgangsmaskin/ansatt/EntraUrl.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/ansatt/EntraUrl.kt
@@ -1,6 +1,7 @@
 package no.nav.tilgangsmaskin.ansatt
 
 import no.nav.tilgangsmaskin.TestApp
+import no.nav.tilgangsmaskin.ansatt.graph.EntraConfig.Companion.GEO_PREFIX
 import no.nav.tilgangsmaskin.ansatt.graph.EntraConfig
 import no.nav.tilgangsmaskin.regler.motor.GlobaleGrupperConfig
 import org.junit.jupiter.api.Assertions.assertEquals
@@ -26,7 +27,7 @@ class EntraUrl {
     @DisplayName("UUIDene til gruppene kommer i rett formatering")
     fun `GrupperCcfURI should correctly format PARAM_NAME_FILTER`() {
         val globaleGrupper = GlobalGruppe.entries.map { env.getProperty(it.property) }.joinToString(",") { "'$it'" }
-        val expectedFilter = "id in($globaleGrupper) or startswith(displayName, '0000-GA-GEO')"
+        val expectedFilter = "id in($globaleGrupper) or $GEO_PREFIX"
         val actualFilter = EntraConfig(baseUri = URI("https://example.com")).grupperURI("Z999999", true).query.substringAfter("\$filter=").substringBefore("&")
         assertEquals(expectedFilter, actualFilter, "PARAM_NAME_FILTER is not formatted correctly")
     }

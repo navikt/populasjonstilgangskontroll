49312fee4e0e968daa3a236cc2a48fbae45c400d
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-01 17:35:02 +0200

    reorg idtype check

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/Token.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/Token.kt
@@ -23,7 +23,7 @@ class Token(private val contextHolder: TokenValidationContextHolder) {
     val systemNavn get() = system.split(":").lastOrNull() ?: "N/A"
     val systemAndNs get() = runCatching { system.split(":").drop(1).joinToString(separator = ":") }.getOrElse { systemNavn }
     val erCC get() = idType == APP
-    val erObo get() = idType != APP
+    val erObo get() = idType?.let { it != APP } ?: false
     val idType get() = claimSet()?.getStringClaim(IDTYP)
     companion object {
         const val AAD_ISSUER: String = "azuread"

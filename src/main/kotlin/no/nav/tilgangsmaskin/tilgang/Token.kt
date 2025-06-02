640a76cd671f33895aa58a59e13778fb0c2f2189
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-02 10:16:05 +0200

    prod

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/Token.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/Token.kt
@@ -19,12 +19,11 @@ class Token(private val contextHolder: TokenValidationContextHolder) {
     val system get() = stringClaim(AZP_NAME)  ?: "N/A"
     val oid get() = stringClaim(OID)?.let { UUID.fromString(it) }
     val ansattId get() = stringClaim(NAVIDENT)?.let { AnsattId(it) }
-    private val idType get() = stringClaim(IDTYP)
     private fun stringClaim(name: String) = claimSet()?.getStringClaim(name)
     private fun claimSet() = runCatching { contextHolder.getTokenValidationContext().getClaims(AAD_ISSUER) }.getOrNull()
     val systemNavn get() = system.split(":").lastOrNull() ?: "N/A"
     val systemAndNs get() = runCatching { system.split(":").drop(1).joinToString(separator = ":") }.getOrElse { systemNavn }
-    val erCC get() = idType == APP
+    val erCC get() = stringClaim(IDTYP) == APP
     val erObo get()  = oid != null
     companion object {
         const val AAD_ISSUER: String = "azuread"

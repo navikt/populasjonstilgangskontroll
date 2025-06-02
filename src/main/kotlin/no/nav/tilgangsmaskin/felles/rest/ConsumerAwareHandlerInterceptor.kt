8d12bc8783efd57a73cc6a08a7eb6a0c97502c26
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-02 10:40:41 +0200

    log some headers

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ConsumerAwareHandlerInterceptor.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ConsumerAwareHandlerInterceptor.kt
@@ -5,20 +5,22 @@ import io.micrometer.core.instrument.Tags
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import no.nav.tilgangsmaskin.tilgang.Token
+import org.slf4j.LoggerFactory.getLogger
 import org.slf4j.MDC
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.HandlerInterceptor
 
 @Component
-class ConsumerAwareHandlerInterceptor(private val token: Token, private val registry: MeterRegistry) : HandlerInterceptor {
+class ConsumerAwareHandlerInterceptor(private val token: Token) : HandlerInterceptor {
+    private val log = getLogger(javaClass)
+
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         MDC.put(USER_ID, token.ansattId?.verdi ?: "N/A")
         MDC.put(CONSUMER_ID, token.systemAndNs)
-       // registry.counter(METRIC, Tags.of("remote_host", request.getHeader("X-Forwarded-Host"))).increment()
+        log.info("X-forwarded-host: ${request.getHeader("X-Forwarded-Host")}, X-forwarded-for: ${request.getHeader("X-Forwarded-For")}")
         return true
     }
     companion object  {
-      //  private const val METRIC = "http_requests_by_remote_host"
         const val CONSUMER_ID = "consumerId"
         const val USER_ID = "userId"
     }

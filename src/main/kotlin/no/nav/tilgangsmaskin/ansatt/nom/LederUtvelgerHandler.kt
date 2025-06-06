8160420fe89fc744e984f6dfd4a4e3ff7a250f8b
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-06 10:27:34 +0200

    ikke sjekk leader før applikasjon er klar

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/LederUtvelgerHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/LederUtvelgerHandler.kt
@@ -1,5 +1,6 @@
 package no.nav.tilgangsmaskin.ansatt.nom
 
+import no.nav.tilgangsmaskin.ansatt.nom.LederUtvelgerHandler.LeaderChangedEvent
 import java.net.URI
 import java.time.LocalDateTime
 import org.springframework.beans.factory.annotation.Value
@@ -8,7 +9,9 @@ import org.springframework.context.ApplicationEvent
 import org.springframework.context.ApplicationEventPublisher
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
+import org.springframework.stereotype.Service
 import org.springframework.web.reactive.function.client.WebClient.Builder
+import java.net.InetAddress
 
 @Component
 class LederUtvelgerHandler(private val builder: Builder,
@@ -30,4 +33,16 @@ class LederUtvelgerHandler(private val builder: Builder,
 
     data class LederUtvelgerRespons(val name: String, val last_update: LocalDateTime)
     class LeaderChangedEvent(source: Any, val leder: String) : ApplicationEvent(source)
+}
+
+@Component
+class LederUtvelger  {
+
+    private val hostname = InetAddress.getLocalHost().hostName
+    var erLeder: Boolean = false
+
+    @EventListener(LeaderChangedEvent::class)
+    fun onApplicationEvent(event: LeaderChangedEvent) {
+        erLeder = event.leder == hostname
+    }
 }

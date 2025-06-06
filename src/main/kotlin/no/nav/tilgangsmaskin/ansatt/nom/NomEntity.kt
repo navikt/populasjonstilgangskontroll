4ea81fe822eacd9ce362d4af0b0435415f744855
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-06 11:30:19 +0200

    trenger ikke en listener her

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomEntity.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomEntity.kt
@@ -12,7 +12,6 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener
         name = "ansatte",
         indexes = [Index(name = "idx_gyldig", columnList = "gyldigtil")],
         uniqueConstraints = [UniqueConstraint(name = "uc_ansattentity_navid", columnNames = ["navid"])])
-@EntityListeners( NomEntityListener::class)
 class NomEntity(
         @Column(length = 7, nullable = false) val navid: String,
         @Column(length = 11, nullable = false) var fnr: String,

064256828b2205d8fc23884892786b49534953ec
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-06 10:24:42 +0200

    ikke sjekk leader før applikasjon er klar

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/LederUtvelger.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/LederUtvelger.kt
@@ -2,17 +2,18 @@ package no.nav.tilgangsmaskin.ansatt.nom
 
 import java.net.InetAddress
 import no.nav.tilgangsmaskin.ansatt.nom.LederUtvelgerHandler.LeaderChangedEvent
-import org.springframework.context.ApplicationListener
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Service
 
 
 @Service
-class LederUtvelger : ApplicationListener<LeaderChangedEvent> {
+class LederUtvelger  {
 
     private val hostname = InetAddress.getLocalHost().hostName
     var erLeder: Boolean = false
 
-    override fun onApplicationEvent(event: LeaderChangedEvent) {
+    @EventListener(LeaderChangedEvent::class)
+    fun onApplicationEvent(event: LeaderChangedEvent) {
         erLeder = event.leder == hostname
     }
 }
--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/LederUtvelgerHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/LederUtvelgerHandler.kt
@@ -3,21 +3,27 @@ package no.nav.tilgangsmaskin.ansatt.nom
 import java.net.URI
 import java.time.LocalDateTime
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.ApplicationEvent
 import org.springframework.context.ApplicationEventPublisher
+import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient.Builder
 
 @Component
-class LederUtvelgerHandler(builder: Builder,
-                           @Value("\${elector.sse.url}") uri: URI,
-                           publisher: ApplicationEventPublisher) {
-    init {
+class LederUtvelgerHandler(private val builder: Builder,
+                           @Value("\${elector.sse.url}") private val uri: URI,
+                           private val publisher: ApplicationEventPublisher) {
+
+
+    @EventListener(ApplicationReadyEvent::class)
+    fun onApplicationReady() {
         builder.build()
             .get()
             .uri(uri)
             .retrieve()
-            .bodyToFlux(LederUtvelgerRespons::class.java).subscribe {
+            .bodyToFlux(LederUtvelgerRespons::class.java)
+            .subscribe {
                 publisher.publishEvent(LeaderChangedEvent(this, it.name))
             }
     }

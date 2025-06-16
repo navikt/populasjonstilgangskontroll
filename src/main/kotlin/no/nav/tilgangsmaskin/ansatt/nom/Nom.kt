8185094f19f72a940d81ce9790c0e358aa294e8e
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-06-16 09:52:53 +0200

    cache på NOM-kall

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/Nom.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/Nom.kt
@@ -2,12 +2,15 @@ package no.nav.tilgangsmaskin.ansatt.nom
 
 import io.micrometer.core.annotation.Timed
 import no.nav.tilgangsmaskin.ansatt.AnsattId
+import no.nav.tilgangsmaskin.ansatt.nom.NomConfig.Companion.NOM
+import no.nav.tilgangsmaskin.ansatt.skjerming.SkjermingConfig.Companion.SKJERMING
+import no.nav.tilgangsmaskin.felles.CacheableRetryingOnRecoverableService
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
-@Service
 @Transactional
 @Timed
+@CacheableRetryingOnRecoverableService(cacheNames = [NOM])
 class Nom(private val adapter: NomJPAAdapter) {
 
     fun lagre(ansattData: NomAnsattData) = adapter.upsert(ansattData)
--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomConfig.kt
@@ -1,11 +1,14 @@
 package no.nav.tilgangsmaskin.ansatt.nom
 
 import no.nav.tilgangsmaskin.ansatt.nom.NomConfig.Companion.NOM
+import no.nav.tilgangsmaskin.felles.rest.CachableRestConfig
 import org.springframework.boot.context.properties.ConfigurationProperties
 
 
 @ConfigurationProperties(NOM)
-class NomConfig(val topic: String) {
+class NomConfig(val topic: String): CachableRestConfig {
+    override val expireHours= 12L
+    override val navn = NOM
 
     companion object {
         const val NOM = "nom"

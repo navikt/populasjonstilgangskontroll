6c8e5e4e2d3fb9308c6781b6e51d0ac5458df052
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-17 09:13:16 +0200

    log

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomVaktmester.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomVaktmester.kt
@@ -9,12 +9,12 @@ import org.springframework.stereotype.Component
 
 @Component
 class NomVaktmester(private val nom: Nom, private val utvelger: LederUtvelger, registry: MeterRegistry) {
+    private val log = getLogger(NomVaktmester::class.java)
 
     init {
         ryddOpp()
     }
 
-    private val log = getLogger(javaClass)
     private val counter = Counter.builder("vaktmester.rader.fjernet")
         .description("Antall rader fjernet")
         .register(registry)

d032049cc4a6b00c6113beb7018aaf656f936ed5
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-23 11:14:19 +0200

    bulk test

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/RegelTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/RegelTjeneste.kt
@@ -8,6 +8,7 @@ import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.bruker.BrukerTjeneste
 import no.nav.tilgangsmaskin.felles.utils.extensions.DomainExtensions.maskFnr
 import no.nav.tilgangsmaskin.regler.motor.IdOgType
+import no.nav.tilgangsmaskin.regler.motor.RegelException
 import no.nav.tilgangsmaskin.regler.motor.RegelMotor
 import no.nav.tilgangsmaskin.regler.motor.RegelSett
 import no.nav.tilgangsmaskin.regler.motor.RegelSett.RegelType.KOMPLETT_REGELTYPE
@@ -47,23 +48,26 @@ class RegelTjeneste(
     fun kjerneregler(ansattId: AnsattId, brukerId: String) =
         motor.kjerneregler(ansatte.ansatt(ansattId), brukere.brukerMedUtvidetFamilie(brukerId))
 
-    fun bulkRegler(ansattId: AnsattId, idOgType: Set<IdOgType>): Set<Pair<BrukerId, HttpStatus>> {
+    fun bulkRegler(ansattId: AnsattId, idOgType: Set<IdOgType>): Set<EnkeltResultat> {
         log.info("Sjekker bulk for ansatt $ansattId og $idOgType brukere")
         val ansatt = ansatte.ansatt(ansattId)
         val brukere = idOgType.brukerIdOgType()
-        val resultater = motor.bulkRegler(ansatt, brukere).map { (brukerId, status, regel) ->
-            if (status == UNAUTHORIZED && overstyring.erOverstyrt(ansattId, brukerId)) {
-                brukerId to ACCEPTED
-            } else {
-                val bruker = brukere.first { it.first.brukerId == brukerId }.first
-               // RegelException(ansatt, bruker,regel!!,  status = status)
-                brukerId to status
-            }
-        }.toSet()
+        val resultater = motor.bulkRegler(ansatt, brukere)
+        val unauth = resultater.filter { it.second == UNAUTHORIZED }
+        val ok = resultater.filter { it.second == OK }.map { EnkeltResultat(it.first,OK) }.toSet()
+        log.info("Fant $ok godkjente")
+         val exceptions = unauth.filter {
+             !overstyring.erOverstyrt(ansattId,it.first)
+         }.map { r ->
+             RegelException(ansatt, brukere.first {  it.first.brukerId == r.first}.first, r.third!!, status = r.second)
+         }.map { EnkeltResultat(it.bruker.brukerId,it.status) }.toSet()
+        log.info("Fant $exceptions avviste")
+
         val resultBrukerIds = resultater.map { it.first.verdi }.toSet()
         val notFound = (idOgType.map { it.brukerId }.toSet() - resultBrukerIds)
-        val nf = notFound.map { BrukerId(it) to NOT_FOUND }
-        return resultater + nf
+        val nf = notFound.map { BrukerId(it)}.map { EnkeltResultat(it,NOT_FOUND) }.toSet()
+        log.info("Ikke funnet $nf")
+        return ok + exceptions + nf
     }
     private fun Set<IdOgType>.brukerIdOgType(): Set<Pair<Bruker, RegelSett.RegelType>> {
         log.info("Bulk henter ${size} brukere")
@@ -74,6 +78,12 @@ class RegelTjeneste(
             }
         }.toSet().also { log.info("Henter ${size} brukere $it") }
     }
+}
+
+ data class EnkeltResultat(
+    val brukerId: BrukerId,
+    val status: HttpStatus,
+    val body: Any? = null ) {
 }
 
 @ConfigurationProperties("regler")

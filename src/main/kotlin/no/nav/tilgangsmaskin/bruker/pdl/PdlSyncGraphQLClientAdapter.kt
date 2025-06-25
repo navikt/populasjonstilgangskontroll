b2e973a889d2c2216505036b99961a9d802b9b11
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-06-25 14:06:53 +0200

    pdl histogram

--- src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PdlSyncGraphQLClientAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PdlSyncGraphQLClientAdapter.kt
@@ -14,7 +14,7 @@ import org.springframework.web.client.RestClient
 import org.springframework.web.client.RestClient.ResponseSpec.ErrorHandler
 
 @Component
-@Timed( value = "pdl_tjeneste", histogram = true, extraTags = ["type", "graphql"] )
+@Timed( value = "pdl_tjeneste", histogram = true, extraTags = ["backend", "graphql"] )
 class PdlSyncGraphQLClientAdapter(
         @Qualifier(PDLGRAPH) graphQlClient: GraphQlClient,
         @Qualifier(PDLGRAPH) restClient: RestClient,

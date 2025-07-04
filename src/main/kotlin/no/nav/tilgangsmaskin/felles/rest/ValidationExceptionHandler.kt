8dea8eae1a47e37651fa9ce274b3beae16b6782e
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-07-04 08:30:45 +0200

    overstyrogn validering

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ValidationExceptionHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ValidationExceptionHandler.kt
@@ -1,25 +1,20 @@
 package no.nav.tilgangsmaskin.felles.rest
 
 import org.slf4j.LoggerFactory
-import org.springframework.core.annotation.Order
 import org.springframework.http.HttpStatus
 import org.springframework.http.ProblemDetail
 import org.springframework.web.ErrorResponseException
-import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 
 @ControllerAdvice
-@Order(0) // Highest precedence
 class ValidationExceptionHandler {
 
     private val log = LoggerFactory.getLogger(javaClass)
 
     @ExceptionHandler(Throwable::class)
-    fun handleValidationException(e: Throwable): Nothing {
+    fun handleValidationException(e: MethodArgumentNotValidException): Nothing {
         log.warn("Validation error", e)
-        throw e
-        /*
         val errors = e.bindingResult.fieldErrors.associate { it.field to (it.defaultMessage ?: "Invalid value") }
         throw ErrorResponseException(
             HttpStatus.BAD_REQUEST,
@@ -30,6 +25,6 @@ class ValidationExceptionHandler {
             e
         ).also {
             log.warn("Valideringsfeil: ${errors.entries.joinToString(", ")}")
-        }*/
+        }
     }
 }

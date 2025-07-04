02d9b0a71c8bed1fa4bfd52d0376d91bf6a3b0d7
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-07-04 14:07:49 +0200

    self

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/AnsattOidResolver.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/AnsattOidResolver.kt
@@ -4,16 +4,22 @@ import no.nav.tilgangsmaskin.ansatt.graph.EntraRestClientAdapter
 import no.nav.tilgangsmaskin.felles.rest.CachableRestConfig
 import no.nav.tilgangsmaskin.tilgang.Token
 import org.slf4j.LoggerFactory.getLogger
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Component
 import java.time.Duration
+import org.springframework.context.annotation.Lazy
 
 @Component
 class AnsattOidResolver(private val adapter: EntraRestClientAdapter, private val token: Token) :CachableRestConfig {
 
     private val log = getLogger(javaClass)
 
-    fun oidForAnsatt(ansattId: AnsattId) =  if (token.erObo)  token.oid else oidFraEntra(ansattId.verdi)
+    @Autowired
+    @Lazy
+    lateinit var self: AnsattOidResolver
+
+    fun oidForAnsatt(ansattId: AnsattId) =  if (token.erObo)  token.oid else self.oidFraEntra(ansattId.verdi)
 
     @Cacheable(cacheNames = ["entra-oid"])
      fun oidFraEntra(ansattId: String) = adapter.oidFraEntra(ansattId).also {

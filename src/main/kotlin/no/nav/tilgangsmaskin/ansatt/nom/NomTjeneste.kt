735043a146a90d0ba2cc251d427f2ab10c9b042c
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-07-08 09:04:10 +0200

    redis l2

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomTjeneste.kt
@@ -2,10 +2,8 @@ package no.nav.tilgangsmaskin.ansatt.nom
 
 import io.micrometer.core.annotation.Timed
 import no.nav.tilgangsmaskin.ansatt.AnsattId
-import no.nav.tilgangsmaskin.ansatt.nom.NomConfig.Companion.NOM
 import no.nav.tilgangsmaskin.felles.RetryingOnRecoverableService
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
@@ -18,7 +16,6 @@ class NomTjeneste(private val adapter: NomJPAAdapter) {
     fun lagre(ansattData: NomAnsattData) = adapter.upsert(ansattData)
 
     @Transactional(readOnly = true)
-    //@Cacheable(cacheNames = [NOM])
     fun fnrForAnsatt(ansattId: AnsattId) = adapter.fnrForAnsatt(ansattId.verdi)
     fun ryddOpp() = adapter.ryddOpp().also {
         if (it > 0) log.info("Vaktmester ryddet opp $it rad(er) med utgått informasjon om ansatte som ikke lenger jobber i Nav")

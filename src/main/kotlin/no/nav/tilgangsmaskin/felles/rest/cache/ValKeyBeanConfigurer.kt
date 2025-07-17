1bbc415ef5b9527923462b9532e2abead8ef68e0
Jan Eirik Nævdal <jan.eirik.b.navdal@nav.no>
2025-07-17 15:43:36 +0200

    Forsøker Locking på cache: Ref error 2 entiries Json serialzation

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyBeanConfigurer.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyBeanConfigurer.kt
@@ -8,20 +8,15 @@ import no.nav.boot.conditionals.EnvUtil.isDevOrLocal
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.felles.rest.CachableRestConfig
 import no.nav.tilgangsmaskin.felles.rest.PingableHealthIndicator
-import org.slf4j.LoggerFactory.getLogger
-import org.springframework.cache.Cache
 import org.springframework.cache.annotation.CachingConfigurer
 import org.springframework.cache.annotation.EnableCaching
-import org.springframework.cache.interceptor.CacheOperationInvocationContext
-import org.springframework.cache.interceptor.CacheResolver
 import org.springframework.cache.interceptor.KeyGenerator
-import org.springframework.cache.interceptor.SimpleCacheResolver
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig
 import org.springframework.data.redis.cache.RedisCacheManager
-import org.springframework.data.redis.cache.RedisCacheWriter.nonLockingRedisCacheWriter
+import org.springframework.data.redis.cache.RedisCacheWriter.lockingRedisCacheWriter
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer
@@ -51,7 +46,7 @@ class ValKeyBeanConfigurer(private val cf: RedisConnectionFactory,
 
     @Bean
     override fun cacheManager(): RedisCacheManager =
-        RedisCacheManager.builder(nonLockingRedisCacheWriter(cf))
+        RedisCacheManager.builder(lockingRedisCacheWriter(cf))
             .withInitialCacheConfigurations(cfgs.associate { it.navn to cacheConfig(it) })
             .enableStatistics()
             .build()

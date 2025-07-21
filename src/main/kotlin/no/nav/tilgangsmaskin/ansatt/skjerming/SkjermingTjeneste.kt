94c799d63c20af901efc9cc35c6c6a205c4f7f34
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-07-21 22:50:46 +0200

    kall fra controller

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingTjeneste.kt
@@ -9,10 +9,10 @@ import org.springframework.cache.annotation.Cacheable
 
 @RetryingOnRecoverableService
 @Timed
-@Cacheable(cacheNames = [SKJERMING])
 // condition = "#root.args.?[!(#this instanceof T(java.util.Collection))].length == #root.args.length")
 class SkjermingTjeneste(private val adapter: SkjermingRestClientAdapter) {
 
+    @Cacheable(cacheNames = [SKJERMING])
     fun skjerming(brukerId: BrukerId) = adapter.skjerming(brukerId.verdi)
 
     fun skjerminger(brukerIds: Set<BrukerId>) = adapter.skjerminger(brukerIds.map { it.verdi }.toSet())

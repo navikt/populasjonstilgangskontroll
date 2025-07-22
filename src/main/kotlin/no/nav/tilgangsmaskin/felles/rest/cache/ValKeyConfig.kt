d4a647181550af943908e671bdf22dcf4cb4f62f
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-07-22 12:29:34 +0200

    cache

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -1,6 +1,9 @@
 package no.nav.tilgangsmaskin.felles.rest.cache
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
 data class ValKeyConfig(
     @Value("\${VALKEY_HOST_VALKEY_TILGANGSMASKIN_CACHE}") val host: String,
     @Value("\${VALKEY_PORT_VALKEY_TILGANGSMASKIN_CACHE}") val port: String

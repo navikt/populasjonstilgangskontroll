345918f779d0332d5266d7b2866aedcca01b48b2
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-08 14:41:19 +0200

    prøv bulk også

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/DevTilgangController.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/DevTilgangController.kt
@@ -32,6 +32,7 @@ import no.nav.tilgangsmaskin.tilgang.RegelTjeneste
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.http.HttpStatus.*
 import org.springframework.web.bind.annotation.*
 
@@ -49,10 +50,19 @@ class DevTilgangController(
     private val overstyring: OverstyringTjeneste,
     private val pip: PdlRestClientAdapter,
     private val nom: NomTjeneste,
+    private val valkey: RedisTemplate,
     private val pdl: PDLTjeneste) {
 
     private  val log = getLogger(javaClass)
 
+
+    @GetMapping("valkey/{cache}")
+    fun sivilstand(@PathVariable  cache: String) {
+        log.info("Henter fra cache: $cache")
+        valkey.keys("*$cache*").forEach {
+            log.info("Cache key: $it")
+        }
+    }
 
     @GetMapping("sivilstand/{id}")
     fun sivilstand(@PathVariable @Valid @ValidId id: String) = graphql.sivilstand(id)

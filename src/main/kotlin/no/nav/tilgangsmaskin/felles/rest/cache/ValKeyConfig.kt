0acc62185557c33048f2ea0f8d6f74418197c6dc
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-08 10:43:06 +0200

    dynamic cache name

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -1,10 +1,18 @@
 package no.nav.tilgangsmaskin.felles.rest.cache
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.env.Environment
+import org.springframework.core.env.getRequiredProperty
 import org.springframework.stereotype.Component
 
 @Component
-data class ValKeyConfig(
-    @Value("\${VALKEY_HOST_CACHE1}") val host: String,
-    @Value("\${VALKEY_PORT_CACHE1}") val port: String
-)
+class ValKeyConfig(environment: Environment) {
+    lateinit var host: String
+    lateinit var port: String
+
+    init {
+        val cacheName = environment.getProperty("nais.cache.name")
+        host = environment.getRequiredProperty("valkey.host.$cacheName")
+        port = environment.getRequiredProperty("valkey.port.$cacheName")
+    }
+}

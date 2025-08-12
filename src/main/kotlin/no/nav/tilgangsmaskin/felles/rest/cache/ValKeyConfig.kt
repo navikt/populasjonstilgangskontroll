220baa1a218fafbb30afe01e72567fda396f6f19
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-12 09:40:51 +0200

    suffix

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -1,10 +1,14 @@
 package no.nav.tilgangsmaskin.felles.rest.cache
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 
 @Component
-data class ValKeyConfig(
-    @Value("\${VALKEY_HOST_CACHE}") val host: String,
-    @Value("\${VALKEY_PORT_CACHE}") val port: String
-)
+data class ValKeyConfig(private val env: Environment, @Value("\${valkey.suffix}") val suffix: String) {
+
+
+    val port: String  get() = env.getRequiredProperty("valkey.port.$suffix")
+    val host: String  get() = env.getRequiredProperty("valkey.host.$suffix")
+
+}

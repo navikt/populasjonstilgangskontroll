6e77deb378859b89f7ab5c19d819a9a2855135cb
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-12 11:18:25 +0200

    config cache name complete?

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -1,14 +1,6 @@
 package no.nav.tilgangsmaskin.felles.rest.cache
 
-import org.springframework.beans.factory.annotation.Value
-import org.springframework.core.env.Environment
-import org.springframework.stereotype.Component
+import org.springframework.boot.context.properties.ConfigurationProperties
 
-@Component
-data class ValKeyConfig(private val env: Environment, @Value("\${valkey.suffix}") val suffix: String) {
-
-
-    val port: String  get() = env.getRequiredProperty("valkey.port.$suffix")
-    val host: String  get() = env.getRequiredProperty("valkey.host.$suffix")
-
-}
+@ConfigurationProperties(prefix = "valkey")
+data class ValKeyConfig(val host: String, val port: String)

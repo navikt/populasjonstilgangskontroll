5f93428c61801720bd994b3da5f62508bb7198d6
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-12 09:42:40 +0200

    test suffix cache

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -1,6 +1,7 @@
 package no.nav.tilgangsmaskin.felles.rest.cache
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 
 @Component
@@ -8,3 +9,11 @@ data class ValKeyConfig(
     @Value("\${VALKEY_HOST_CACHE1}") val host: String,
     @Value("\${VALKEY_PORT_CACHE1}") val port: String
 )
+
+@Component
+data class ValKeyConfig1(private val env: Environment, @Value("\${valkey.suffix}") val suffix: String) {
+
+    val port: String  get() = env.getRequiredProperty("valkey.port.$suffix")
+    val host: String  get() = env.getRequiredProperty("valkey.host.$suffix")
+
+}

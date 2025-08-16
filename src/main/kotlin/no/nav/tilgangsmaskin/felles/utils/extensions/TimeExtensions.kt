68dd3d78b5d3902461919eb5616129362cc375b2
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-16 12:36:00 +0200

    key

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/utils/extensions/TimeExtensions.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/utils/extensions/TimeExtensions.kt
@@ -23,7 +23,7 @@ object TimeExtensions {
             Period.between(this, it).let { it.years * 12 + it.months } + if (it.dayOfMonth > dayOfMonth) 1 else 0
         }
 
-    private fun Duration.format(): String {
+     fun Duration.format(): String {
         val days = inWholeDays
         val hours = inWholeHours % 24
         val minutes = inWholeMinutes % 60

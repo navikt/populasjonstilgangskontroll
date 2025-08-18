70a6b0e0d8428d5b79b1802053036e5e86f90b69
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-18 11:16:59 +0200

    added uusername/password

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -5,4 +5,4 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import java.net.URI
 
 @ConfigurationProperties(VALKEY)
-data class ValKeyConfig(val host: String, val port: String, val uri: URI)
+data class ValKeyConfig(val username: String, val password: String, val host: String, val port: String, val uri: URI)

85436bbee4ad8f813db80a7ee70b0db22c96c73e
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-18 12:37:46 +0200

    test ny url b

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.tilgangsmaskin.felles.rest.cache
 
+import io.lettuce.core.RedisURI.URI_SCHEME_REDIS_SECURE
 import io.lettuce.core.RedisURI
 import no.nav.tilgangsmaskin.felles.rest.cache.ValKeyAdapter.Companion.VALKEY
 import org.springframework.boot.context.properties.ConfigurationProperties
@@ -7,6 +8,11 @@ import java.net.URI
 
 @ConfigurationProperties(VALKEY)
 data class ValKeyConfig(val username: String, val password: String, val host: String, val port: String, val uri: URI) {
-    val redisURI  = RedisURI.create("rediss://${username}:${password}@${host}:${port}");
+    val redisURI = RedisURI.Builder
+        .redis(host, port.toInt())
+        .withSsl(true)
+        .withAuthentication(username, password)
+        .build()
+    //val redisURI  = RedisURI.create("$URI_SCHEME_REDIS_SECURE://${username}:${password}@${host}:${port}");
 }
 

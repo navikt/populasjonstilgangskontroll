3e7f6bf6386bb4961e317661448c82beaafd7888
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-18 13:14:08 +0200

    nonlocking

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyBeanConfigurer.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyBeanConfigurer.kt
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.core.env.Environment
 import org.springframework.data.redis.cache.RedisCacheConfiguration.defaultCacheConfig
 import org.springframework.data.redis.cache.RedisCacheManager
-import org.springframework.data.redis.cache.RedisCacheWriter.lockingRedisCacheWriter
+import org.springframework.data.redis.cache.RedisCacheWriter.nonLockingRedisCacheWriter
 import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair.fromSerializer
@@ -46,7 +46,7 @@ class ValKeyBeanConfigurer(private val cf: RedisConnectionFactory,
 
     @Bean
     override fun cacheManager(): RedisCacheManager =
-        RedisCacheManager.builder(lockingRedisCacheWriter(cf))
+        RedisCacheManager.builder(nonLockingRedisCacheWriter(cf))
             .withInitialCacheConfigurations(cfgs.associate { it.navn to cacheConfig(it) })
             .enableStatistics()
             .build()

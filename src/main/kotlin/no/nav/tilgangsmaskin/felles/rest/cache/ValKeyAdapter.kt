4aeea4ef8a99bf07393f138ea712324358d30ed3
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-18 12:58:24 +0200

    test ny url b

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyAdapter.kt
@@ -18,6 +18,7 @@ class ValKeyAdapter(private val cf: RedisConnectionFactory, private val cfg: Val
 
     override val pingEndpoint  =  "${cfg.host}:${cfg.port}"
     override val name = "ValKey Cache"
+    private val connection = RedisClient.create(cfg.valkeyURI).connect()
 
 
     override fun ping() =
@@ -31,8 +32,6 @@ class ValKeyAdapter(private val cf: RedisConnectionFactory, private val cfg: Val
         }
 
     fun lookup(key: String): String? {
-        val client = RedisClient.create(cfg.redisURI)
-        val connection = client.connect()
         val commands: RedisCommands<String, String> = connection.sync()
         val value = commands.get(key)
         connection.close()

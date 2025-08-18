520ee640d1ecf2ab9799848e8c88cc2df17ac526
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-18 11:33:51 +0200

    valkey lookup test

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -1,8 +1,12 @@
 package no.nav.tilgangsmaskin.felles.rest.cache
 
+import io.lettuce.core.RedisURI
 import no.nav.tilgangsmaskin.felles.rest.cache.ValKeyAdapter.Companion.VALKEY
 import org.springframework.boot.context.properties.ConfigurationProperties
 import java.net.URI
 
 @ConfigurationProperties(VALKEY)
-data class ValKeyConfig(val username: String, val password: String, val host: String, val port: String, val uri: URI)
+data class ValKeyConfig(val username: String, val password: String, val host: String, val port: String, val uri: URI) {
+    val redisURI  = RedisURI.create("rediss://${username}:${password}@${uri}");
+}
+

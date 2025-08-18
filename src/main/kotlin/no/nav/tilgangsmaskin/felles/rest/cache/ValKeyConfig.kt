86bc4db0c6fe2cfc0ea61af905785d1f4f95261f
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-08-18 11:05:13 +0200

    added uri

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/cache/ValKeyConfig.kt
@@ -2,6 +2,7 @@ package no.nav.tilgangsmaskin.felles.rest.cache
 
 import no.nav.tilgangsmaskin.felles.rest.cache.ValKeyAdapter.Companion.VALKEY
 import org.springframework.boot.context.properties.ConfigurationProperties
+import java.net.URI
 
 @ConfigurationProperties(VALKEY)
-data class ValKeyConfig(val host: String, val port: String, uri: URI)
+data class ValKeyConfig(val host: String, val port: String, val uri: URI)

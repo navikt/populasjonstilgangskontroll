fa39b8f0b9392c0060c545b4137b0094b88e2ed9
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-09-01 20:40:46 +0200

    bedre impl Geografisk

--- src/main/kotlin/no/nav/tilgangsmaskin/oppfølging/OppfølgingTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/oppfølging/OppfølgingTjeneste.kt
@@ -1,13 +1,29 @@
 package no.nav.tilgangsmaskin.oppfølging
 
+import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.bruker.Enhetsnummer
+import no.nav.tilgangsmaskin.felles.rest.CachableRestConfig
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
+import java.time.Duration
 
 @Service
-class OppfølgingTjeneste {
+class OppfølgingTjeneste : CachableRestConfig {
+    @Cacheable(cacheNames = [OPPFØLGING],key = "#brukerId.verdi")
     fun enhetFor(brukerId: BrukerId): Enhetsnummer? {
         // TODO implementer kall til oppfølging
         return null
     }
+
+    override val varighet = Duration.ofHours(12)
+    override val navn = OPPFØLGING
+
+    companion object {
+        const val OPPFØLGING = "oppfølging"
+    }
 }
+
+
+
+

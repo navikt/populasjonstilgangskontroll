97a73be5f57a2a9ad6dcff351fe7865786b838cc
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-09-02 15:25:44 +0200

    open the sucker

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 val javaVersion = JavaLanguageVersion.of(21)
 val springdocVersion = "2.8.11"
-val tokenSupportVersion = "5.0.35"
+val tokenSupportVersion = "5.0.36"
 val mockkVersion = "1.14.5"
 
 group = "no.nav.tilgangsmaskin.populasjonstrilgangskontroll"

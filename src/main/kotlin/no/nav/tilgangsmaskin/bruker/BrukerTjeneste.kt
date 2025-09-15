d1eaa96709b82b000a024fda33cc255f93af9305
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-09-15 18:37:50 +0200

    fix med oppslagId

--- src/main/kotlin/no/nav/tilgangsmaskin/bruker/BrukerTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/bruker/BrukerTjeneste.kt
@@ -15,6 +15,7 @@ class BrukerTjeneste(private val personTjeneste: PDLTjeneste, val skjermingTjene
 
     @WithSpan
     fun brukere(brukerIds: Set<String>) : Set<Bruker> {
+        log.info("Mottok bulk kall for $brukerIds")
         if (brukerIds.isEmpty()) {
             log.info("Bulk ingen personer å slå opp")
             return emptySet()
@@ -55,7 +56,9 @@ class BrukerTjeneste(private val personTjeneste: PDLTjeneste, val skjermingTjene
     @WithSpan
     private fun brukerMedSkjerming(id: String, hentFamilie: (String) -> Person) =
         with(hentFamilie(id)) {
-            tilBruker(this, skjermingTjeneste.skjerming(brukerId))
+            tilBruker(this, skjermingTjeneste.skjerming(brukerId)).also {
+                log.trace("Bruker er {}", it)
+            }
         }
             /*
             val statuser = skjermingTjeneste.skjerminger(historiskeIds + brukerId)
--- src/test/kotlin/no/nav/tilgangsmaskin/regler/BrukerBuilder.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/regler/BrukerBuilder.kt
@@ -27,7 +27,9 @@ data class BrukerBuilder(
     var far: FamilieMedlem? = null,
     var barn: Set<FamilieMedlem> = emptySet(),
     var partnere: Set<FamilieMedlem> = emptySet(),
+    var oppslagId: String = id.verdi,
     var dødsdato: LocalDate? = null) {
+    fun oppslagId(oppslagId: String) = apply { this.oppslagId = oppslagId }
     fun aktørId(aktørId: AktørId) = apply { this.aktørId = aktørId }
     fun gt(gt: GeografiskTilknytning) = apply { this.gt = gt }
     fun kreverMedlemskapI(vararg grupper: GlobalGruppe) = apply { this.grupper = setOf(*grupper) }
@@ -37,7 +39,7 @@ data class BrukerBuilder(
     fun partnere(partnere: Set<BrukerId>) = apply { this.partnere = partnere.tilFamilieMedlemmer(PARTNER) }
     fun historiske(historiske: Set<BrukerId>) = apply { this.historiske = historiske }
     fun build() = Bruker(
-            BrukerIds(id, id.verdi,historiske, aktørId),
+            BrukerIds(id, oppslagId,historiske, aktørId),
             gt, grupper,
             Familie(setOfNotNull(mor, far), barn, søsken, partnere),
             dødsdato)

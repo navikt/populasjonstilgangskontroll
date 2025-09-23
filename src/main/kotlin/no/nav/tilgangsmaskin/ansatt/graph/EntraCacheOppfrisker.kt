9801877ff626255d9729dbbf524f28e6f605104c
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-09-23 11:41:37 +0200

    oppfrisker

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/graph/EntraCacheOppfrisker.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/graph/EntraCacheOppfrisker.kt
@@ -13,11 +13,10 @@ class EntraCacheOppfrisker(private val entra: EntraTjeneste, private val oid: An
     fun oppfrisk(detaljer: DetaljerFraKey, ansattId: AnsattId) {
         if (detaljer.metode == "geoGrupper") {
             entra.geoGrupper(ansattId, oid.oidFraEntra(ansattId))
-            log.info("Oppfrisket ${detaljer.cacheName}::geoGrupper for ${ansattId.verdi} etter sletting" )
         }
         if (detaljer.metode == "geoOgGlobaleGrupper") {
             entra.geoOgGlobaleGrupper(ansattId, oid.oidFraEntra(ansattId))
-            log.info("Oppfrisket ${detaljer.cacheName}::geoOgGlobaleGrupper  for ${ansattId.verdi} etter sletting" )
         }
+        log.info("Oppfrisket ${detaljer.cacheName}::${detaljer.metode}  for ${ansattId.verdi} etter sletting" )
     }
 }

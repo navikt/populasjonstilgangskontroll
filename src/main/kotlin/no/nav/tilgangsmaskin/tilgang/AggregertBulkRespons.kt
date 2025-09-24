6adf679695fb0f42085b58377ffef9b189e13ce9
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-09-24 14:05:44 +0200

    bruk oppslagId

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/AggregertBulkRespons.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/AggregertBulkRespons.kt
@@ -3,12 +3,13 @@ package no.nav.tilgangsmaskin.tilgang
 import com.fasterxml.jackson.annotation.JsonIgnore
 import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.bruker.BrukerId
+import no.nav.tilgangsmaskin.felles.utils.extensions.DomainExtensions.maskFnr
 import no.nav.tilgangsmaskin.regler.motor.RegelException
 import org.springframework.http.HttpStatus
 import org.springframework.http.HttpStatus.*
 
 data class AggregertBulkRespons(val ansattId: AnsattId, val resultater: Set<EnkeltBulkRespons> = emptySet()) {
-    data class EnkeltBulkRespons(val brukerId: String, @JsonIgnore val httpStatus: HttpStatus, val detaljer: Any? = null ) {
+    data class EnkeltBulkRespons(val oppslagId: String, @JsonIgnore val httpStatus: HttpStatus, val detaljer: Any? = null ) {
         constructor(e: RegelException) : this(e.bruker.oppslagId, e.status,e.body)
         val status = httpStatus.value()
 
@@ -16,6 +17,9 @@ data class AggregertBulkRespons(val ansattId: AnsattId, val resultater: Set<Enke
             fun ok(brukerId: String) = EnkeltBulkRespons(brukerId, NO_CONTENT)
            // fun ok(brukerId: BrukerId) = ok(brukerId.verdi)
         }
+
+        override fun toString(): String =
+            "EnkeltBulkRespons(oppslagId='${oppslagId.maskFnr()}', httpStatus=$httpStatus, detaljer=$detaljer, status=$status)"
     }
     @JsonIgnore
     val ukjente = filter(NOT_FOUND)
@@ -26,4 +30,6 @@ data class AggregertBulkRespons(val ansattId: AnsattId, val resultater: Set<Enke
 
     private fun filter(status: HttpStatus) = resultater.filter { it.httpStatus == status }.toSet()
 
+    override fun toString(): String =
+        "AggregertBulkRespons(ansattId=$ansattId, resultater=$resultater)"
 }

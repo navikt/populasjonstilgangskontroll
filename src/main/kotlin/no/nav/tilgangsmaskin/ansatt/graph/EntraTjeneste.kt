7a328d3e5c9df8b2e9384293610d4c1b77d48fe5
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-09-29 16:38:22 +0200

    EntraTjeneste er en oppfrisker selv

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/graph/EntraTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/graph/EntraTjeneste.kt
@@ -8,10 +8,19 @@ import no.nav.tilgangsmaskin.ansatt.graph.EntraConfig.Companion.GRAPH
 import no.nav.tilgangsmaskin.felles.RetryingOnRecoverableService
 import org.springframework.cache.annotation.Cacheable
 import java.util.UUID
+import no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL
+import no.nav.tilgangsmaskin.felles.cache.CacheNøkkelElementer
+import no.nav.tilgangsmaskin.felles.cache.CacheOppfrisker
+import no.nav.tilgangsmaskin.felles.utils.extensions.DomainExtensions.maskFnr
+import org.slf4j.LoggerFactory.getLogger
+import kotlin.reflect.KCallable
 
 @RetryingOnRecoverableService
 @Timed(value = "entra", histogram = true)
-class EntraTjeneste(private val adapter: EntraRestClientAdapter, private val resolver: AnsattOidTjeneste) {
+class EntraTjeneste(private val adapter: EntraRestClientAdapter, private val resolver: AnsattOidTjeneste) : CacheOppfrisker {
+
+    override val cacheName: String = GRAPH
+    private val log = getLogger(javaClass)
 
     @Cacheable(cacheNames = [GRAPH],  key = "#root.methodName + ':' + #ansattId.verdi")
     @WithSpan
@@ -21,5 +30,26 @@ class EntraTjeneste(private val adapter: EntraRestClientAdapter, private val res
     @WithSpan
     fun geoGrupper(ansattId: AnsattId, oid: UUID) = adapter.grupper(oid.toString(), false)
 
+
+
+    override fun oppfrisk(deler: CacheNøkkelElementer) {
+        runCatching {
+            with(AnsattId(deler.id)) {
+                valider(deler).call(this@EntraTjeneste,this, resolver.oidFraEntra(this)).also {
+                    log.trace(CONFIDENTIAL,"Oppfrisket ${deler.key} etter sletting")
+                }
+            }
+        }.getOrElse {
+            log.info("Oppfrisking av ${deler.id.maskFnr()} etter sletting feilet, dette er ikke kritisk",it)
+        }
+    }
+     private fun valider(deler: CacheNøkkelElementer)  =
+        EntraTjeneste::class.members.first { it.name == deler.metode }
+            .also {
+                val params = it.parameters.drop(1)
+                require(params[0].type.classifier == AnsattId::class) { "Argument 1 er ikke AnsattId" }
+                require(params[1].type.classifier == UUID::class) { "Argument 2 er ikke UUID" }
+            }
     override fun toString() = "${javaClass.simpleName} [adapter=$adapter resolver=$resolver]"
+
 }
--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheOppfrisker.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheOppfrisker.kt
@@ -5,5 +5,4 @@ import kotlin.reflect.KCallable
 interface CacheOppfrisker {
     val cacheName: String
     fun oppfrisk(deler: CacheNøkkelElementer)
-    fun valider(deler: CacheNøkkelElementer): KCallable<*>
 }

630f92646e41c3244b1b2fc3ee9e2c7e444c839c
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-10-01 14:45:32 +0200

    bruk smartLifecycle

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheExpiredEventListener.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheExpiredEventListener.kt
@@ -7,17 +7,17 @@ import no.nav.tilgangsmaskin.felles.cache.CacheRemovalListener.CacheExpiredEvent
 import no.nav.tilgangsmaskin.felles.utils.LeaderAware
 import no.nav.tilgangsmaskin.regler.motor.BulkCacheTeller
 import org.slf4j.LoggerFactory.getLogger
-import org.springframework.context.event.ContextClosedEvent
+import org.springframework.context.SmartLifecycle
 import org.springframework.context.event.EventListener
 import org.springframework.stereotype.Component
 
 @Component
-class CacheExpiredEventListener( val teller: BulkCacheTeller,erLeder: Boolean = true,private vararg val oppfriskere: CacheOppfrisker) :LeaderAware(erLeder) {
+class CacheExpiredEventListener( val teller: BulkCacheTeller,erLeder: Boolean = true,private vararg val oppfriskere: CacheOppfrisker) :LeaderAware(erLeder), SmartLifecycle {
     private val log = getLogger(javaClass)
-
+    private var running = false
     @EventListener
     fun cacheInnslagFjernet(hendelse: CacheExpiredEvent) {
-        if (erLeder && !stopper.get()) {
+        if (erLeder && isRunning()) {
             val elementer = CacheNøkkelElementer(hendelse.nøkkel)
             oppfriskere.firstOrNull { it.cacheName == elementer.cacheName }?.run {
                 oppfrisk(elementer)
@@ -29,12 +29,7 @@ class CacheExpiredEventListener( val teller: BulkCacheTeller,erLeder: Boolean = 
         }
     }
 
-    @EventListener
-    fun stopper(event: ContextClosedEvent) {
-        log.info("Applikasjonen holder på å stoppe")
-        stopper.set(true)
-    }
-    companion object {
-        private val stopper = AtomicBoolean(false)
-    }
+    override fun start() { running = true }
+    override fun isRunning() = running
+    override fun stop() { running = false }
 }

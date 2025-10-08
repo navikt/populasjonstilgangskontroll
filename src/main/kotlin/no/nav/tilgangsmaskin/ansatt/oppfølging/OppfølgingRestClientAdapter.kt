56958925f4a826fda2be6b90c4093aad18f4c762
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-10-08 14:38:46 +0200

    any

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRestClientAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRestClientAdapter.kt
@@ -12,15 +12,20 @@ import org.springframework.web.client.RestClient
 class OppfølgingRestClientAdapter(@Qualifier(OPPFØLGING) restClient: RestClient, val cf: OppfølgingConfig) :
     AbstractRestClientAdapter(restClient, cf) {
     fun enheterFor(ids: List<String>) =
-        post<List<Map<Any,Any>>>(cf.baseUri, Identer(ids)).also {
+        post<List<EnhetRespons>>(cf.baseUri, Identer(ids)).also {
+    //    post<List<Map<Any,Any>>>(cf.baseUri, Identer(ids)).also {
             log.info("Oppfølging returnerte ${it.size} identer")
         }.map {
             OppfølgingsEnhet(
+                it.ident, it.kontorId?.let { Enhetsnummer(it) }
+            )
+            /*
+            OppfølgingsEnhet(
                 ident = it["ident"] as String,
                 enhet = (it["kontorId"] as String?)?.let { Enhetsnummer(it) }
-            )
+            )*/
         }
 }
-    data class OppfølgingsEnhet(val ident: String, val enhet: Enhetsnummer? = null)
-    private data class Identer(val identer: List<String>)
-    data class EnhetRespons(val ident: String, val httpStatus: Int?, val kontorId: String?)
+data class EnhetRespons(val ident: String, val httpStatus: Int?, val kontorId: String?)
+data class OppfølgingsEnhet(val ident: String, val enhet: Enhetsnummer? = null)
+private data class Identer(val identer: List<String>)

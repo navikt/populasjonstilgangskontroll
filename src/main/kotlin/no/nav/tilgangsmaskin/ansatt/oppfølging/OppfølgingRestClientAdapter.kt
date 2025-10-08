8b1a19392f8c45b6c788f164cfe09f9d818ecedb
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-10-08 11:32:41 +0200

    serialize

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRestClientAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRestClientAdapter.kt
@@ -1,6 +1,8 @@
 package no.nav.tilgangsmaskin.ansatt.oppfølging
 
 import no.nav.tilgangsmaskin.ansatt.oppfølging.OppfølgingConfig.Companion.OPPFØLGING
+import no.nav.tilgangsmaskin.bruker.Enhetsnummer
+import no.nav.tilgangsmaskin.bruker.Identifikator
 import no.nav.tilgangsmaskin.felles.rest.AbstractRestClientAdapter
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
@@ -10,6 +12,14 @@ import org.springframework.web.client.RestClient
 class OppfølgingRestClientAdapter(@Qualifier(OPPFØLGING) restClient: RestClient, val cf: OppfølgingConfig) :
     AbstractRestClientAdapter(restClient, cf) {
     fun enheterFor(ids: List<String>) =
-         post<Any>(cf.baseUri, Identer(ids))
+        post<List<EnhetRespons>>(cf.baseUri, Identer(ids)).also {
+            log.info("Oppfølging returnerte respons $it for ${ids.size} identer")
+        }.map {
+            OppfølgingsEnhet(Identifikator(it.ident), Enhetsnummer(it.kontorId))
+        }
+}
+    data class OppfølgingsEnhet(val ident: Identifikator, val enhet: Enhetsnummer? = null)
+    private data class Identer(val identer: List<String>)
+    private data class EnhetRespons(val ident: String, val httpStatus: String?, val kontorId: String)
 
-    private data class Identer(val identer: List<String>)}
+}

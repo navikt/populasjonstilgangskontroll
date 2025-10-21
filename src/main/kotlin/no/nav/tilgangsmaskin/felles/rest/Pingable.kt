b0b56db633e1c6ec1574e39d99dc942f9b08a5d8
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-10-21 12:00:09 +0200

    Any?

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/Pingable.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/Pingable.kt
@@ -2,7 +2,7 @@ package no.nav.tilgangsmaskin.felles.rest
 
 interface Pingable {
 
-    fun ping(): Any
+    fun ping(): Any?
     val pingEndpoint: String
     val name: String
     val isEnabled get(): Boolean = true

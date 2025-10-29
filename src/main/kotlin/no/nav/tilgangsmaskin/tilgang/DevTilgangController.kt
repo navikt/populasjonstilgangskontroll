69ea4397ca2d6e65da5ce0f217dad6d42dc7c797
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-10-29 15:15:20 +0100

    cache size

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/DevTilgangController.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/DevTilgangController.kt
@@ -73,7 +73,7 @@ class DevTilgangController(
     fun cachePersoner(@RequestBody  navIds: Set<Identifikator>) = cache.getMany<Person>(CachableConfig(PDL),navIds.map { it.verdi }.toSet())
 
     @GetMapping("cache/count/{prefix}")
-    fun size(@PathVariable prefix: String) = cache.cacheSize(prefix)
+    fun size(@PathVariable prefix: String) = cache.cacheStørrelse(prefix)
 
     @GetMapping("sivilstand/{id}")
     fun sivilstand(@PathVariable  id: String) = graphql.partnere(id)

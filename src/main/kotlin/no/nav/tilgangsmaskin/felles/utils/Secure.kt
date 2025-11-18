2983bd2a524600be11717e0c702f8a092ef44107
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-18 17:30:27 +0100

    dupllikat, gjort i app

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/utils/Secure.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/utils/Secure.kt
@@ -5,9 +5,12 @@ import org.springframework.stereotype.Component
 
 @Component
 class Auditor {
-    private val secureLog = LoggerFactory.getLogger("secureLog")
+    private val secureLog = LoggerFactory.getLogger(AUDIT_LOGGER_NAME)
     fun trace(message: String) = secureLog.trace(message)
     fun warn(message: String) = secureLog.warn(message)
     fun info(message: String) = secureLog.info(message)
 
+    companion object {
+        private const val AUDIT_LOGGER_NAME = "secureLog"
+    }
 }

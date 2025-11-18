8b051382e8f299dd5f4f4e42c80446ca8e7a5f4d
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-18 11:43:37 +0100

    no conditional

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheModule.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheModule.kt
@@ -3,7 +3,6 @@ package no.nav.tilgangsmaskin.felles.cache
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As.PROPERTY
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id.CLASS
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Value.construct
-import no.nav.boot.conditionals.ConditionalOnNotProd
 import tools.jackson.core.Version.unknownVersion
 import tools.jackson.databind.AnnotationIntrospector
 import tools.jackson.databind.cfg.MapperConfig
@@ -11,7 +10,7 @@ import tools.jackson.databind.introspect.Annotated
 import tools.jackson.databind.jsontype.impl.StdTypeResolverBuilder
 import tools.jackson.databind.module.SimpleModule
 
-@ConditionalOnNotProd
+//@ConditionalOnNotProd
 /**
 Dette er en modul for Jackson-serialisering som legger til en egendefinert AnnotationIntrospector. Denne introspektoren styrer hvordan typeinformasjon håndteres ved serialisering og deserialisering, slik at objekter får med seg typeinformasjon i JSON-feltet @class. Modulen aktiveres freløpigkun utenfor produksjonsmiljø.
  */

d59f8440634b397e0c4488cf4d5b1f75b0cf5464
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-18 17:19:05 +0100

    log retries via debug

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RetryingWhenRecoverable.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RetryingWhenRecoverable.kt
@@ -4,11 +4,14 @@ import org.springframework.core.annotation.AliasFor
 import org.springframework.resilience.annotation.Retryable
 import org.springframework.web.client.ResourceAccessException
 import java.lang.annotation.Inherited
+import kotlin.annotation.AnnotationRetention.RUNTIME
+import kotlin.annotation.AnnotationTarget.CLASS
+import kotlin.annotation.AnnotationTarget.FUNCTION
 import kotlin.reflect.KClass
 
 @Retryable
-@Target(AnnotationTarget.FUNCTION, AnnotationTarget.CLASS)
-@Retention(AnnotationRetention.RUNTIME)
+@Target(FUNCTION, CLASS)
+@Retention(RUNTIME)
 @Inherited
 @MustBeDocumented
 annotation class RetryingWhenRecoverable(

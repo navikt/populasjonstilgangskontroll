4d090af3d3cfcbe0676bd988293a6c8499f6756c
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-11-18 12:16:17 +0100

    6.0.0-SNAPHSOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 val javaVersion = JavaLanguageVersion.of(21)
 val springdocVersion = "3.0.0-RC1"
-val tokenSupportVersion = "3.0.1-SNAPSHOT"
+val tokenSupportVersion = "6.0.0-SNAPSHOT"
 val mockkVersion = "1.14.6"
 
 

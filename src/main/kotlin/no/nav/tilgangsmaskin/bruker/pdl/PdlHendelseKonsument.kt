117e7a2206a6f8bbb1854eb5c29aad3ee349dc16
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-12-08 16:46:26 +0100

    filter logic

--- src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PdlHendelseKonsument.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PdlHendelseKonsument.kt
@@ -1,10 +1,10 @@
 package no.nav.tilgangsmaskin.bruker.pdl
 
 
-import no.nav.boot.conditionals.EnvUtil.CONFIDENTIAL
 import no.nav.person.pdl.leesah.Personhendelse
 import no.nav.tilgangsmaskin.bruker.pdl.PdlRestClientAdapter.Companion.PDL_CACHES
 import no.nav.tilgangsmaskin.felles.cache.CacheClient
+import no.nav.tilgangsmaskin.felles.utils.extensions.DomainExtensions.maskFnr
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
@@ -15,11 +15,15 @@ class PdlHendelseKonsument(private val client: CacheClient) {
 
     @KafkaListener(topics = [ "pdl.leesah-v1"], containerFactory = "pdlAvroListenerContainerFactory", filter = "graderingFilterStrategy")
     fun listen(hendelse: Personhendelse) {
-        log.info("Mottok hendelse fra PDL $hendelse, tømmer caches" )
+        log.info("Mottok hendelse av tyoe fra PDL ${hendelse.adressebeskyttelse?.gradering?.name}, tømmer cacher" )
         PDL_CACHES.forEach { cache ->
             hendelse.personidenter.forEach { id ->
-              client.delete(cache, id)
-                log.trace(CONFIDENTIAL, "Slettet ident {} etter hendelse: {}", id, hendelse)
+              if (client.delete(cache, id) > 0) {
+                log.trace( "Slettet ident {} fra ${cache.name} etter hendelse av type: {}", id.maskFnr(), hendelse.adressebeskyttelse?.gradering?.name)
+              }
+                else {
+                log.trace( "Fant ikke ident {} i ${cache.name} for sletting ved hendelse av type: {}", id.maskFnr(), hendelse.adressebeskyttelse?.gradering?.name)
+              }
             }
         }
     }

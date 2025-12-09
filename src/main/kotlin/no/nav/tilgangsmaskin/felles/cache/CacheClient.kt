cb0592a673d3b7e8ff1142f8d3d166b5a3913c31
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-12-09 21:46:41 +0100

    log

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheClient.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheClient.kt
@@ -26,7 +26,7 @@ class CacheClient(
     val log = getLogger(javaClass)
 
     @WithSpan
-    fun delete(vararg caches: CachableConfig, id: String) = caches.sumOf { cache -> conn.sync().del(handler.tilNøkkel(cache, id)) }
+    fun delete(vararg caches: CachableConfig, id: String) = caches.sumOf { cache -> conn.sync().del(handler.tilNøkkel(cache, id).also { log.info("Nøkkel er $it") }) }
     @WithSpan
     inline fun <reified T> getOne(cache: CachableConfig, id: String) =
             conn.sync().get(handler.tilNøkkel(cache,id))?.let { json ->

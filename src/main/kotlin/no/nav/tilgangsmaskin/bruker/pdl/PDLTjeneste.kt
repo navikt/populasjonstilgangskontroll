ff30bb60539109368fd9c6e22c7d2dbfcb8d4483
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-12-15 15:20:05 +0100

    observed

--- src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PDLTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PDLTjeneste.kt
@@ -1,5 +1,6 @@
 package no.nav.tilgangsmaskin.bruker.pdl
 
+import io.micrometer.observation.annotation.Observed
 import io.opentelemetry.instrumentation.annotations.WithSpan
 import no.nav.tilgangsmaskin.bruker.pdl.PdlConfig.Companion.PDL
 import no.nav.tilgangsmaskin.felles.rest.RetryingWhenRecoverable
@@ -12,10 +13,12 @@ import org.springframework.stereotype.Service
 class PDLTjeneste(private val adapter: PdlRestClientAdapter, private val graphQL: PdlSyncGraphQLClientAdapter) {
 
     @WithSpan
+    @Observed
     @Cacheable(cacheNames = [PDL],  key = "#root.methodName + ':' + #id")
     fun medUtvidetFamile(id: String) = adapter.medUtvidetFamile(id,graphQL.partnere(id))
 
     @WithSpan
+    @Observed
     @Cacheable(cacheNames = [PDL],  key = "#root.methodName + ':' + #id")
     fun medFamilie(id: String) = adapter.person(id)
 

0959645ba159931cba33de58681134f44eb89fee
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-12-16 17:04:19 +0100

    støtt aktørid

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingTjeneste.kt
@@ -1,6 +1,7 @@
 package no.nav.tilgangsmaskin.ansatt.oppfølging
 
 import no.nav.tilgangsmaskin.ansatt.nom.NomConfig.Companion.OPPFØLGING
+import no.nav.tilgangsmaskin.ansatt.oppfølging.OppfølgingConfig.Companion.OPPFØLGING
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 

93b4379c4102c741b95549198cc9a641042e1f24
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-12-16 11:30:18 +0100

    oppfølging migration

---
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRepository.kt
@@ -0,0 +1,43 @@
+package no.nav.tilgangsmaskin.ansatt.`oppfølging`
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import no.nav.tilgangsmaskin.bruker.AktørId.Companion.AKTØRID_LENGTH
+import no.nav.tilgangsmaskin.bruker.BrukerId.Companion.BRUKERID_LENGTH
+import org.hibernate.annotations.NaturalId
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.repository.JpaRepository
+import java.time.Instant
+import java.util.UUID
+
+interface `OppfølgingRepository`: JpaRepository<OppfølgingEntity, UUID>
+@Entity
+@Table(name = "oppfolging")
+class OppfølgingEntity( @NaturalId @Id val id: UUID) {
+
+    @Column(length = BRUKERID_LENGTH, nullable = false)
+    var brukerid: String? = null
+
+    @Column(length = AKTØRID_LENGTH, nullable = false)
+    var aktoerid: String? = null
+
+    @Column(nullable = false)
+    var startTidspunkt: Instant? = null
+
+    @Column(length = 4, nullable = false)
+    var kontor: String? = null
+
+    @Column
+    var sluttTidspunkt: Instant? = null
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    var created: Instant? = null
+
+    @Column(nullable = false)
+    @LastModifiedDate
+    var updated: Instant? = null
+}

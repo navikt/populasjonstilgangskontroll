935dd0534d6e722c56d13265d3fea356bc276a8c
Jan-Olav Eide <jan-olav.eide@nav.no>
2025-12-16 10:34:48 +0100

    sterk typing

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingHendelseKonsument.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingHendelseKonsument.kt
@@ -1,9 +1,14 @@
 package no.nav.tilgangsmaskin.ansatt.oppfølging
 
 import no.nav.tilgangsmaskin.ansatt.oppfølging.OppfølgingConfig.Companion.OPPFØLGING
+import no.nav.tilgangsmaskin.bruker.AktørId
+import no.nav.tilgangsmaskin.bruker.BrukerId
+import no.nav.tilgangsmaskin.bruker.Enhetsnummer
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
+import java.time.Instant
+import java.util.UUID
 import kotlin.jvm.javaClass
 
 @Component
@@ -13,7 +18,7 @@ class `OppfølgingHendelseKonsument` {
     @KafkaListener(
         topics = ["poao.siste-oppfolgingsperiode-v2"],
         properties = ["spring.json.value.default.type=no.nav.tilgangsmaskin.ansatt.oppfølging.OppfølgingHendelse"],
-        groupId = OPPFØLGING)
+        groupId = "$OPPFØLGING-hendelse-konsument")
     fun listen(hendelse: OppfølgingHendelse) {
         log.info("Mottok oppfølginghendelse: $hendelse")
     }
@@ -21,16 +26,19 @@ class `OppfølgingHendelseKonsument` {
 
 data class OppfølgingHendelse(
     val kontor: Kontor,
-    val sisteEndringsType: String,
-    val oppfolgingsperiodeUuid: String,
-    val aktorId: String,
-    val ident: String,
-    val startTidspunkt: String,
-    val sluttTidspunkt: String?, // Nullable, always null on start messages
-    val producerTimestamp: String
-)
+    val sisteEndringsType: EndringType,
+    val oppfolgingsperiodeUuid: UUID,
+    val aktorId: AktørId,
+    val ident: BrukerId,
+    val startTidspunkt: Instant,
+    val sluttTidspunkt: Instant?, // Nullable, always null on start messages
+    val producerTimestamp: Instant) {
 
-data class Kontor(
-    val kontorId: String,
-    val kontorNavn: String
-)
+    enum class EndringType {
+        ARBEIDSOPPFOLGINGSKONTOR_ENDRET,
+        OPPFOLGING_STARTET,
+        OPPFOLGING_AVSLUTTET
+    }
+    data class Kontor(val kontorId: Enhetsnummer, val kontorNavn: String)
+}
+

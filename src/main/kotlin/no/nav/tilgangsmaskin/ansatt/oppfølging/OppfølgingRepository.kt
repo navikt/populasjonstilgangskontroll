84b3c96c4d717bf7c0393f877ab43382ed4f90cf
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-01-06 07:57:11 +0100

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRepository.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRepository.kt
@@ -6,10 +6,6 @@ import java.util.UUID
 interface OppfølgingRepository: JpaRepository<OppfølgingEntity, UUID>  {
     fun findByBrukerid(id: String): OppfølgingEntity?
     fun findByAktoerid(id: String): OppfølgingEntity?
-
-   // @Modifying
-   // @Query("DELETE FROM OppfølgingEntity o WHERE o.id = :id")
-   // fun deleteByUUID(id: UUID): Int
 }
 
 

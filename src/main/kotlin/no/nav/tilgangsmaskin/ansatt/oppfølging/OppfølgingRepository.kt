db30b18dd8d428b9ab504b20b671a9c63508dcfc
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-01-06 07:55:39 +0100

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRepository.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingRepository.kt
@@ -1,13 +1,9 @@
 package no.nav.tilgangsmaskin.ansatt.`oppfølging`
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
 import java.util.UUID
 
 interface OppfølgingRepository: JpaRepository<OppfølgingEntity, UUID>  {
-    @Modifying
-    @Query("UPDATE OppfølgingEntity o SET o.kontor = :kontor WHERE o.id = :id")
     fun findByBrukerid(id: String): OppfølgingEntity?
     fun findByAktoerid(id: String): OppfølgingEntity?
 

ae43fd0809eb2ecfc8aea0787f6173ed9a8cd58e
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-01-09 15:45:14 +0100

    400

--- src/main/resources/db/migration/V17__overstyring.sql
+++ src/main/resources/db/migration/V17__overstyring.sql
@@ -1,13 +1,5 @@
-ALTER TABLE overstyring
-    ADD CONSTRAINT pk_overstyring PRIMARY KEY (id);
 
 CREATE INDEX idx_gyldig ON ansatte (gyldigtil);
 
-DROP SEQUENCE ansatte_id_seq CASCADE;
-
-DROP SEQUENCE overstyring_id_seq CASCADE;
-
 ALTER TABLE overstyring
     ALTER COLUMN begrunnelse TYPE VARCHAR(400) USING (begrunnelse::VARCHAR(400));
-
-CREATE INDEX idx_overstyringentity_navid ON overstyring (navid, fnr);

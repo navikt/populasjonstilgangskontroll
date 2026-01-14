2719c1067d996137fb8cd4c7fefbcea136962e49
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-01-14 14:30:41 +0100

    proxy

---
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyBeanConfig.kt
@@ -0,0 +1,21 @@
+package no.nav.tilgangsmaskin.ansatt.entraproxy
+
+import no.nav.tilgangsmaskin.ansatt.entraproxy.EntraProxyConfig.Companion.ENTRAPROXY
+import no.nav.tilgangsmaskin.felles.rest.PingableHealthIndicator
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.client.RestClient.Builder
+
+@Configuration
+class EntraProxyBeanConfig {
+
+    @Bean
+    @Qualifier(ENTRAPROXY)
+    fun proxyRestClient(b: Builder, cfg: EntraProxyConfig) =
+        b.baseUrl(cfg.baseUri).build()
+
+    @Bean
+    fun proxyHealthIndicator(a: EntraProxyRestClientAdapter) =  PingableHealthIndicator(a)
+
+}
---
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyConfig.kt
@@ -0,0 +1,29 @@
+package no.nav.tilgangsmaskin.ansatt.entraproxy
+
+import no.nav.tilgangsmaskin.ansatt.entraproxy.EntraProxyConfig.Companion.ENTRAPROXY
+import no.nav.tilgangsmaskin.ansatt.graph.EntraConfig.Companion.GRAPH
+import no.nav.tilgangsmaskin.felles.rest.AbstractRestConfig
+import org.springframework.boot.context.properties.ConfigurationProperties
+import java.net.URI
+
+@ConfigurationProperties(ENTRAPROXY)
+class EntraProxyConfig(
+    baseUri: URI = DEFAULT_URI,
+    pingPath: String = DEFAULT_PING_PATH,
+    enabled: Boolean = true) :  AbstractRestConfig(baseUri, pingPath, GRAPH, enabled) {
+
+
+    fun brukerURI(navIdent: String) = builder().apply {
+        path(USERS_PATH)
+    }.build(navIdent)
+
+
+    override fun toString() = "$javaClass.simpleName [baseUri=$baseUri, pingEndpoint=$pingEndpoint]"
+
+    companion object {
+        private val DEFAULT_URI = URI.create("http://entra-proxy.sikkerhetstjenesten")
+        const val ENTRAPROXY = "entra-proxy"
+        private const val USERS_PATH = "/api/v1/ansatt/{navIdent}"
+        private const val DEFAULT_PING_PATH = "/monitoring/health/liveness"
+    }
+}

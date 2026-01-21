61a87814dcd1dc63e0fe422b41a21b2e9b369d9b
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-01-21 15:32:18 +0100

    byttet navn

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/RegelMotorLogger.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/RegelMotorLogger.kt
@@ -33,14 +33,14 @@ class RegelMotorLogger(private val registry: MeterRegistry, private val token: T
 
 
     fun avvist(ansatt: Ansatt, bruker: Bruker, regelSett: RegelSett, regel: Regel,type: EvalueringType) =
-        withMDC(Pair(BESLUTNING, regel.kode),Pair(REGELSETT, regelSett.type.beskrivelse),Pair(EVALTYPE, type.name)) {
+        withMDC(Pair(BESLUTNING, regel.kode),Pair(REGELSETT, regelSett.type.beskrivelse),Pair(OPPSLAGTYPE, type.name)) {
             log.info("Tilgang avvist av regel '${regel.kortNavn}'. (${regel.begrunnelse}) for ${ansatt.ansattId} for ${bruker.brukerId} ${konsument()}")
             auditor.info("Tilgang til ${bruker.oppslagId} med GT '${bruker.geografiskTilknytning}' avvist av regel '${regel.kortNavn}' for ${ansatt.ansattId} med gruppetilhørigheter '${ansatt.grupper.map { it.displayName }}' ${konsument()}")
             teller.tell(TILGANG_AVVIST_TAG, beskrivelseTag(regelSett),regelTag(regel),tokenTag(token),evaltypeTag(type))
         }
 
     fun ok(ansatt: Ansatt, bruker: Bruker,regelSett: RegelSett, type: EvalueringType) =
-        withMDC(Pair(BESLUTNING, OK),Pair(REGELSETT, regelSett.type.beskrivelse),Pair(EVALTYPE, type.name)) {
+        withMDC(Pair(BESLUTNING, OK),Pair(REGELSETT, regelSett.type.beskrivelse),Pair(OPPSLAGTYPE, type.name)) {
             log.info("${regelSett.beskrivelse} ga tilgang for ${ansatt.ansattId} ${konsument()}")
             auditor.info("${regelSett.beskrivelse} ga tilgang til ${bruker.oppslagId} for ${ansatt.ansattId} ${konsument()}")
             teller.tell(TILGANG_AKSEPTERT_TAG, beskrivelseTag(regelSett),INGEN_REGEL_TAG,tokenTag(token),evaltypeTag(type))
@@ -57,12 +57,12 @@ class RegelMotorLogger(private val registry: MeterRegistry, private val token: T
 
     companion object   {
         private fun konsument(): String = MDC.get(CONSUMER_ID)?.let { "fra $it" } ?: "(fra uautentisert konsument)"
-        private fun evaltypeTag(type: EvalueringType) = Tag.of(EVALTYPE, type.name.lowercase())
+        private fun evaltypeTag(type: EvalueringType) = Tag.of(OPPSLAGTYPE, type.name.lowercase())
         private fun beskrivelseTag(regelsett: RegelSett) = Tag.of(BESKRIVELSE, regelsett.beskrivelse)
         private val TILGANG_AKSEPTERT_TAG = Tag.of(RESULTAT, OK)
         private val TILGANG_AVVIST_TAG = Tag.of(RESULTAT, AVVIST)
         private const val BESKRIVELSE = "type"
-        private const val EVALTYPE = "evalueringtype"
+        private const val OPPSLAGTYPE = "oppslagtype"
         private const val REGELSETT = "regelsett"
         private const val RESULTAT = "resultat"
         private const val BESLUTNING = "beslutning"

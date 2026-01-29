d1ae3c33c04cea4bfd453fec43279eaa6bfbc47e
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-01-29 16:39:36 +0100

    retry på socket timeout

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RetryingWhenRecoverable.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RetryingWhenRecoverable.kt
@@ -4,6 +4,7 @@ import org.springframework.core.annotation.AliasFor
 import org.springframework.resilience.annotation.Retryable
 import org.springframework.web.client.ResourceAccessException
 import java.lang.annotation.Inherited
+import java.net.SocketTimeoutException
 import kotlin.annotation.AnnotationRetention.RUNTIME
 import kotlin.annotation.AnnotationTarget.CLASS
 import kotlin.annotation.AnnotationTarget.FUNCTION
@@ -15,5 +16,5 @@ import kotlin.reflect.KClass
 @Inherited
 @MustBeDocumented
 annotation class RetryingWhenRecoverable(
-    @get:AliasFor(annotation = Retryable::class) val value: Array<KClass<out Throwable>> = [RecoverableRestException::class, ResourceAccessException::class]
+    @get:AliasFor(annotation = Retryable::class) val value: Array<KClass<out Throwable>> = [RecoverableRestException::class, SocketTimeoutException::class, ResourceAccessException::class]
 )

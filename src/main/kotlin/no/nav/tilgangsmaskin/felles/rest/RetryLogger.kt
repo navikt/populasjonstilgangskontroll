27d887635849567750a268c6c1c021e330494b74
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-10 12:34:46 +0100

    bruk set

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RetryLogger.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RetryLogger.kt
@@ -2,7 +2,6 @@ package no.nav.tilgangsmaskin.felles.rest
 
 import org.slf4j.LoggerFactory
 import org.springframework.context.event.EventListener
-import org.springframework.http.HttpStatus
 import org.springframework.resilience.retry.MethodRetryEvent
 import org.springframework.stereotype.Component
 
@@ -12,17 +11,16 @@ class RetryLogger {
 
     @EventListener(MethodRetryEvent::class)
     fun onEvent(event: MethodRetryEvent) {
-        val failure = event.failure
-        if (failure is NotFoundRestException) {
-            log.info("Aborterer metode ${event.method.name} siden ${failure.identifikator} ikke ble funnet på ${failure.uri}", failure)
-        }
-        else {
-            if (event.isRetryAborted) {
+        val failure = (event.failure as? NotFoundRestException)
+            ?: (event.failure.cause as? NotFoundRestException)
+            ?: event.failure
+        when {
+            failure is NotFoundRestException ->
+                log.info("Aborterer metode ${event.method.name} siden ${failure.identifikator} ikke ble funnet på ${failure.uri}", failure)
+            event.isRetryAborted ->
                 log.warn("Aborterer metode ${event.method.name} grunnet ${failure.javaClass.simpleName}", failure)
-            }
-            else {
+            else ->
                 log.warn("Feil i metode ${event.method.name}, prøver igjen", failure)
-            }
         }
     }
 }

111df37e79281c569e6eccfd73fa3954bfab08ac
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-11 16:35:02 +0100

    otel starter gone

--- build.gradle.kts
+++ build.gradle.kts
@@ -82,9 +82,6 @@ dependencies {
     implementation("org.postgresql:postgresql")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:$springdocVersion")
     implementation("org.springframework.boot:spring-boot-starter-flyway")
-    implementation("org.springframework.boot:spring-boot-starter-opentelemetry") {
-        exclude(group = "io.opentelemetry", module = "opentelemetry-exporter-otlp")
-    }
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-cache")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")

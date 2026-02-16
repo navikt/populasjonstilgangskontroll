f5c5e1d8679af615af5deeba6e6f4a40b385eeb3
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-16 09:02:18 +0100

    any and log

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingHendelse.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingHendelse.kt
@@ -1,7 +1,5 @@
 package no.nav.tilgangsmaskin.ansatt.`oppfølging`
 
-import com.fasterxml.jackson.annotation.JsonCreator
-import com.fasterxml.jackson.annotation.JsonProperty
 import no.nav.tilgangsmaskin.bruker.AktørId
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.bruker.Enhetsnummer
@@ -15,7 +13,7 @@ data class OppfølgingHendelse(
     val aktorId: AktørId,
     val ident: BrukerId,
     val startTidspunkt: Instant,
-    val sluttTidspunkt: Instant?,
+    val sluttTidspunkt: Instant?, // Nullable, always null on start messages
     val producerTimestamp: Instant) {
 
     enum class EndringType {
@@ -23,35 +21,5 @@ data class OppfølgingHendelse(
         OPPFOLGING_STARTET,
         OPPFOLGING_AVSLUTTET
     }
-
-    data class Kontor @JsonCreator constructor(
-        @JsonProperty("kontorId") kontorId: String,
-        @JsonProperty("kontorNavn") val kontorNavn: String = "Ukjent"
-    ) {
-        val kontorId: Enhetsnummer = Enhetsnummer(kontorId)
-    }
-
-    companion object {
-        @JvmStatic
-        @JsonCreator
-        fun create(
-            @JsonProperty("kontor") kontor: Kontor?,
-            @JsonProperty("sisteEndringsType") sisteEndringsType: EndringType,
-            @JsonProperty("oppfolgingsperiodeUuid") oppfolgingsperiodeUuid: UUID,
-            @JsonProperty("aktorId") aktorId: String,
-            @JsonProperty("ident") ident: String,
-            @JsonProperty("startTidspunkt") startTidspunkt: Instant,
-            @JsonProperty("sluttTidspunkt") sluttTidspunkt: Instant?,
-            @JsonProperty("producerTimestamp") producerTimestamp: Instant
-        ) = OppfølgingHendelse(
-            kontor = kontor,
-            sisteEndringsType = sisteEndringsType,
-            oppfolgingsperiodeUuid = oppfolgingsperiodeUuid,
-            aktorId = AktørId(aktorId),
-            ident = BrukerId(ident),
-            startTidspunkt = startTidspunkt,
-            sluttTidspunkt = sluttTidspunkt,
-            producerTimestamp = producerTimestamp
-        )
-    }
+    data class Kontor(val kontorId: Enhetsnummer, val kontorNavn: String = "Ukjent")
 }

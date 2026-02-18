a11cbbb98256e97c53a4684aed4275f352057539
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-18 10:48:31 +0100

    reset topic

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingHendelseKonsument.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/oppfølging/OppfølgingHendelseKonsument.kt
@@ -20,7 +20,7 @@ class OppfølgingHendelseKonsument(private val oppfølging: OppfølgingTjeneste)
     @KafkaListener(
         topics = ["poao.siste-oppfolgingsperiode-v2"],
         properties = ["spring.json.value.default.type=no.nav.tilgangsmaskin.ansatt.oppfølging.OppfølgingHendelse"],
-        groupId = OPPFØLGING)
+        groupId = OPPFØLGING + "test123")
 
     fun listen(hendelse: OppfølgingHendelse) {
         when (hendelse.sisteEndringsType) {

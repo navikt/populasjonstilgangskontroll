f913fa6cada5688c47bf2610910d54af0fc1132f
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-19 16:36:59 +0100

    bump boot

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,7 @@ plugins {
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.spring") version kotlinVersion
     kotlin("plugin.jpa") version kotlinVersion
-    id("org.springframework.boot") version "4.0.2"
+    id("org.springframework.boot") version "4.0.3"
     id("io.spring.dependency-management") version "1.1.7"
     id("org.cyclonedx.bom") version "3.0.0"
     id("com.google.cloud.tools.jib") version "3.5.3"

1e92199ee8cad1e2fda0f83b1984da2a1daaf349
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-02-25 13:06:11 +0100

    bedre logging av abort retry

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RetryLogger.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RetryLogger.kt
@@ -2,6 +2,7 @@ package no.nav.tilgangsmaskin.felles.rest
 
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.context.event.EventListener
+import org.springframework.core.retry.RetryException
 import org.springframework.resilience.retry.MethodRetryEvent
 import org.springframework.stereotype.Component
 
@@ -17,9 +18,15 @@ class RetryLogger {
         log.info("Retry event $event",event.failure)
         if (failure is NotFoundRestException)
             log.info("Ikke funnet exception cause fra '${event.method.name}' for [${failure.identifikator}] på ${failure.uri} (${event.isRetryAborted}", event.failure)
-        else if (event.isRetryAborted)
-            log.warn("Aborterer metode '${event.method.name}' grunnet ${failure.javaClass.simpleName} (${event.source.arguments.toSet()}", failure)
-        else
+        else if (event.isRetryAborted) {
+            if (failure !is RetryException) {
+                log.warn("Aborterer metode '${event.method.name}' grunnet ${failure.javaClass.simpleName} (${event.source.arguments.toSet()}", failure) // Skjer dette noen gang? Det er vel bare 
+            } else {
+                log.warn("Aborterer metode '${event.method.name}' grunnet ${failure.cause?.javaClass?.simpleName} (${event.source.arguments.toSet()}", failure.cause)
+            }
+        }
+        else {
             log.warn("Feil i metode '${event.method.name}',(${event.source.arguments.toSet()})  prøver igjen", failure)
+        }
     }
 }

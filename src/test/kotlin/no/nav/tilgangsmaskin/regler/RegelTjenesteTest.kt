1e4b0782e430d36dc896cb739e7fe05c5489f5c0
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-03-10 14:23:35 +0100

    kotestify

--- src/test/kotlin/no/nav/tilgangsmaskin/regler/RegelTjenesteTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/regler/RegelTjenesteTest.kt
@@ -1,8 +1,14 @@
 package no.nav.tilgangsmaskin.regler
 
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.collections.shouldBeEmpty
+import io.kotest.matchers.shouldBe
+import io.mockk.MockKAnnotations
+import io.mockk.clearAllMocks
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.ansatt.AnsattTjeneste
@@ -16,114 +22,101 @@ import no.nav.tilgangsmaskin.regler.motor.RegelException
 import no.nav.tilgangsmaskin.regler.motor.RegelMotor
 import no.nav.tilgangsmaskin.regler.overstyring.OverstyringTjeneste
 import no.nav.tilgangsmaskin.tilgang.RegelTjeneste
-import org.assertj.core.api.Assertions.assertThat
-import org.assertj.core.api.Assertions.assertThatCode
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.extension.ExtendWith
 import java.net.URI
-import kotlin.test.BeforeTest
 
-@ExtendWith(MockKExtension::class)
-class RegelTjenesteTest {
-
-    private val vanligBrukerId = BrukerId("08526835670")
-    private val ansattId = AnsattId("Z999999")
+class RegelTjenesteTest : DescribeSpec() {
 
     @MockK
-    private lateinit var motor: RegelMotor
+    lateinit var motor: RegelMotor
     @MockK
-    private lateinit var brukere: BrukerTjeneste
+    lateinit var brukere: BrukerTjeneste
     @MockK
-    private lateinit var ansatte: AnsattTjeneste
+    lateinit var ansatte: AnsattTjeneste
     @MockK
-    private lateinit var overstyring: OverstyringTjeneste
-    private lateinit var regler: RegelTjeneste
+    lateinit var overstyring: OverstyringTjeneste
 
-    @BeforeTest
-    fun before() {
-        every { ansatte.ansatt(ansattId) } returns AnsattBuilder(ansattId).build()
-        regler = RegelTjeneste(motor, brukere, ansatte, overstyring)
-    }
+    init {
+        val vanligBrukerId = BrukerId("08526835670")
+        val ansattId = AnsattId("Z999999")
 
-    @Test
-    @DisplayName("Verifiser at avvist bruker i bulk havner i godkjente når overstyring er registrert")
-    fun avvistMedOverstyringHavnerIGodkjente() {
-        val funnetBruker = BrukerBuilder(vanligBrukerId).build()
-        val ansatt = AnsattBuilder(ansattId).build()
-        val regelException = RegelException(ansatt, funnetBruker, mockk<Regel>(relaxed = true))
-        every { brukere.brukere(setOf(vanligBrukerId.verdi)) } returns setOf(funnetBruker)
-        every { motor.bulkRegler(any(), any()) } returns setOf(BulkResultat.avvist(funnetBruker, regelException))
-        every { overstyring.overstyringer(any(), any()) } returns listOf(vanligBrukerId)
+        lateinit var regler: RegelTjeneste
 
-        val resultater = regler.bulkRegler(ansattId, setOf(BrukerIdOgRegelsett(vanligBrukerId.verdi)))
+        beforeSpec {
+            MockKAnnotations.init(this@RegelTjenesteTest)
+        }
 
-        assertThat(resultater.godkjente).hasSize(1)
-        assertThat(resultater.godkjente.single().brukerId).isEqualTo(vanligBrukerId.verdi)
-        assertThat(resultater.avviste).isEmpty()
-        assertThat(resultater.ukjente).isEmpty()
-    }
+        beforeEach {
+            clearAllMocks()
+            every { ansatte.ansatt(ansattId) } returns AnsattBuilder(ansattId).build()
+            regler = RegelTjeneste(motor, brukere, ansatte, overstyring)
+        }
 
-    @Test
-    @DisplayName("Verifiser at bruker havner i avviste når motor avviser og ingen overstyring er registrert")
-    fun avvistUtenOverstyringHavnerIAvviste() {
-        val funnetBruker = BrukerBuilder(vanligBrukerId).build()
-        val ansatt = AnsattBuilder(ansattId).build()
-        val regelException = RegelException(ansatt, funnetBruker, mockk<Regel>(relaxed = true))
-        every { brukere.brukere(setOf(vanligBrukerId.verdi)) } returns setOf(funnetBruker)
-        every { motor.bulkRegler(any(), any()) } returns setOf(BulkResultat.avvist(funnetBruker, regelException))
-        every { overstyring.overstyringer(any(), any()) } returns emptyList()
+        describe("bulk") {
 
-        val resultater = regler.bulkRegler(ansattId, setOf(BrukerIdOgRegelsett(vanligBrukerId.verdi)))
+            it("avvist bruker havner i godkjente når overstyring er registrert") {
+                val funnetBruker = BrukerBuilder(vanligBrukerId).build()
+                val regelException = RegelException(AnsattBuilder(ansattId).build(), funnetBruker, mockk<Regel>(relaxed = true))
+                every { brukere.brukere(setOf(vanligBrukerId.verdi)) } returns setOf(funnetBruker)
+                every { motor.bulkRegler(any(), any()) } returns setOf(BulkResultat.avvist(funnetBruker, regelException))
+                every { overstyring.overstyringer(any(), any()) } returns listOf(vanligBrukerId)
 
-        assertThat(resultater.avviste).hasSize(1)
-        assertThat(resultater.avviste.single().brukerId).isEqualTo(vanligBrukerId.verdi)
-        assertThat(resultater.godkjente).isEmpty()
-        assertThat(resultater.ukjente).isEmpty()
-    }
+                val resultater = regler.bulkRegler(ansattId, setOf(BrukerIdOgRegelsett(vanligBrukerId.verdi)))
 
-    @Test
-    @DisplayName("Verifiser at id som ikke finnes i PDL får tilgang i bulk")
-    fun ikkeFunnetIPdlFårTilgangIBulk() {
-        val ikkeFunnetId = BrukerId("11111111111")
-        val funnetBruker = BrukerBuilder(vanligBrukerId).build()
-        every {
-            brukere.brukere(setOf(vanligBrukerId.verdi, ikkeFunnetId.verdi))
-        } returns setOf(funnetBruker)
-        every { motor.bulkRegler(any(), any()) } returns setOf(BulkResultat.ok(funnetBruker))
-        every { overstyring.overstyringer(any(), any()) } returns emptyList()
+                resultater.godkjente.shouldBe(setOf(resultater.godkjente.single()))
+                resultater.godkjente.single().brukerId shouldBe vanligBrukerId.verdi
+                resultater.avviste.shouldBeEmpty()
+                resultater.ukjente.shouldBeEmpty()
+            }
 
-        val resultater = regler.bulkRegler(ansattId,
-            setOf(BrukerIdOgRegelsett(vanligBrukerId.verdi), BrukerIdOgRegelsett(ikkeFunnetId.verdi)))
+            it("avvist bruker havner i avviste når ingen overstyring er registrert") {
+                val funnetBruker = BrukerBuilder(vanligBrukerId).build()
+                val regelException = RegelException(AnsattBuilder(ansattId).build(), funnetBruker, mockk<Regel>(relaxed = true))
+                every { brukere.brukere(setOf(vanligBrukerId.verdi)) } returns setOf(funnetBruker)
+                every { motor.bulkRegler(any(), any()) } returns setOf(BulkResultat.avvist(funnetBruker, regelException))
+                every { overstyring.overstyringer(any(), any()) } returns emptyList()
 
-        assertThat(resultater.godkjente).hasSize(2)
-        assertThat(resultater.godkjente.map { it.brukerId })
-            .containsExactlyInAnyOrder(vanligBrukerId.verdi, ikkeFunnetId.verdi)
-        assertThat(resultater.avviste).isEmpty()
-        assertThat(resultater.ukjente).isEmpty()
-    }
+                val resultater = regler.bulkRegler(ansattId, setOf(BrukerIdOgRegelsett(vanligBrukerId.verdi)))
 
-    @Test
-    @DisplayName("Verifiser at tilgang gis når bruker ikke finnes i PDL ved kjerneregelssjekk")
-    fun brukerIkkeFunnetIPdlKjerneregler() {
-        every {
-            brukere.brukerMedNærmesteFamilie(vanligBrukerId.verdi)
-        } throws NotFoundRestException(URI.create("http://pdl"))
+                resultater.avviste.single().brukerId shouldBe vanligBrukerId.verdi
+                resultater.godkjente.shouldBeEmpty()
+                resultater.ukjente.shouldBeEmpty()
+            }
 
-        assertThatCode {
-            regler.kjerneregler(ansattId, vanligBrukerId.verdi)
-        }.doesNotThrowAnyException()
-    }
+            it("id som ikke finnes i PDL får tilgang") {
+                val ikkeFunnetId = BrukerId("11111111111")
+                val funnetBruker = BrukerBuilder(vanligBrukerId).build()
+                every { brukere.brukere(setOf(vanligBrukerId.verdi, ikkeFunnetId.verdi)) } returns setOf(funnetBruker)
+                every { motor.bulkRegler(any(), any()) } returns setOf(BulkResultat.ok(funnetBruker))
+                every { overstyring.overstyringer(any(), any()) } returns emptyList()
 
-    @Test
-    @DisplayName("Verifiser at tilgang gis når bruker ikke finnes i PDL ved komplett regelsjekk")
-    fun brukerIkkeFunnetIPdl() {
-        every {
-            brukere.brukerMedNærmesteFamilie(vanligBrukerId.verdi)
-        } throws NotFoundRestException(URI.create("http://pdl"))
+                val resultater = regler.bulkRegler(ansattId,
+                    setOf(BrukerIdOgRegelsett(vanligBrukerId.verdi), BrukerIdOgRegelsett(ikkeFunnetId.verdi)))
 
-        assertThatCode {
-            regler.kompletteRegler(ansattId, vanligBrukerId.verdi)
-        }.doesNotThrowAnyException()
+                resultater.godkjente.map { it.brukerId } shouldContainExactlyInAnyOrder
+                    listOf(vanligBrukerId.verdi, ikkeFunnetId.verdi)
+                resultater.avviste.shouldBeEmpty()
+                resultater.ukjente.shouldBeEmpty()
+            }
+        }
+
+        describe("kjerneregler") {
+
+            it("tilgang gis når bruker ikke finnes i PDL") {
+                every { brukere.brukerMedNærmesteFamilie(vanligBrukerId.verdi) } throws
+                    NotFoundRestException(URI.create("http://pdl"))
+
+                shouldNotThrowAny { regler.kjerneregler(ansattId, vanligBrukerId.verdi) }
+            }
+        }
+
+        describe("kompletteRegler") {
+
+            it("tilgang gis når bruker ikke finnes i PDL") {
+                every { brukere.brukerMedNærmesteFamilie(vanligBrukerId.verdi) } throws
+                    NotFoundRestException(URI.create("http://pdl"))
+
+                shouldNotThrowAny { regler.kompletteRegler(ansattId, vanligBrukerId.verdi) }
+            }
+        }
     }
 }

38374bbd86d808a4d7880d1b55c8f76ec9845c71
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-03-11 10:38:05 +0100

    test entra proxy

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyTjeneste.kt
@@ -1,16 +1,10 @@
 package no.nav.tilgangsmaskin.ansatt.entraproxy
 
-import io.micrometer.core.annotation.Timed
 import io.opentelemetry.instrumentation.annotations.WithSpan
-import jakarta.annotation.Generated
+import javax.annotation.processing.Generated
 import no.nav.tilgangsmaskin.ansatt.AnsattId
-import no.nav.tilgangsmaskin.ansatt.AnsattOidTjeneste
-import no.nav.tilgangsmaskin.ansatt.graph.EntraConfig.Companion.GRAPH
-import no.nav.tilgangsmaskin.ansatt.graph.EntraRestClientAdapter
 import no.nav.tilgangsmaskin.felles.rest.RetryingWhenRecoverable
-import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
-import java.util.*
 
 @RetryingWhenRecoverable
 @Service

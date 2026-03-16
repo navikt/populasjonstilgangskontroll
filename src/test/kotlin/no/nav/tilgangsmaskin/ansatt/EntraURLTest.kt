3cf3c5b43f0a86a0548b05073d0e1d78599b16f7
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-03-16 09:18:47 +0100

    test brukertjeneste

--- src/test/kotlin/no/nav/tilgangsmaskin/ansatt/EntraURLTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/ansatt/EntraURLTest.kt
@@ -1,12 +1,13 @@
 package no.nav.tilgangsmaskin.ansatt
 
+import io.kotest.core.extensions.ApplyExtension
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.shouldBe
 import no.nav.tilgangsmaskin.TestApp
 import no.nav.tilgangsmaskin.ansatt.graph.EntraConfig
 import no.nav.tilgangsmaskin.ansatt.graph.EntraConfig.Companion.GEO_PREFIX
 import no.nav.tilgangsmaskin.regler.motor.GlobaleGrupperConfig
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.restclient.test.autoconfigure.RestClientTest
@@ -19,17 +20,23 @@ import java.net.URI
 @TestPropertySource(locations = ["classpath:test.properties"])
 @EnableConfigurationProperties(GlobaleGrupperConfig::class)
 @ContextConfiguration(classes = [TestApp::class])
-class EntraURL {
+@ApplyExtension(SpringExtension::class)
+class EntraURLTest : DescribeSpec() {
 
     @Autowired
     private lateinit var env: Environment
 
-    @Test
-    @DisplayName("UUIDene til gruppene kommer i rett formatering")
-    fun `GrupperCcfURI should correctly format PARAM_NAME_FILTER`() {
-        val globaleGrupper = GlobalGruppe.entries.map { env.getProperty(it.property) }.joinToString(",") { "'$it'" }
-        val expectedFilter = "id in($globaleGrupper) or $GEO_PREFIX"
-        val actualFilter = EntraConfig(baseUri = URI("https://example.com")).grupperURI("Z999999", true).query.substringAfter("\$filter=").substringBefore("&")
-        assertEquals(expectedFilter, actualFilter, "PARAM_NAME_FILTER is not formatted correctly")
+    init {
+        describe("grupperURI") {
+            it("UUIDene til gruppene kommer i rett formatering") {
+                val globaleGrupper = GlobalGruppe.entries.map { env.getProperty(it.property) }.joinToString(",") { "'$it'" }
+                val expectedFilter = "id in($globaleGrupper) or $GEO_PREFIX"
+                val actualFilter = EntraConfig(baseUri = URI("https://example.com"))
+                    .grupperURI("Z999999", true).query
+                    .substringAfter("\$filter=").substringBefore("&")
+
+                actualFilter shouldBe expectedFilter
+            }
+        }
     }
 }

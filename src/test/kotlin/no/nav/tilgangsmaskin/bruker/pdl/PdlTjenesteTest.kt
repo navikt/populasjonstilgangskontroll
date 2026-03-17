c4a050f0ea875d3aee2457762d6ee0d855c3cc5f
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-03-17 13:48:45 +0100

    some test mappers

---
+++ src/test/kotlin/no/nav/tilgangsmaskin/bruker/pdl/PdlTjenesteTest.kt
@@ -0,0 +1,127 @@
+package no.nav.tilgangsmaskin.bruker.pdl
+
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.extensions.ApplyExtension
+import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import no.nav.tilgangsmaskin.bruker.AktørId
+import no.nav.tilgangsmaskin.bruker.BrukerId
+import no.nav.tilgangsmaskin.bruker.GeografiskTilknytning.Kommune
+import no.nav.tilgangsmaskin.bruker.GeografiskTilknytning.KommuneTilknytning
+import no.nav.tilgangsmaskin.bruker.pdl.BrukerTilPersonMapper.tilPerson
+import no.nav.tilgangsmaskin.bruker.pdl.PdlConfig.Companion.PDL
+import no.nav.tilgangsmaskin.bruker.pdl.PdlConfig.Companion.PDL_MED_FAMILIE_CACHE
+import no.nav.tilgangsmaskin.bruker.pdl.PdlTjenesteTest.PdlTestConfig
+import no.nav.tilgangsmaskin.felles.cache.CacheOperations
+import no.nav.tilgangsmaskin.felles.cache.ConcurrentMapCacheOperations
+import no.nav.tilgangsmaskin.regler.BrukerBuilder
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.restclient.test.autoconfigure.RestClientTest
+import org.springframework.boot.test.context.TestConfiguration
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Import
+import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.resilience.annotation.EnableResilientMethods
+import org.springframework.test.context.TestPropertySource
+import org.springframework.test.web.client.MockRestServiceServer
+import org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo
+import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
+import org.springframework.web.client.RestClient
+import tools.jackson.databind.json.JsonMapper
+import java.time.Duration.ofSeconds
+
+@RestClientTest(components = [PdlRestClientAdapter::class, PdlTjeneste::class])
+@EnableConfigurationProperties(PdlConfig::class)
+@Import(PdlTestConfig::class)
+@TestPropertySource(properties = ["pdl.base-uri=http://pdl"])
+@EnableResilientMethods
+@ApplyExtension(SpringExtension::class)
+class PdlTjenesteTest : DescribeSpec() {
+
+    @TestConfiguration
+    @EnableCaching
+    class PdlTestConfig {
+        @Bean fun cacheManager(): CacheManager = ConcurrentMapCacheManager(PDL)
+        @Bean fun cache(cacheManager: CacheManager): CacheOperations = ConcurrentMapCacheOperations(cacheManager)
+
+        @Bean
+        @Qualifier(PDL)
+        fun pdlRestClient(b: RestClient.Builder) = b.build()
+    }
+
+    @MockkBean lateinit var graphQL: PdlSyncGraphQLClientAdapter
+
+    @Autowired lateinit var tjeneste: PdlTjeneste
+    @Autowired
+    lateinit var server: MockRestServiceServer
+    @Autowired lateinit var cfg: PdlConfig
+    @Autowired lateinit var cacheManager: CacheManager
+    @Autowired lateinit var cache: CacheOperations
+    @Autowired lateinit var mapper: JsonMapper
+
+    init {
+        beforeEach {
+            server.reset()
+            cacheManager.getCache(PDL)?.clear()
+            every { graphQL.partnere(any()) } returns emptySet()
+        }
+
+        describe("personer") {
+
+            it("REST kalles kun for cache-misser, treff hentes fra cache") {
+                cache.putOne(PDL_MED_FAMILIE_CACHE, I1, P1, ofSeconds(2))
+                server.expect(requestTo(cfg.personerURI))
+                    .andRespond(withSuccess(PdlTestMapper.restRespons(mapper, P2), APPLICATION_JSON))
+                tjeneste.personer(IDS) shouldContainExactlyInAnyOrder listOf(P1, P2)
+                server.verify()
+            }
+
+            it("REST kalles ikke når alle er i cache") {
+                cache.putOne(PDL_MED_FAMILIE_CACHE, I1, P1, ofSeconds(2))
+                cache.putOne(PDL_MED_FAMILIE_CACHE, I2, P2, ofSeconds(2))
+                tjeneste.personer(IDS) shouldContainExactlyInAnyOrder listOf(P1, P2)
+                server.verify()
+            }
+
+            it("slett ett element og verifiser at REST kalles for det elementet") {
+                cache.putOne(PDL_MED_FAMILIE_CACHE, I1, P1, ofSeconds(2))
+                cache.putOne(PDL_MED_FAMILIE_CACHE, I2, P2, ofSeconds(2))
+                tjeneste.personer(IDS) shouldContainExactlyInAnyOrder listOf(P1, P2)
+                cache.delete(PDL_MED_FAMILIE_CACHE, I2)
+                server.expect(requestTo(cfg.personerURI))
+                    .andRespond(withSuccess(PdlTestMapper.restRespons(mapper, P2), APPLICATION_JSON))
+
+                tjeneste.personer(IDS) shouldContainExactlyInAnyOrder listOf(P1, P2)
+                cache.getOne(PDL_MED_FAMILIE_CACHE, I2, Person::class) shouldBe P2
+                server.verify()
+            }
+
+            it("REST kalles ikke når settet er tomt") {
+                tjeneste.personer(emptySet()) shouldContainExactlyInAnyOrder emptyList()
+                server.verify()
+            }
+        }
+    }
+
+    companion object {
+        const val I1 = "03508331575"
+        const val I2 = "20478606614"
+        val IDS = setOf(I1, I2)
+        val P1 = tilPerson(BrukerBuilder(BrukerId(I1))
+            .aktørId(AktørId("1234567890123"))
+            .gt(KommuneTilknytning(Kommune("0301")))
+            .build())
+        val P2 = tilPerson(BrukerBuilder(BrukerId(I2))
+            .aktørId(AktørId("9876543210987"))
+            .gt(KommuneTilknytning(Kommune("1111")))
+            .build())
+    }
+}

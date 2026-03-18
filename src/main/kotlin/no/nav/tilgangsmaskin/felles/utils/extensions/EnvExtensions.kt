e95737cda52d994248c7f83f4cfdb06194b9db0e
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-03-18 14:33:40 +0100

    test overstyrng bedre

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/utils/extensions/EnvExtensions.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/utils/extensions/EnvExtensions.kt
@@ -1,12 +1,15 @@
 package no.nav.tilgangsmaskin.felles.utils.extensions
 
+import no.nav.tilgangsmaskin.felles.Generated
 import org.springframework.core.env.Environment
 import org.springframework.core.env.getRequiredProperty
 
+@Generated
 object EnvExtensions {
      fun Environment.schemaRegistryUrl() =
         getRequiredProperty<String>("kafka.schema.registry")
 
+    @Generated
      fun Environment.userInfo() =
         "${getRequiredProperty<String>("kafka.schema.registry.user")}:${getRequiredProperty<String>("kafka.schema.registry.password")}"
 

4fe46a69fa6825ad10e9f9999a375897cae5e51c
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-03-20 11:27:01 +0100

    assertSoftly

--- src/test/kotlin/no/nav/tilgangsmaskin/ansatt/graph/EntraCacheOppfriskerTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/ansatt/graph/EntraCacheOppfriskerTest.kt
@@ -1,9 +1,11 @@
 package no.nav.tilgangsmaskin.ansatt.graph
 
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.extensions.ApplyExtension
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.extensions.spring.SpringExtension
 import io.mockk.clearMocks
 import io.mockk.every
-import io.mockk.mockk
 import io.mockk.verify
 import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.ansatt.AnsattOidTjeneste
@@ -12,112 +14,125 @@ import no.nav.tilgangsmaskin.felles.cache.CacheClient
 import no.nav.tilgangsmaskin.felles.cache.CacheNøkkelElementer
 import no.nav.tilgangsmaskin.felles.rest.NotFoundRestException
 import no.nav.tilgangsmaskin.regler.motor.OppfriskingTeller
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.restclient.test.autoconfigure.RestClientTest
 import java.net.URI
-import java.util.*
+import java.util.UUID
 
-class EntraCacheOppfriskerTest : DescribeSpec({
+@ApplyExtension(SpringExtension::class)
+@RestClientTest(components = [EntraCacheOppfrisker::class])
+class EntraCacheOppfriskerTest : DescribeSpec() {
 
-    val entra = mockk<EntraTjeneste>(relaxed = true)
-    val oidTjeneste = mockk<AnsattOidTjeneste>()
-    val cache = mockk<CacheClient>(relaxed = true)
-    val teller = mockk<OppfriskingTeller>(relaxed = true)
-    val oppfrisker = EntraCacheOppfrisker(entra, oidTjeneste, cache, teller)
+    @MockkBean(relaxed = true)
+    private lateinit var entra: EntraTjeneste
 
-    val ansattId = AnsattId("Z999999")
-    val oid = UUID.fromString("11111111-1111-1111-1111-111111111111")
-    val nyOid = UUID.fromString("22222222-2222-2222-2222-222222222222")
+    @MockkBean
+    private lateinit var oidTjeneste: AnsattOidTjeneste
 
-    fun nøkkel(metode: String) = CacheNøkkelElementer("graph::$metode:${ansattId.verdi}")
+    @MockkBean(relaxed = true)
+    private lateinit var cache: CacheClient
 
-    beforeEach {
-        clearMocks(entra, oidTjeneste, cache, teller)
-        every { oidTjeneste.oidFraEntra(ansattId) } returns oid
+    @MockkBean
+    private lateinit var teller: OppfriskingTeller
+
+    @Autowired
+    private lateinit var oppfrisker: EntraCacheOppfrisker
+
+    private val ansattId = AnsattId("Z999999")
+
+    private fun nøkkel(metode: String) = CacheNøkkelElementer("graph::$metode:${ansattId.verdi}")
+
+    init {
+        beforeEach {
+            clearMocks(entra, oidTjeneste, cache, teller)
+            every { oidTjeneste.oidFraEntra(ansattId) } returns OID
+        }
+
+        describe("oppfrisk") {
+
+            describe("geoGrupper") {
+                it("kaller entra.geoGrupper med riktig ansattId og oid") {
+                    oppfrisker.oppfrisk(nøkkel("geoGrupper"))
+
+                    verify { entra.geoGrupper(ansattId, OID) }
+                    verify(exactly = 0) { entra.geoOgGlobaleGrupper(any(), any()) }
+                }
+            }
+
+            describe("geoOgGlobaleGrupper") {
+                it("kaller entra.geoOgGlobaleGrupper med riktig ansattId og oid") {
+                    oppfrisker.oppfrisk(nøkkel("geoOgGlobaleGrupper"))
+
+                    verify { entra.geoOgGlobaleGrupper(ansattId, OID) }
+                    verify(exactly = 0) { entra.geoGrupper(any(), any()) }
+                }
+            }
+
+            describe("ukjent metode") {
+                it("kaller verken geoGrupper eller geoOgGlobaleGrupper") {
+                    oppfrisker.oppfrisk(CacheNøkkelElementer("graph::ukjentMetode:${ansattId.verdi}"))
+
+                    verify(exactly = 0) { entra.geoGrupper(any(), any()) }
+                    verify(exactly = 0) { entra.geoOgGlobaleGrupper(any(), any()) }
+                }
+            }
+
+            describe("NotFoundRestException — tøm og oppfrisk") {
+                beforeEach {
+                    every { oidTjeneste.oidFraEntra(ansattId) } returnsMany listOf(OID, NY_OID)
+                }
+
+                describe("geoGrupper") {
+                    beforeEach {
+                        every { entra.geoGrupper(ansattId, OID) } throws NotFoundRestException(URI.create("http://entra"))
+                    }
+
+                    it("sletter OID-cache og henter ny OID") {
+                        oppfrisker.oppfrisk(nøkkel("geoGrupper"))
+
+                        verify { cache.delete(OID_CACHE, ansattId.verdi) }
+                        verify(exactly = 2) { oidTjeneste.oidFraEntra(ansattId) }
+                    }
+
+                    it("kaller oppfrisk på nytt med ny OID") {
+                        oppfrisker.oppfrisk(nøkkel("geoGrupper"))
+
+                        verify { entra.geoGrupper(ansattId, NY_OID) }
+                    }
+
+                    it("teller") {
+                        oppfrisker.oppfrisk(nøkkel("geoGrupper"))
+
+                        verify { teller.tell() }
+                    }
+                }
+
+                it("fungerer også for geoOgGlobaleGrupper") {
+                    every { entra.geoOgGlobaleGrupper(ansattId, OID) } throws NotFoundRestException(URI.create("http://entra"))
+
+                    oppfrisker.oppfrisk(nøkkel("geoOgGlobaleGrupper"))
+
+                    verify { cache.delete(OID_CACHE, ansattId.verdi) }
+                    verify { entra.geoOgGlobaleGrupper(ansattId, NY_OID) }
+                    verify { teller.tell() }
+                }
+            }
+
+            describe("annen exception") {
+                it("teller ikke og sletter ikke cache ved RuntimeException") {
+                    every { entra.geoGrupper(ansattId, OID) } throws RuntimeException("noe gikk galt")
+
+                    oppfrisker.oppfrisk(nøkkel("geoGrupper"))
+
+                    verify(exactly = 0) { teller.tell() }
+                    verify(exactly = 0) { cache.delete(any(), any()) }
+                }
+            }
+        }
     }
 
-    describe("oppfrisk") {
-
-        describe("geoGrupper") {
-
-            it("kaller entra.geoGrupper med riktig ansattId og oid") {
-                oppfrisker.oppfrisk(nøkkel("geoGrupper"))
-
-                verify { entra.geoGrupper(ansattId, oid) }
-                verify(exactly = 0) { entra.geoOgGlobaleGrupper(any(), any()) }
-            }
-        }
-
-        describe("geoOgGlobaleGrupper") {
-
-            it("kaller entra.geoOgGlobaleGrupper med riktig ansattId og oid") {
-                oppfrisker.oppfrisk(nøkkel("geoOgGlobaleGrupper"))
-
-                verify { entra.geoOgGlobaleGrupper(ansattId, oid) }
-                verify(exactly = 0) { entra.geoGrupper(any(), any()) }
-            }
-        }
-
-        describe("ukjent metode") {
-
-            it("kaller verken geoGrupper eller geoOgGlobaleGrupper") {
-                oppfrisker.oppfrisk(CacheNøkkelElementer("graph::ukjentMetode:${ansattId.verdi}"))
-
-                verify(exactly = 0) { entra.geoGrupper(any(), any()) }
-                verify(exactly = 0) { entra.geoOgGlobaleGrupper(any(), any()) }
-            }
-        }
-
-        describe("NotFoundRestException — tøm og oppfrisk") {
-
-            it("sletter OID-cache og henter ny OID ved NotFoundRestException") {
-                every { entra.geoGrupper(ansattId, oid) } throws NotFoundRestException(URI.create("http://entra"))
-                every { oidTjeneste.oidFraEntra(ansattId) } returnsMany listOf(oid, nyOid)
-
-                oppfrisker.oppfrisk(nøkkel("geoGrupper"))
-
-                verify { cache.delete(OID_CACHE, ansattId.verdi) }
-                verify(exactly = 2) { oidTjeneste.oidFraEntra(ansattId) }
-            }
-
-            it("kaller oppfrisk på nytt med ny OID etter NotFoundRestException") {
-                every { entra.geoGrupper(ansattId, oid) } throws NotFoundRestException(URI.create("http://entra"))
-                every { oidTjeneste.oidFraEntra(ansattId) } returnsMany listOf(oid, nyOid)
-
-                oppfrisker.oppfrisk(nøkkel("geoGrupper"))
-
-                verify { entra.geoGrupper(ansattId, nyOid) }
-            }
-
-            it("teller ved vellykket tøm-og-oppfrisk") {
-                every { entra.geoGrupper(ansattId, oid) } throws NotFoundRestException(URI.create("http://entra"))
-                every { oidTjeneste.oidFraEntra(ansattId) } returnsMany listOf(oid, nyOid)
-
-                oppfrisker.oppfrisk(nøkkel("geoGrupper"))
-
-                verify { teller.tell() }
-            }
-
-            it("fungerer også for geoOgGlobaleGrupper") {
-                every { entra.geoOgGlobaleGrupper(ansattId, oid) } throws NotFoundRestException(URI.create("http://entra"))
-                every { oidTjeneste.oidFraEntra(ansattId) } returnsMany listOf(oid, nyOid)
-
-                oppfrisker.oppfrisk(nøkkel("geoOgGlobaleGrupper"))
-
-                verify { cache.delete(OID_CACHE, ansattId.verdi) }
-                verify { entra.geoOgGlobaleGrupper(ansattId, nyOid) }
-                verify { teller.tell() }
-            }
-        }
-
-        describe("annen exception") {
-
-            it("teller ikke og sletter ikke cache ved RuntimeException") {
-                every { entra.geoGrupper(ansattId, oid) } throws RuntimeException("noe gikk galt")
-
-                oppfrisker.oppfrisk(nøkkel("geoGrupper"))
-
-                verify(exactly = 0) { teller.tell() }
-                verify(exactly = 0) { cache.delete(any(), any()) }
-            }
-        }
+    companion object {
+        private val OID = UUID.fromString("11111111-1111-1111-1111-111111111111")
+        private val NY_OID = UUID.fromString("22222222-2222-2222-2222-222222222222")
     }
-})
+}

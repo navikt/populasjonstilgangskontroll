757d5a2364ed36fed6b566b7b3e8b4c0a919a996
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-04-07 17:20:52 +0200

    path verge

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålConfig.kt
@@ -21,9 +21,9 @@ class VergemålConfig(
     override fun toString() = "$javaClass.simpleName [baseUri=$baseUri, pingEndpoint=$pingEndpoint]"
 
     companion object {
-        private val DEFAULT_URI = URI.create("https://repr-api.repr/api/v2/internbruker")
+        private val DEFAULT_URI = URI.create("https://repr-api.repr")
         const val VERGEMÅL = "verge"
-        private const val DEFAULT_PATH = "/vergemaal/kan-representere"
+        private const val DEFAULT_PATH = "/api/v2/internbruker/vergemaal/kan-representere"
         private const val DEFAULT_PING_PATH = "/actuator/health/liveness"
     }
 }

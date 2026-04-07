94cb65c280e11e3df634a307381651cb0d5951de
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-04-07 08:53:12 +0200

    behaviourSpec

--- src/test/kotlin/no/nav/tilgangsmaskin/tilgang/TokenTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/tilgang/TokenTest.kt
@@ -1,7 +1,7 @@
 package no.nav.tilgangsmaskin.tilgang
 
 import io.kotest.assertions.throwables.shouldThrow
-import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
@@ -18,7 +18,7 @@ import no.nav.tilgangsmaskin.tilgang.Token.Companion.NAVIDENT
 import no.nav.tilgangsmaskin.tilgang.Token.Companion.OID
 import java.util.*
 
-class TokenTest : DescribeSpec({
+class TokenTest : BehaviorSpec({
 
     val contextHolder = mockk<TokenValidationContextHolder>()
     val validationContext = mockk<TokenValidationContext>()
@@ -34,201 +34,244 @@ class TokenTest : DescribeSpec({
         every { claims.getAsList(any()) } returns emptyList()
     }
 
-    describe("erCC") {
-        it("er true når idtyp er 'app'") {
-            every { claims.getStringClaim(IDTYP) } returns APP
-            token.erCC shouldBe true
+    Given("erCC") {
+        When("idtyp er 'app'") {
+            Then("CC er true") {
+                every { claims.getStringClaim(IDTYP) } returns APP
+                token.erCC shouldBe true
+            }
         }
-
-        it("er false når idtyp ikke er 'app'") {
-            every { claims.getStringClaim(IDTYP) } returns "user"
-            token.erCC shouldBe false
+        When("idtyp ikke er 'app'") {
+            Then("CC er false") {
+                every { claims.getStringClaim(IDTYP) } returns "user"
+                token.erCC shouldBe false
+            }
         }
-
-        it("er false når idtyp mangler") {
-            token.erCC shouldBe false
-        }
-    }
-
-    describe("erObo") {
-        it("er true når oid finnes og idtyp ikke er 'app'") {
-            every { claims.getStringClaim(OID) } returns oid.toString()
-            token.erObo shouldBe true
-        }
-
-        it("er false når token er CC (idtyp=app)") {
-            every { claims.getStringClaim(IDTYP) } returns APP
-            every { claims.getStringClaim(OID) } returns oid.toString()
-            token.erObo shouldBe false
-        }
-
-        it("er false når oid mangler") {
-            token.erObo shouldBe false
+        When("idtyp mangler") {
+            Then("CC er false") {
+                token.erCC shouldBe false
+            }
         }
     }
 
-    describe("ansattId") {
-        it("returnerer AnsattId når NAVident finnes") {
-            every { claims.getStringClaim(NAVIDENT) } returns "Z999999"
-            token.ansattId shouldBe AnsattId("Z999999")
+    Given("erObo") {
+        When("oid finnes og idtyp ikke er 'app'") {
+            Then("OBO er true") {
+                every { claims.getStringClaim(OID) } returns oid.toString()
+                token.erObo shouldBe true
+            }
         }
-
-        it("returnerer null når NAVident mangler") {
-            token.ansattId shouldBe null
+        When("token er CC (idtyp=app)") {
+            Then("OBO er false") {
+                every { claims.getStringClaim(IDTYP) } returns APP
+                every { claims.getStringClaim(OID) } returns oid.toString()
+                token.erObo shouldBe false
+            }
         }
-    }
-
-    describe("oid") {
-        it("returnerer UUID når oid finnes") {
-            every { claims.getStringClaim(OID) } returns oid.toString()
-            token.oid shouldBe oid
-        }
-
-        it("returnerer null når oid mangler") {
-            token.oid shouldBe null
-        }
-    }
-
-    describe("system") {
-        it("returnerer azp_name når den finnes") {
-            every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
-            token.system shouldBe "dev-gcp:team:app"
-        }
-
-        it("returnerer UTILGJENGELIG når azp_name mangler") {
-            token.system shouldBe UTILGJENGELIG
+        When("oid mangler") {
+            Then("OBO er false") {
+                token.erObo shouldBe false
+            }
         }
     }
 
-    describe("systemNavn") {
-        it("returnerer siste del av azp_name når tre deler") {
-            every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
-            token.systemNavn shouldBe "app"
+    Given("ansattId") {
+        When("NAVident finnes") {
+            Then("returnerer AnsattId") {
+                every { claims.getStringClaim(NAVIDENT) } returns "Z999999"
+                token.ansattId shouldBe AnsattId("Z999999")
+            }
         }
-
-        it("returnerer azp_name uendret når én del uten kolon") {
-            every { claims.getStringClaim(AZP_NAME) } returns "app"
-            token.systemNavn shouldBe "app"
-        }
-
-        it("returnerer UTILGJENGELIG når azp_name mangler") {
-            token.systemNavn shouldBe UTILGJENGELIG
+        When("NAVident mangler") {
+            Then("AnsattId er null") {
+                token.ansattId shouldBe null
+            }
         }
     }
 
-    describe("cluster") {
-        it("returnerer første del av azp_name når tre deler") {
-            every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
-            token.cluster shouldBe "dev-gcp"
+    Given("oid") {
+        When("oid finnes") {
+            Then("returnerer oid") {
+                every { claims.getStringClaim(OID) } returns oid.toString()
+                token.oid shouldBe oid
+            }
         }
-
-        it("returnerer azp_name uendret når én del uten kolon") {
-            every { claims.getStringClaim(AZP_NAME) } returns "app"
-            token.cluster shouldBe "app"
-        }
-
-        it("returnerer UTILGJENGELIG når azp_name mangler") {
-            token.cluster shouldBe UTILGJENGELIG
+        When("oid mangler") {
+            Then("oid er null") {
+                token.oid shouldBe null
+            }
         }
     }
 
-    describe("systemAndNs") {
-        it("returnerer namespace:app når azp_name er cluster:namespace:app") {
-            every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
-            token.systemAndNs shouldBe "team:app"
+    Given("system") {
+        When("azp_name finnes") {
+            Then("returnerer azp_name") {
+                every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
+                token.system shouldBe "dev-gcp:team:app"
+            }
         }
-
-        it("returnerer tom streng når azp_name er et enkelt ord uten kolon") {
-            every { claims.getStringClaim(AZP_NAME) } returns "app"
-            token.systemAndNs shouldBe ""
-        }
-
-        it("returnerer app når azp_name har to deler") {
-            every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:app"
-            token.systemAndNs shouldBe "app"
-        }
-
-        it("returnerer tom streng når azp_name mangler") {
-            token.systemAndNs shouldBe ""
+        When("azp_name mangler") {
+            Then("returnerer UTILGJENGELIG") {
+                token.system shouldBe UTILGJENGELIG
+            }
         }
     }
 
-
-    describe("clusterAndSystem") {
-        it("returnerer 'app:cluster' når azp_name har tre deler") {
-            every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
-            token.clusterAndSystem shouldBe "app:dev-gcp"
+    Given("systemNavn") {
+        When("azp_name har tre deler") {
+            Then("returnerer siste del") {
+                every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
+                token.systemNavn shouldBe "app"
+            }
         }
-
-        it("returnerer system uendret når azp_name ikke har tre deler") {
-            every { claims.getStringClaim(AZP_NAME) } returns "app"
-            token.clusterAndSystem shouldBe "app"
+        When("azp_name er ett ord uten kolon") {
+            Then("returnerer azp_name uendret") {
+                every { claims.getStringClaim(AZP_NAME) } returns "app"
+                token.systemNavn shouldBe "app"
+            }
+        }
+        When("azp_name mangler") {
+            Then("returnerer UTILGJENGELIG") {
+                token.systemNavn shouldBe UTILGJENGELIG
+            }
         }
     }
 
-    describe("globaleGruppeIds") {
-        it("returnerer liste av UUIDs fra groups-claim") {
-            val gruppeId = UUID.randomUUID()
-            every { claims.getAsList("groups") } returns listOf(gruppeId.toString())
-            token.globaleGruppeIds shouldBe listOf(gruppeId)
+    Given("cluster") {
+        When("azp_name har tre deler") {
+            Then("returnerer første del") {
+                every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
+                token.cluster shouldBe "dev-gcp"
+            }
         }
-
-        it("returnerer tom liste når groups mangler") {
-            token.globaleGruppeIds shouldBe emptyList()
+        When("azp_name er ett ord uten kolon") {
+            Then("returnerer azp_name uendret") {
+                every { claims.getStringClaim(AZP_NAME) } returns "app"
+                token.cluster shouldBe "app"
+            }
         }
-
-        it("returnerer tom liste når groups er en tom liste") {
-            every { claims.getAsList("groups") } returns emptyList()
-            token.globaleGruppeIds shouldBe emptyList()
+        When("azp_name mangler") {
+            Then("returnerer UTILGJENGELIG") {
+                token.cluster shouldBe UTILGJENGELIG
+            }
         }
+    }
 
-        it("returnerer tom liste når claimSet er null (ingen token-kontekst)") {
+    Given("systemAndNs") {
+        When("azp_name er cluster:namespace:app") {
+            Then("returnerer namespace:app") {
+                every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
+                token.systemAndNs shouldBe "team:app"
+            }
+        }
+        When("azp_name har to deler") {
+            Then("returnerer siste del") {
+                every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:app"
+                token.systemAndNs shouldBe "app"
+            }
+        }
+        When("azp_name er ett ord uten kolon") {
+            Then("returnerer tom streng") {
+                every { claims.getStringClaim(AZP_NAME) } returns "app"
+                token.systemAndNs shouldBe ""
+            }
+        }
+        When("azp_name mangler") {
+            Then("returnerer tom streng") {
+                token.systemAndNs shouldBe ""
+            }
+        }
+    }
+
+    Given("clusterAndSystem") {
+        When("azp_name har tre deler") {
+            Then("returnerer 'app:cluster'") {
+                every { claims.getStringClaim(AZP_NAME) } returns "dev-gcp:team:app"
+                token.clusterAndSystem shouldBe "app:dev-gcp"
+            }
+        }
+        When("azp_name ikke har tre deler") {
+            Then("returnerer system uendret") {
+                every { claims.getStringClaim(AZP_NAME) } returns "app"
+                token.clusterAndSystem shouldBe "app"
+            }
+        }
+    }
+
+    Given("globaleGruppeIds") {
+        When("groups-claim inneholder gyldige UUIDs") {
+            Then("returnerer liste av UUIDs") {
+                val gruppeId = UUID.randomUUID()
+                every { claims.getAsList("groups") } returns listOf(gruppeId.toString())
+                token.globaleGruppeIds shouldBe listOf(gruppeId)
+            }
+        }
+        When("groups mangler") {
+            Then("returnerer tom liste") {
+                token.globaleGruppeIds shouldBe emptyList()
+            }
+        }
+        When("groups er tom liste") {
+            Then("returnerer tom liste") {
+                every { claims.getAsList("groups") } returns emptyList()
+                token.globaleGruppeIds shouldBe emptyList()
+            }
+        }
+        When("getClaims kaster exception") {
+            Then("returnerer tom liste") {
+                every { validationContext.getClaims(AAD_ISSUER) } throws RuntimeException("ingen token")
+                token.globaleGruppeIds shouldBe emptyList()
+            }
+        }
+        When("getAsList returnerer null") {
+            Then("returnerer tom liste") {
+                every { claims.getAsList("groups") } returns null
+                token.globaleGruppeIds shouldBe emptyList()
+            }
+        }
+        When("groups inneholder ugyldig UUID-verdi") {
+            Then("kaster IllegalArgumentException") {
+                every { claims.getAsList("groups") } returns listOf("ikke-en-uuid")
+                shouldThrow<IllegalArgumentException> { token.globaleGruppeIds }
+            }
+        }
+    }
+
+    Given("ingen gyldig token-kontekst") {
+        beforeEach {
             every { validationContext.getClaims(AAD_ISSUER) } throws RuntimeException("ingen token")
-            token.globaleGruppeIds shouldBe emptyList()
         }
-
-        it("returnerer tom liste når getAsList returnerer null") {
-            every { claims.getAsList("groups") } returns null
-            token.globaleGruppeIds shouldBe emptyList()
-        }
-
-        it("kaster IllegalArgumentException ved ugyldig UUID-verdi i groups") {
-            every { claims.getAsList("groups") } returns listOf("ikke-en-uuid")
-            shouldThrow<IllegalArgumentException> { token.globaleGruppeIds }
-        }
-    }
-
-    describe("ingen gyldig token-kontekst") {
-        it("erCC er false når getClaims kaster exception") {
-            every { validationContext.getClaims(AAD_ISSUER) } throws RuntimeException("ingen token")
-            token.erCC shouldBe false
-        }
-
-        it("erObo er false når getClaims kaster exception") {
-            every { validationContext.getClaims(AAD_ISSUER) } throws RuntimeException("ingen token")
-            token.erObo shouldBe false
-        }
-
-        it("ansattId er null når getClaims kaster exception") {
-            every { validationContext.getClaims(AAD_ISSUER) } throws RuntimeException("ingen token")
-            token.ansattId shouldBe null
+        When("getClaims kaster exception") {
+            Then("erCC er false") {
+                token.erCC shouldBe false
+            }
+            Then("erObo er false") {
+                token.erObo shouldBe false
+            }
+            Then("ansattId er null") {
+                token.ansattId shouldBe null
+            }
         }
     }
 
-    describe("TokenType.from") {
-        it("returnerer OBO for OBO-token") {
-            every { claims.getStringClaim(OID) } returns oid.toString()
-            TokenType.from(token) shouldBe TokenType.OBO
+    Given("TokenType.from") {
+        When("token er OBO") {
+            Then("returnerer OBO") {
+                every { claims.getStringClaim(OID) } returns oid.toString()
+                TokenType.from(token) shouldBe TokenType.OBO
+            }
         }
-
-        it("returnerer CCF for CC-token") {
-            every { claims.getStringClaim(IDTYP) } returns APP
-            TokenType.from(token) shouldBe TokenType.CCF
+        When("token er CC") {
+            Then("returnerer CCF") {
+                every { claims.getStringClaim(IDTYP) } returns APP
+                TokenType.from(token) shouldBe TokenType.CCF
+            }
         }
-
-        it("returnerer UNAUTHENTICATED når ingen claims finnes") {
-            every { validationContext.getClaims(AAD_ISSUER) } throws RuntimeException("ingen token")
-            TokenType.from(token) shouldBe TokenType.UNAUTHENTICATED
+        When("ingen claims finnes") {
+            Then("returnerer UNAUTHENTICATED") {
+                every { validationContext.getClaims(AAD_ISSUER) } throws RuntimeException("ingen token")
+                TokenType.from(token) shouldBe TokenType.UNAUTHENTICATED
+            }
         }
     }
 })

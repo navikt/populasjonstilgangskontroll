8ea9f3d510cf7ad741ea34a941ae6cf8562d80bd
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-04-08 14:07:14 +0200

    transactional

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålRestClientAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålRestClientAdapter.kt
@@ -1,6 +1,7 @@
 package no.nav.tilgangsmaskin.ansatt.vergemål
 
 import no.nav.tilgangsmaskin.ansatt.vergemål.VergemålConfig.Companion.VERGEMÅL
+import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.felles.rest.AbstractRestClientAdapter
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Component
@@ -10,8 +11,14 @@ import org.springframework.web.client.RestClient
 class VergemålRestClientAdapter(@Qualifier(VERGEMÅL) restClient: RestClient,/* @Qualifier(VERGEMÅL + "ping") pingClient: RestClient*/ val cf: VergemålConfig) :
     AbstractRestClientAdapter(restClient, cf, /*pingClient*/) {
 
-    fun vergemål(ident: String) =
-        post<List<Vergemål>>(cf.vergemålURI, VergemålIdent(ident)).map { it.vergehaver }
+
+
+    fun vergemål(ident: String): List<BrukerId> {
+        log.info("Henter vergemål for $ident")
+        val respons =  post<List<Vergemål>>(cf.vergemålURI, VergemålIdent(ident))
+        log.info("Hentet vergemål $respons for $ident")
+        return respons.map { it.vergehaver }
+    }
 
     private data class VergemålIdent(val ident: String)
 
--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålTjeneste.kt
@@ -6,11 +6,13 @@ import no.nav.tilgangsmaskin.ansatt.entraproxy.EntraProxyRestClientAdapter
 import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.felles.Generated
 import no.nav.tilgangsmaskin.felles.rest.RetryingWhenRecoverable
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.stereotype.Service
 
 @RetryingWhenRecoverable
 @Service
 class VergemålTjeneste(private val adapter: VergemålRestClientAdapter)  {
+
 
     @WithSpan
     fun vergemål(ansatt: BrukerId?) =

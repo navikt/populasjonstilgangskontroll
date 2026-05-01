ac613f9cdb19bfc16f34b80fdf18cd9d01460dd6
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-01 14:01:40 +0200

    forenklet

--- src/test/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingTestConfig.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/ansatt/skjerming/SkjermingTestConfig.kt
@@ -1,5 +1,6 @@
 package no.nav.tilgangsmaskin.ansatt.skjerming
 
+import no.nav.tilgangsmaskin.ansatt.skjerming.SkjermingConfig.Companion.SKJERMING
 import no.nav.tilgangsmaskin.felles.cache.CacheOperations
 import no.nav.tilgangsmaskin.felles.cache.ConcurrentMapCacheOperations
 import org.springframework.boot.test.context.TestConfiguration
@@ -7,12 +8,14 @@ import org.springframework.cache.CacheManager
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager
 import org.springframework.context.annotation.Bean
+import org.springframework.resilience.annotation.EnableResilientMethods
 
 @TestConfiguration
 @EnableCaching
-class CacheConfig {
+@EnableResilientMethods
+class SkjermingTestConfig {
     @Bean
-    fun cacheManager(): CacheManager = ConcurrentMapCacheManager(SkjermingConfig.SKJERMING)
+    fun cacheManager(): CacheManager = ConcurrentMapCacheManager(SKJERMING)
     @Bean
     fun cache(cacheManager: CacheManager): CacheOperations = ConcurrentMapCacheOperations(cacheManager)
 }

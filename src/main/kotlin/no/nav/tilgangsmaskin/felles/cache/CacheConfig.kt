c037095d31ede6ba04cd80786ba58982485ca0a2
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-04 19:30:41 +0200

    fjernet ubrukt

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheConfig.kt
@@ -1,13 +1,13 @@
 package no.nav.tilgangsmaskin.felles.cache
 
-import io.lettuce.core.RedisURI
+import io.lettuce.core.RedisURI.Builder
 import no.nav.tilgangsmaskin.felles.cache.CacheAdapter.Companion.VALKEY
 import org.springframework.boot.context.properties.ConfigurationProperties
 import java.time.Duration
 
 @ConfigurationProperties(VALKEY)
 data class CacheConfig(val username: String, val password: String, val host: String, val port: Int, val timeout: Duration, val connectTimeout: Duration) {
-    val cacheURI = RedisURI.Builder
+    val cacheURI = Builder
         .redis(host, port)
         .withSsl(true)
         .withAuthentication(username, password)

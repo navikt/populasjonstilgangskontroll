2e040c8f62afa687dcef576db4e4d0cc8c3f9f96
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-04 13:50:20 +0200

    spec

--- src/test/kotlin/no/nav/tilgangsmaskin/felles/rest/LoggingRequestInterceptorTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/felles/rest/LoggingRequestInterceptorTest.kt
@@ -4,7 +4,7 @@ import ch.qos.logback.classic.Level.DEBUG
 import ch.qos.logback.classic.Logger
 import ch.qos.logback.classic.spi.ILoggingEvent
 import ch.qos.logback.core.read.ListAppender
-import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
@@ -23,7 +23,7 @@ import org.springframework.test.web.client.response.MockRestResponseCreators.wit
 import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
 import org.springframework.web.client.RestClient
 
-class LoggingRequestInterceptorTest : DescribeSpec({
+class LoggingRequestInterceptorTest : BehaviorSpec({
 
     val interceptor = LoggingRequestInterceptor()
     val builder = RestClient.builder().requestInterceptor(interceptor)
@@ -34,7 +34,6 @@ class LoggingRequestInterceptorTest : DescribeSpec({
     beforeEach {
         server = bindTo(builder).build()
         client = builder.build()
-
         logAppender = ListAppender<ILoggingEvent>().also {
             it.start()
             (getLogger(LoggingRequestInterceptor::class.java) as Logger).apply {
@@ -49,78 +48,56 @@ class LoggingRequestInterceptorTest : DescribeSpec({
         server.verify()
     }
 
-    describe("intercept") {
-
-        it("logger ikke når URI inneholder 'monitoring'") {
-            server.expect(requestTo("http://example.com/monitoring/health"))
-                .andRespond(withSuccess())
-
-            client.get().uri("http://example.com/monitoring/health")
-                .retrieve().toBodilessEntity()
-
-            logAppender.list.shouldBeEmpty()
+    Given("intercept") {
+        When("URI inneholder 'monitoring'") {
+            Then("logges ingenting") {
+                server.expect(requestTo("http://example.com/monitoring/health")).andRespond(withSuccess())
+                client.get().uri("http://example.com/monitoring/health").retrieve().toBodilessEntity()
+                logAppender.list.shouldBeEmpty()
+            }
         }
-
-        it("logger ikke ved tom body og 2xx respons") {
-            server.expect(requestTo("http://example.com/api/resource"))
-                .andExpect(method(GET))
-                .andRespond(withSuccess())
-
-            client.get().uri("http://example.com/api/resource")
-                .retrieve().toBodilessEntity()
-
-            logAppender.list.shouldBeEmpty()
+        When("body er tom og respons er 2xx") {
+            Then("logges ingenting") {
+                server.expect(requestTo("http://example.com/api/resource")).andExpect(method(GET)).andRespond(withSuccess())
+                client.get().uri("http://example.com/api/resource").retrieve().toBodilessEntity()
+                logAppender.list.shouldBeEmpty()
+            }
         }
-
-        it("logger body når body ikke er tom") {
-            server.expect(requestTo("http://example.com/api/resource"))
-                .andExpect(method(POST))
-                .andRespond(withSuccess("""{"result":"ok"}""", APPLICATION_JSON))
-
-            client.post().uri("http://example.com/api/resource")
-                .body("""{"key":"value"}""")
-                .retrieve().toBodilessEntity()
-
-            logAppender.list shouldHaveSize 1
-            logAppender.list[0].formattedMessage shouldContain """{"key":"value"}"""
+        When("body ikke er tom") {
+            Then("logges body") {
+                server.expect(requestTo("http://example.com/api/resource")).andExpect(method(POST))
+                    .andRespond(withSuccess("""{"result":"ok"}""", APPLICATION_JSON))
+                client.post().uri("http://example.com/api/resource").body("""{"key":"value"}""").retrieve().toBodilessEntity()
+                logAppender.list shouldHaveSize 1
+                logAppender.list[0].formattedMessage shouldContain """{"key":"value"}"""
+            }
         }
-
-        it("logger response status når respons ikke er 2xx") {
-            server.expect(requestTo("http://example.com/api/resource"))
-                .andExpect(method(GET))
-                .andRespond(withStatus(INTERNAL_SERVER_ERROR))
-
-            client.get().uri("http://example.com/api/resource")
-                .retrieve().onStatus({ it.isError }, { _, _ -> })
-                .toBodilessEntity()
-
-            logAppender.list shouldHaveSize 1
-            logAppender.list[0].formattedMessage shouldContain "500"
+        When("respons ikke er 2xx") {
+            Then("logges response status") {
+                server.expect(requestTo("http://example.com/api/resource")).andExpect(method(GET))
+                    .andRespond(withStatus(INTERNAL_SERVER_ERROR))
+                client.get().uri("http://example.com/api/resource").retrieve()
+                    .onStatus({ it.isError }, { _, _ -> }).toBodilessEntity()
+                logAppender.list shouldHaveSize 1
+                logAppender.list[0].formattedMessage shouldContain "500"
+            }
         }
-
-        it("logger både body og response status når begge er til stede") {
-            server.expect(requestTo("http://example.com/api/resource"))
-                .andExpect(method(POST))
-                .andRespond(withStatus(INTERNAL_SERVER_ERROR))
-
-            client.post().uri("http://example.com/api/resource")
-                .body("""{"key":"value"}""")
-                .retrieve().onStatus({ it.isError }, { _, _ -> })
-                .toBodilessEntity()
-
-            logAppender.list shouldHaveSize 2
-            logAppender.list.map { it.formattedMessage }.joinToString() shouldContain """{"key":"value"}"""
-            logAppender.list.map { it.formattedMessage }.joinToString() shouldContain "500"
+        When("body ikke er tom og respons ikke er 2xx") {
+            Then("logges både body og response status") {
+                server.expect(requestTo("http://example.com/api/resource")).andExpect(method(POST))
+                    .andRespond(withStatus(INTERNAL_SERVER_ERROR))
+                client.post().uri("http://example.com/api/resource").body("""{"key":"value"}""").retrieve()
+                    .onStatus({ it.isError }, { _, _ -> }).toBodilessEntity()
+                logAppender.list shouldHaveSize 2
+                logAppender.list.map { it.formattedMessage }.joinToString() shouldContain """{"key":"value"}"""
+                logAppender.list.map { it.formattedMessage }.joinToString() shouldContain "500"
+            }
         }
-
-        it("statusCode er tilgjengelig i responsen") {
-            server.expect(requestTo("http://example.com/api/resource"))
-                .andRespond(withSuccess())
-
-            val resp = client.get().uri("http://example.com/api/resource")
-                .retrieve().toBodilessEntity()
-
-            resp.statusCode shouldBe OK
+        When("respons er 2xx") {
+            Then("er statusCode tilgjengelig i responsen") {
+                server.expect(requestTo("http://example.com/api/resource")).andRespond(withSuccess())
+                client.get().uri("http://example.com/api/resource").retrieve().toBodilessEntity().statusCode shouldBe OK
+            }
         }
     }
 })

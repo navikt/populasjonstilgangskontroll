04ed58d7c33aff97cdacb54dbd0e270a9dd59afb
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-07 18:57:16 +0200

    fjernet log

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomDBOpprydder.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomDBOpprydder.kt
@@ -1,7 +1,6 @@
 package no.nav.tilgangsmaskin.ansatt.nom
 
 import no.nav.tilgangsmaskin.felles.utils.LeaderAware
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import java.util.concurrent.TimeUnit.*
@@ -12,7 +11,6 @@ class NomDBOpprydder(
     private val antallKall: NomKallTeller,
     private val raderFjernet: NomRaderFjernetTeller) : LeaderAware() {
 
-    private val log = getLogger(javaClass)
 
     override fun doHandleLeaderChange() {
         ryddOpp()
@@ -21,7 +19,6 @@ class NomDBOpprydder(
     @Scheduled(fixedRate = 24, timeUnit = HOURS)
     fun ryddOpp() =
         somLeder("daglig opprydding i Nom-databasen", {
-            log.info("Vaktmester rydder opp i Nom-databasen")
             val antall = nom.ryddOpp()
             antallKall.tell()
             raderFjernet.tell(antall)

6db64358f611ae3d61d9b5bc9961d5015d56035b
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-07 10:09:04 +0200

    forenklet cfg

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyConfig.kt
@@ -1,19 +1,16 @@
 package no.nav.tilgangsmaskin.ansatt.entraproxy
 
 import no.nav.tilgangsmaskin.ansatt.entraproxy.EntraProxyClient.Companion.PING_PATH
-import no.nav.tilgangsmaskin.ansatt.entraproxy.EntraProxyConfig.Companion.ENTRAPROXY
 import no.nav.tilgangsmaskin.ansatt.graph.EntraConfig.Companion.GRAPH
-import no.nav.tilgangsmaskin.felles.Generated
 import no.nav.tilgangsmaskin.felles.rest.AbstractRestConfig
-import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.stereotype.Component
 import java.net.URI
 
-@ConfigurationProperties(ENTRAPROXY)
+@Component
 class EntraProxyConfig : AbstractRestConfig(PROXY_BASE, PING_PATH, GRAPH)
 {
 
     companion object {
         val PROXY_BASE = URI.create("http://entra-proxy.sikkerhetstjenesten")
-        const val ENTRAPROXY = "entra-proxy"
     }
 }
--- src/test/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyTjenesteTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyTjenesteTest.kt
@@ -31,8 +31,7 @@ import org.springframework.test.web.client.response.MockRestResponseCreators.wit
 import org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess
 import org.springframework.web.util.UriComponentsBuilder.fromUriString
 
-@RestClientTest(components = [EntraProxyPingable::class,EntraProxyClient::class, EntraProxyBeanConfig::class, EntraProxyTjeneste::class, DefaultRestErrorHandler::class])
-@EnableConfigurationProperties(EntraProxyConfig::class)
+@RestClientTest(components = [EntraProxyPingable::class,EntraProxyClient::class, EntraProxyBeanConfig::class, EntraProxyTjeneste::class, EntraProxyConfig::class, DefaultRestErrorHandler::class])
 @ApplyExtension(SpringExtension::class)
 class EntraProxyTjenesteTest : BehaviorSpec() {
 

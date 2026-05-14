b691fdb84fdb10bb481404a43d7dca08e65d1f0f
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-14 17:00:27 +0200

    test Nom mot db

--- src/test/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomTjenesteTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomTjenesteTest.kt
@@ -1,72 +1,86 @@
 package no.nav.tilgangsmaskin.ansatt.nom
 
+import com.ninjasquad.springmockk.MockkBean
+import io.kotest.core.extensions.ApplyExtension
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldBe
-import io.mockk.clearMocks
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import io.kotest.matchers.shouldNotBe
+import no.nav.tilgangsmaskin.TestApp
 import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.bruker.BrukerId
+import no.nav.tilgangsmaskin.regler.motor.GlobaleGrupperConfig
+import no.nav.tilgangsmaskin.tilgang.Token
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.context.TestPropertySource
+import org.testcontainers.junit.jupiter.Testcontainers
+import org.testcontainers.postgresql.PostgreSQLContainer
 
-class NomTjenesteTest : BehaviorSpec({
+@DataJpaTest
+@Testcontainers
+@TestPropertySource(locations = ["classpath:test.properties"])
+@EnableConfigurationProperties(value = [GlobaleGrupperConfig::class])
+@ContextConfiguration(classes = [TestApp::class, NomTjeneste::class, NomJPAAdapter::class])
+@ApplyExtension(SpringExtension::class)
+class NomTjenesteTest : BehaviorSpec() {
 
-    val adapter = mockk<NomJPAAdapter>(relaxed = true)
-    val tjeneste = NomTjeneste(adapter)
+    @Autowired private lateinit var tjeneste: NomTjeneste
+    @Autowired private lateinit var adapter: NomJPAAdapter
 
-    val ansattId = AnsattId("Z999999")
-    val brukerId = BrukerId("08526835670")
+    @MockkBean private lateinit var token: Token
 
-    beforeEach { clearMocks(adapter) }
+    private val ansattId = AnsattId("Z999999")
+    private val brukerId = BrukerId("08526835670")
 
-    Given("fnrForAnsatt kalles") {
-        When("ansatt finnes") {
-            Then("returnerer fnr fra adapter") {
-                every { adapter.fnrForAnsatt(ansattId.verdi) } returns brukerId
-                tjeneste.fnrForAnsatt(ansattId) shouldBe brukerId
+    init {
+        Given("fnrForAnsatt kalles") {
+            When("ansatt finnes") {
+                Then("returnerer fnr") {
+                    adapter.upsert(NomAnsattData(ansattId, brukerId))
+                    tjeneste.fnrForAnsatt(ansattId) shouldBe brukerId
+                }
+            }
+
+            When("ansatt ikke finnes") {
+                Then("returnerer null") {
+                    tjeneste.fnrForAnsatt(AnsattId("X000000")) shouldBe null
+                }
             }
         }
 
-        When("ansatt ikke finnes") {
-            Then("returnerer null") {
-                every { adapter.fnrForAnsatt(ansattId.verdi) } returns null
-                tjeneste.fnrForAnsatt(ansattId) shouldBe null
+        Given("lagre kalles") {
+            When("ny ansatt lagres") {
+                Then("kan hentes igjen") {
+                    tjeneste.lagre(NomAnsattData(ansattId, brukerId))
+                    tjeneste.fnrForAnsatt(ansattId) shouldNotBe null
+                }
+            }
+
+            When("eksisterende ansatt oppdateres") {
+                Then("nyeste fnr returneres") {
+                    val nyttBrukerId = BrukerId("12345678901")
+                    tjeneste.lagre(NomAnsattData(ansattId, brukerId))
+                    tjeneste.lagre(NomAnsattData(ansattId, nyttBrukerId))
+                    tjeneste.fnrForAnsatt(ansattId) shouldBe nyttBrukerId
+                }
             }
         }
 
-        Then("delegerer til adapter med riktig verdi") {
-            every { adapter.fnrForAnsatt(ansattId.verdi) } returns brukerId
-            tjeneste.fnrForAnsatt(ansattId)
-            verify(exactly = 1) { adapter.fnrForAnsatt(ansattId.verdi) }
+        Given("ryddOpp kalles") {
+            When("ingen utgåtte ansatte") {
+                Then("returnerer 0") {
+                    tjeneste.ryddOpp() shouldBe 0
+                }
+            }
         }
     }
 
-    Given("ryddOpp kalles") {
-        When("adapter sletter rader") {
-            Then("returnerer antall slettede rader") {
-                every { adapter.ryddOpp() } returns 3
-                tjeneste.ryddOpp() shouldBe 3
-            }
-        }
-
-        When("ingen rader slettes") {
-            Then("returnerer 0") {
-                every { adapter.ryddOpp() } returns 0
-                tjeneste.ryddOpp() shouldBe 0
-            }
-        }
-
-        Then("delegerer til adapter") {
-            tjeneste.ryddOpp()
-            verify(exactly = 1) { adapter.ryddOpp() }
-        }
+    companion object {
+        @ServiceConnection
+        private val postgres = PostgreSQLContainer("postgres:18")
     }
-
-    Given("lagre kalles") {
-        Then("delegerer til adapter.upsert") {
-            val data = NomAnsattData(ansattId, brukerId)
-            tjeneste.lagre(data)
-            verify(exactly = 1) { adapter.upsert(data) }
-        }
-    }
-})
+}

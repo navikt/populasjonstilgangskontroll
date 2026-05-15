694fec76cc80afbbd4a7e85e729e68a5b757886e
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-15 13:51:00 +0200

    støtte for interceptors i createClient

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/graph/oid/EntraOidBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/graph/oid/EntraOidBeanConfig.kt
@@ -3,7 +3,7 @@ package no.nav.tilgangsmaskin.ansatt.graph.oid
 import no.nav.tilgangsmaskin.ansatt.graph.EntraGrupperConfig
 import no.nav.tilgangsmaskin.ansatt.graph.EntraGrupperConfig.Companion.CONSISTENCY_LEVEL
 import no.nav.tilgangsmaskin.felles.PingableHealthIndicator.Companion
-import no.nav.tilgangsmaskin.felles.rest.RestClientFactory
+import no.nav.tilgangsmaskin.felles.rest.RestClientFactory.createClient
 import no.nav.tilgangsmaskin.felles.rest.RestHeaderAddingRequestInterceptor
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -14,12 +14,9 @@ class EntraOidBeanConfig {
 
     @Bean
     fun entraOidClient(builder: Builder, cfg: EntraGrupperConfig) =
-        RestClientFactory.createClient<EntraOidClient>(
+        createClient<EntraOidClient>(
             cfg,
-            builder.clone().requestInterceptors {
-                it.add(RestHeaderAddingRequestInterceptor(CONSISTENCY_LEVEL))
-            },
-        )
+            builder, interceptors = arrayOf(RestHeaderAddingRequestInterceptor(CONSISTENCY_LEVEL)))
 
     @Bean
     fun graphHealthIndicator(cfg: EntraGrupperConfig, client: EntraOidClient) =
--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RestClientFactory.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/RestClientFactory.kt
@@ -1,6 +1,7 @@
 package no.nav.tilgangsmaskin.felles.rest
 
 import org.springframework.http.HttpStatusCode
+import org.springframework.http.client.ClientHttpRequestInterceptor
 import org.springframework.web.client.RestClient.Builder
 import org.springframework.web.client.RestClient.ResponseSpec.ErrorHandler
 import org.springframework.web.client.support.RestClientAdapter.create
@@ -8,16 +9,19 @@ import org.springframework.web.service.invoker.HttpServiceProxyFactory.builderFo
 import java.net.URI
 
 object RestClientFactory {
-    fun createProxyFactory(baseUri: URI, builder: Builder, errorHandler: ErrorHandler) =
+    fun createProxyFactory(baseUri: URI, builder: Builder, errorHandler: ErrorHandler, vararg interceptors: ClientHttpRequestInterceptor) =
         builderFor(
             create(
                 builder.baseUrl(baseUri)
+                    .requestInterceptors {
+                        it.addAll(interceptors)
+                    }
                     .defaultStatusHandler(HttpStatusCode::isError, errorHandler::handle)
                     .build()
             )
         ).build()
 
-    inline fun <reified T : Any> createClient(cfg: RestConfig, builder: Builder, errorHandler: ErrorHandler = RestDefaultErrorHandler()) =
-        createProxyFactory(cfg.baseUri, builder, errorHandler).createClient(T::class.java)
+    inline fun <reified T : Any> createClient(cfg: RestConfig, builder: Builder, errorHandler: ErrorHandler = RestDefaultErrorHandler(),vararg interceptors: ClientHttpRequestInterceptor) =
+        createProxyFactory(cfg.baseUri, builder, errorHandler, *interceptors).createClient(T::class.java)
 }
 

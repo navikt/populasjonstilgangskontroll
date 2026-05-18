bfb6484f4954a7dd29b1c2c73135d6e059a7a0df
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-18 14:31:07 +0200

    test corner cases

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/OpenApiMessageResolverConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/OpenApiMessageResolverConfig.kt
@@ -1,14 +1,16 @@
 package no.nav.tilgangsmaskin.tilgang
 
 import io.swagger.v3.oas.models.Operation
+import no.nav.tilgangsmaskin.felles.Generated
 import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.util.Locale
 import java.util.Locale.getDefault
-
+typealias NoCoverageAnalysis = Generated
 @Configuration
+@NoCoverageAnalysis
 class OpenApiMessageResolverConfig(private val messageSource: MessageSource) {
 
     @Bean

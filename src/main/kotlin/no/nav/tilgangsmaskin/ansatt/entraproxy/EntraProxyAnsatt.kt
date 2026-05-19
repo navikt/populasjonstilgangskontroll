97d02ef9a27f9d738dcbc25ef913b85bc76c4c59
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-19 11:44:38 +0200

    threnger ikke ansattid i responsen

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyAnsatt.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/entraproxy/EntraProxyAnsatt.kt
@@ -1,10 +1,9 @@
 package no.nav.tilgangsmaskin.ansatt.entraproxy
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.bruker.Enhetsnummer
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-data class EntraProxyAnsatt(val navIdent: AnsattId, val enhet: Enhet) {
+data class EntraProxyAnsatt(val enhet: Enhet) {
     data class Enhet(val enhetnummer: Enhetsnummer, val navn: String)
 }

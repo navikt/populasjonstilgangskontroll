b3717b40e713a1a4a49bfa0a9a223b738baa247a
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-20 20:26:11 +0200

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/OverstyrbareRegler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/OverstyrbareRegler.kt
@@ -2,24 +2,17 @@ package no.nav.tilgangsmaskin.regler.motor
 
 import io.micrometer.core.instrument.Tags
 import no.nav.boot.conditionals.ConditionalOnNotProd
-import no.nav.boot.conditionals.ConditionalOnProd
 import no.nav.tilgangsmaskin.ansatt.Ansatt
 import no.nav.tilgangsmaskin.ansatt.GlobalGruppe
 import no.nav.tilgangsmaskin.ansatt.GlobalGruppe.NASJONAL
 import no.nav.tilgangsmaskin.ansatt.GlobalGruppe.UKJENT_BOSTED
 import no.nav.tilgangsmaskin.ansatt.GlobalGruppe.UTENLANDSK
-import no.nav.tilgangsmaskin.ansatt.entraproxy.EntraProxyTjeneste
 import no.nav.tilgangsmaskin.ansatt.oppfølging.OppfølgingTjeneste
 import no.nav.tilgangsmaskin.ansatt.vergemål.VergemålTjeneste
 import no.nav.tilgangsmaskin.bruker.Bruker
 import no.nav.tilgangsmaskin.bruker.Identifikator
-import no.nav.tilgangsmaskin.felles.utils.Auditor
-import no.nav.tilgangsmaskin.felles.utils.extensions.DomainExtensions.UTILGJENGELIG
-import no.nav.tilgangsmaskin.felles.utils.extensions.TimeExtensions.Dødsperiode
-import no.nav.tilgangsmaskin.felles.utils.extensions.TimeExtensions.intervallSiden
 import no.nav.tilgangsmaskin.regler.motor.GruppeMetadata.AVDØD
 import no.nav.tilgangsmaskin.regler.motor.GruppeMetadata.VERGEMÅL
-import no.nav.tilgangsmaskin.tilgang.Token
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.core.Ordered.LOWEST_PRECEDENCE
 import org.springframework.core.annotation.Order
@@ -56,36 +49,8 @@ class UtlandRegel : GlobalGruppeRegel(UTENLANDSK), OverstyrbarRegel {
         }
 }
 
-@Component
-@Order(LOWEST_PRECEDENCE - 3)
-@ConditionalOnProd
-class AvdødBrukerTellendeRegel(private val teller: AvdødTeller, private val proxy: EntraProxyTjeneste, private val auditor: Auditor, private val token: Token) : TellendeRegel {
-
-    override val metadata = RegelMetadata(AVDØD)
-
-    override val skalTelle = { _: Ansatt, bruker: Bruker -> bruker.dødsdato != null }
-
-    override fun tell(ansatt: Ansatt, bruker: Bruker) {
-        val intervall = bruker.dødsdato!!.intervallSiden()
-        with(enhet(intervall, ansatt)) {
-            teller.tell(intervall, this)
-            if (this != UTILGJENGELIG) {
-                auditor.info("Ansatt ${ansatt.ansattId.verdi} i enhet $this fikk tilgang til forlengst avdød bruker ${bruker.brukerId.verdi} fra applikasjon ${token.system}")
-            }
-        }
-    }
-
-    private fun enhet(intervall: Dødsperiode, ansatt: Ansatt) =
-        runCatching {
-            intervall.takeIf { it >= Dødsperiode.MND_13_24 }
-                ?.let { proxy.enhet(ansatt.ansattId).navn }
-                ?: UTILGJENGELIG
-        }.getOrDefault(UTILGJENGELIG)
-}
-
-
+@SortertRegel(LOWEST_PRECEDENCE - 3)
 @ConditionalOnNotProd
-@Order(LOWEST_PRECEDENCE - 3)
 @Component
 class AvdødBrukerRegel : OverstyrbarRegel {
 
@@ -97,8 +62,7 @@ class AvdødBrukerRegel : OverstyrbarRegel {
         }
 }
 
-@Component
-@Order(LOWEST_PRECEDENCE - 4)
+@SortertRegel(LOWEST_PRECEDENCE - 4)
 @ConditionalOnNotProd
 class VergemålRegel(private val vergemål: VergemålTjeneste) : OverstyrbarRegel {
 
--- src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/TellendeRegler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/motor/TellendeRegler.kt
@@ -2,14 +2,18 @@ package no.nav.tilgangsmaskin.regler.motor
 
 import no.nav.boot.conditionals.ConditionalOnProd
 import no.nav.tilgangsmaskin.ansatt.Ansatt
+import no.nav.tilgangsmaskin.ansatt.entraproxy.EntraProxyTjeneste
 import no.nav.tilgangsmaskin.ansatt.vergemål.VergemålTjeneste
 import no.nav.tilgangsmaskin.bruker.Bruker
 import no.nav.tilgangsmaskin.felles.utils.Auditor
+import no.nav.tilgangsmaskin.felles.utils.extensions.DomainExtensions.UTILGJENGELIG
+import no.nav.tilgangsmaskin.felles.utils.extensions.TimeExtensions.Dødsperiode
+import no.nav.tilgangsmaskin.felles.utils.extensions.TimeExtensions.intervallSiden
+import no.nav.tilgangsmaskin.regler.motor.GruppeMetadata.AVDØD
 import no.nav.tilgangsmaskin.regler.motor.GruppeMetadata.VERGEMÅL
+import no.nav.tilgangsmaskin.tilgang.Token
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.core.Ordered.LOWEST_PRECEDENCE
-import org.springframework.core.annotation.Order
-import org.springframework.stereotype.Component
 
 interface TellendeRegel : Regel {
     val skalTelle: (Ansatt, Bruker) -> Boolean
@@ -24,8 +28,7 @@ interface TellendeRegel : Regel {
     }
 }
 
-@Component
-@Order(LOWEST_PRECEDENCE - 4)
+@SortertRegel(LOWEST_PRECEDENCE - 3)
 @ConditionalOnProd
 class VergemålTellendeRegel(private val vergemål: VergemålTjeneste, private val auditor: Auditor, private val teller: VergemålTeller) : TellendeRegel {
 
@@ -48,4 +51,30 @@ class VergemålTellendeRegel(private val vergemål: VergemålTjeneste, private v
 
     override fun tell(ansatt: Ansatt, bruker: Bruker) =
         teller.tell()
+}
+
+@SortertRegel(LOWEST_PRECEDENCE - 4)
+@ConditionalOnProd
+class AvdødBrukerTellendeRegel(private val teller: AvdødTeller, private val proxy: EntraProxyTjeneste, private val auditor: Auditor, private val token: Token) : TellendeRegel {
+
+    override val metadata = RegelMetadata(AVDØD)
+
+    override val skalTelle = { _: Ansatt, bruker: Bruker -> bruker.dødsdato != null }
+
+    override fun tell(ansatt: Ansatt, bruker: Bruker) {
+        val intervall = bruker.dødsdato!!.intervallSiden()
+        with(enhet(intervall, ansatt)) {
+            teller.tell(intervall, this)
+            if (this != UTILGJENGELIG) {
+                auditor.info("Ansatt ${ansatt.ansattId.verdi} i enhet $this fikk tilgang til forlengst avdød bruker ${bruker.brukerId.verdi} fra applikasjon ${token.system}")
+            }
+        }
+    }
+
+    private fun enhet(intervall: Dødsperiode, ansatt: Ansatt) =
+        runCatching {
+            intervall.takeIf { it >= Dødsperiode.MND_13_24 }
+                ?.let { proxy.enhet(ansatt.ansattId).navn }
+                ?: UTILGJENGELIG
+        }.getOrDefault(UTILGJENGELIG)
 }

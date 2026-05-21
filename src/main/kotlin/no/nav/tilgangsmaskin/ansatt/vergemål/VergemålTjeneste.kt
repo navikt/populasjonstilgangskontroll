dd646040b52faea280fb2a668406ffa217ab43be
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-21 07:34:00 +0200

    revert

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålTjeneste.kt
@@ -17,7 +17,7 @@ class VergemålTjeneste(private val nom: NomTjeneste, private val client: Vergem
     fun vergemål(ansattId: AnsattId) =
         nom.fnrForAnsatt(ansattId)?.let { fnr ->
             client.vergemål(VergemålIdent(fnr.verdi))
-                .mapTo(mutableSetOf()) { it.vergehaver }
+                .map { it.vergehaver }
                 .toSet()
         } ?: emptySet()
 

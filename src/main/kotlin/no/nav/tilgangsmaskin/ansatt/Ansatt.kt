f3193c6a3fbaf7bb42721a31a3cc1cca1d9fe480
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-22 09:31:17 +0200

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/Ansatt.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/Ansatt.kt
@@ -27,13 +27,11 @@ data class Ansatt(val ansattId: AnsattId, val bruker: Bruker? = null, val gruppe
             is BydelTilknytning -> gt.bydel.verdi
             else -> return true
         }
-        return grupper.any { it.displayName.endsWith("GEO_$kode") }
+        return harGruppeMedSuffix("GEO_$kode")
     }
 
     infix fun tilhører(enhet: Enhetsnummer?) =
-        enhet?.let { e ->
-            grupper.any { it.displayName.endsWith("ENHET_${e.verdi}") }
-        } ?: false
+        enhet?.let { harGruppeMedSuffix("ENHET_${it.verdi}") } ?: false
 
     infix fun erMedlemAv(gruppe: GlobalGruppe) = grupper.any {
         it.id == gruppe.id
@@ -49,12 +47,10 @@ data class Ansatt(val ansattId: AnsattId, val bruker: Bruker? = null, val gruppe
 
     infix fun erSøskenTil(bruker: Bruker) = bruker erNærståendeMed søsken
 
-    infix fun harFellesBarnMed(bruker: Bruker) = bruker.barn harMinstEnFelles barn
+    infix fun harFellesBarnMed(bruker: Bruker) = bruker.barn.any { it in barn }
 
-    private infix fun Set<FamilieMedlem>.harMinstEnFelles(medlemmer: Set<FamilieMedlem>) =
-        intersect(medlemmer).isNotEmpty()
+    private fun harGruppeMedSuffix(suffix: String) = grupper.any { it.displayName.endsWith(suffix) }
 
     private infix fun Bruker.erNærståendeMed(medlemmer: Set<FamilieMedlem>) = medlemmer.any { it.brukerId == brukerId }
 
 }
-

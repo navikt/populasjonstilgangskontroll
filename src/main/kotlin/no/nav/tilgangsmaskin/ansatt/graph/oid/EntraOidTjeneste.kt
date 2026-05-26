48818ef1aec4ab39d7ec42fda2a1e0cffd8cf337
Jan-Olav Eide <jan-olav.eide@nav.no>
2026-05-26 10:09:07 +0200

    archUnit

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/graph/oid/EntraOidTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/graph/oid/EntraOidTjeneste.kt
@@ -4,10 +4,9 @@ import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.ansatt.graph.oid.EntraOidClient.Companion.filter
 import no.nav.tilgangsmaskin.ansatt.graph.oid.EntraOidConfig.Companion.ENTRA_OID
 import no.nav.tilgangsmaskin.ansatt.graph.oid.EntraOidRespons.EntraOid
-import no.nav.tilgangsmaskin.felles.rest.IrrecoverableRestException
+import no.nav.tilgangsmaskin.felles.rest.ConflictRestException
 import no.nav.tilgangsmaskin.felles.rest.NotFoundRestException
 import org.springframework.cache.annotation.Cacheable
-import org.springframework.http.HttpStatus.CONFLICT
 import org.springframework.stereotype.Component
 import java.util.*
 
@@ -18,12 +17,11 @@ class EntraOidTjeneste(private val oidClient: EntraOidClient, private val cfg: E
     fun oid(ansattId: AnsattId) =
         validerRespons(ansattId, oidClient.oid(filter(ansattId)).oids)
 
-    private fun validerRespons(ansattId: AnsattId, oids: Set<EntraOid>): UUID {
-        return when (oids.size) {
+    private fun validerRespons(ansattId: AnsattId, oids: Set<EntraOid>): UUID =
+        when (oids.size) {
             0 -> throw NotFoundRestException(cfg.baseUri, ansattId.verdi, "Fant ingen identifikator oid")
             1 -> oids.single().id
-            else -> throw IrrecoverableRestException(CONFLICT, cfg.baseUri,
+            else -> throw ConflictRestException(cfg.baseUri,
                 "Forventet kun én oid for $ansattId, fant ${oids.size} (${oids.formattert()})")
         }
-    }
 }

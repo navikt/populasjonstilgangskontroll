5c10cefd567ce67a318c8a2f420ce4fae32c85d9
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-04 11:11:37 +0200

    merge with main

--- src/test/kotlin/no/nav/tilgangsmaskin/felles/cache/CaffeineCacheClient.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/felles/cache/CaffeineCacheClient.kt
@@ -2,7 +2,6 @@ package no.nav.tilgangsmaskin.felles.cache
 
 import com.github.benmanes.caffeine.cache.Cache
 import no.nav.boot.conditionals.ConditionalOnLocalOrTest
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.cache.CacheManager
 import java.time.Duration
 import kotlin.reflect.KClass
@@ -10,18 +9,9 @@ import kotlin.reflect.KClass
 @ConditionalOnLocalOrTest
 class CaffeineCacheClient(private val mgr: CacheManager) : CacheOperations {
 
-    private val log = getLogger(javaClass)
 
     override fun delete(cfg: CacheNøkkelConfig, id: String) =
-        with(cache(cfg)) {
-            val nøkkel = cfg.tilNøkkel(id)
-            if (get(nøkkel) != null) {
-                evict(nøkkel)
-                1L
-            } else {
-                0L
-            }
-        }
+        if (cache(cfg).evictIfPresent(cfg.tilNøkkel(id))) 1L else 0L
 
     override fun <T : Any> getOne(cfg: CacheNøkkelConfig, id: String, clazz: KClass<T>): T? =
         cache(cfg).get(cfg.tilNøkkel(id))?.get()?.let {
@@ -31,7 +21,6 @@ class CaffeineCacheClient(private val mgr: CacheManager) : CacheOperations {
     override fun putOne(cfg: CacheNøkkelConfig, id: String, value: Any, ttl: Duration) =
         cache(cfg).put(cfg.tilNøkkel(id), value)
 
-
     override fun <T : Any> getMany(cfg: CacheNøkkelConfig, ids: Set<String>, clazz: KClass<T>): Map<String, T?> {
         val keys = ids.associateBy { cfg.tilNøkkel(it) }
         val found = typedCache(cfg).getAllPresent(keys.keys)
@@ -39,20 +28,19 @@ class CaffeineCacheClient(private val mgr: CacheManager) : CacheOperations {
     }
 
     override fun putMany(cfg: CacheNøkkelConfig, innslag: Map<String, Any>, ttl: Duration) {
-        log.trace("Caffeine bulk lagrer {} verdier for cache {}", innslag.size, cfg.name)
         val keyed = innslag.map { (id, value) -> cfg.tilNøkkel(id) to value }.toMap()
         typedCache(cfg).putAll(keyed)
     }
 
     override fun clear(cfg: CacheNøkkelConfig) {
-        val cache = cache(cfg)
         if (cfg.extraPrefix == null) {
-            cache.clear()
+            cache(cfg).clear()
         } else {
-            nativeCache(cfg).asMap().keys
-                .filterIsInstance<String>()
-                .filter { it.startsWith(cfg.tilNøkkel( "")) }
-                .forEach { cache.evict(it) }
+            with(typedCache(cfg))  {
+                val prefix = cfg.tilNøkkel("")
+                val keysToRemove = asMap().keys.filter { it.startsWith(prefix) }
+                invalidateAll(keysToRemove)
+            }
         }
     }
 

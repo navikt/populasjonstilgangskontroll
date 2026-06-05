d3cb7dc1764a91ccf0b6a871a83106dfa4da7a08
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-05 17:34:19 +0200

    500002

--- src/main/resources/scripts/count-all-keys.lua
+++ src/main/resources/scripts/count-all-keys.lua
@@ -3,7 +3,7 @@ for i, pattern in ipairs(ARGV) do
     local cursor = "0"
     local count = 0
     repeat
-        local result = redis.call("SCAN", cursor, "MATCH", pattern, "COUNT", 10000)
+        local result = redis.call("SCAN", cursor, "MATCH", pattern, "COUNT", 50000)
         cursor = result[1]
         count = count + #result[2]
     until cursor == "0"

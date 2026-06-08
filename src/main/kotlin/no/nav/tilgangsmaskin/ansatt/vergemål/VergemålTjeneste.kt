0132a0b5eb77fd0c9dc93b77c9d879d7eed158bb
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-08 20:26:55 +0200

    sort the set

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålTjeneste.kt
@@ -5,21 +5,23 @@ import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.ansatt.nom.NomTjeneste
 import no.nav.tilgangsmaskin.ansatt.vergemål.VergemålClient.VergemålIdent
 import no.nav.tilgangsmaskin.ansatt.vergemål.VergemålConfig.Companion.VERGEMÅL
+import no.nav.tilgangsmaskin.bruker.BrukerId
 import no.nav.tilgangsmaskin.felles.NoCoverageAnalysis
 import no.nav.tilgangsmaskin.felles.rest.RetryingWhenRecoverableRestService
 import org.springframework.cache.annotation.Cacheable
+import java.util.TreeSet
 
 @RetryingWhenRecoverableRestService
 class VergemålTjeneste(private val nom: NomTjeneste, private val client: VergemålClient) {
 
     @WithSpan
     @Cacheable(cacheNames = [VERGEMÅL], key = "#ansattId.verdi")
-    fun vergemål(ansattId: AnsattId) =
+    fun vergemål(ansattId: AnsattId): Set<BrukerId> =
         nom.fnrForAnsatt(ansattId)?.let { fnr ->
             client.vergemål(VergemålIdent(fnr.verdi))
                 .map { it.vergehaver }
-                .toSet()
-        } ?: emptySet()
+                .toSortedSet(compareBy { it.verdi })
+        } ?: TreeSet(compareBy { it.verdi })
 
 
     @NoCoverageAnalysis

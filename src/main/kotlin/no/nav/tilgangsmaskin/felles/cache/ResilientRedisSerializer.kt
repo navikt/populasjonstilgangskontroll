62c31d51f5315da5fa79751efe1a57387fca0370
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-09 13:31:27 +0200

    runCatching

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ResilientRedisSerializer.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ResilientRedisSerializer.kt
@@ -3,7 +3,6 @@ package no.nav.tilgangsmaskin.felles.cache
 import io.micrometer.core.instrument.MeterRegistry
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.data.redis.serializer.RedisSerializer
-import org.springframework.data.redis.serializer.SerializationException
 
 class ResilientRedisSerializer(private val delegate: RedisSerializer<Any>, private val meterRegistry: MeterRegistry) : RedisSerializer<Any> {
 
@@ -12,11 +11,11 @@ class ResilientRedisSerializer(private val delegate: RedisSerializer<Any>, priva
     override fun serialize(t: Any?): ByteArray = delegate.serialize(t)
 
     override fun deserialize(bytes: ByteArray?): Any? =
-        try {
+        runCatching {
             delegate.deserialize(bytes)
-        } catch (e: SerializationException) {
+        }.getOrElse {
             meterRegistry.counter("cache.deserialize.failed").increment()
-            log.warn("Kunne ikke deserialisere cache-entry, behandler som miss",e)
+            log.warn("Kunne ikke deserialisere cache-entry, behandler som miss", it)
             null
         }
 }

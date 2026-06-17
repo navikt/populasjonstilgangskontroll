f1c7249f4260aac32d886216ed64aeb12b586f92
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-17 22:29:44 +0200

    chore

--- src/test/kotlin/no/nav/tilgangsmaskin/tilgang/CCFBulkTilgangControllerTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/tilgang/CCFBulkTilgangControllerTest.kt
@@ -1,9 +1,19 @@
 package no.nav.tilgangsmaskin.tilgang
 
 import io.mockk.every
+import no.nav.tilgangsmaskin.ansatt.Ansatt
+import no.nav.tilgangsmaskin.bruker.Bruker
+import no.nav.tilgangsmaskin.bruker.BrukerId
+import no.nav.tilgangsmaskin.regler.AnsattBuilder
+import no.nav.tilgangsmaskin.regler.BrukerBuilder
 import no.nav.tilgangsmaskin.regler.motor.BrukerIdOgRegelsett
+import no.nav.tilgangsmaskin.regler.motor.GruppeMetadata.STRENGT_FORTROLIG
+import no.nav.tilgangsmaskin.regler.motor.OverstyrbarRegel
+import no.nav.tilgangsmaskin.regler.motor.RegelException
+import no.nav.tilgangsmaskin.regler.motor.RegelMetadata
 import no.nav.tilgangsmaskin.regler.motor.RegelSett.RegelType.KJERNE_REGELTYPE
 import no.nav.tilgangsmaskin.regler.motor.RegelSett.RegelType.KOMPLETT_REGELTYPE
+import no.nav.tilgangsmaskin.tilgang.AggregertBulkRespons.EnkeltBulkRespons
 import no.nav.tilgangsmaskin.tilgang.AggregertBulkRespons.EnkeltBulkRespons.Companion.ok
 import org.springframework.http.MediaType.APPLICATION_JSON
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document
@@ -15,21 +25,34 @@ class CCFBulkTilgangControllerTest : TilgangControllerTestBase() {
 
         Given("CCF bulk") {
 
-            val specs = setOf(BrukerIdOgRegelsett(brukerId, KOMPLETT_REGELTYPE))
-            val respons = AggregertBulkRespons(ansattId, setOf(ok(brukerId)))
+            val avvistBrukerId = "12345678901"
+            val specs = setOf(
+                BrukerIdOgRegelsett(brukerId, KOMPLETT_REGELTYPE),
+                BrukerIdOgRegelsett(avvistBrukerId, KOMPLETT_REGELTYPE)
+            )
 
             beforeEach { every { token.erCC } returns true }
 
             When("bulk/ccf kalles med gyldige specs") {
-                Then("returnerer 207 med resultater") {
+                Then("returnerer 207 med mix av godkjente og avviste") {
+                    val testAnsatt = AnsattBuilder(ansattId).build()
+                    val testBruker = BrukerBuilder(BrukerId(avvistBrukerId)).build()
+                    val testRegel = object : OverstyrbarRegel {
+                        override val metadata = RegelMetadata(STRENGT_FORTROLIG)
+                        override fun evaluer(ansatt: Ansatt, bruker: Bruker) = false
+                    }
+                    val regelException = RegelException(testAnsatt, testBruker, testRegel)
+                    val respons = AggregertBulkRespons(ansattId, setOf(
+                        ok(brukerId),
+                        EnkeltBulkRespons(regelException)
+                    ))
                     every { regelTjeneste.bulkRegler(ansattId, specs) } returns respons
                     mockMvc.post("/api/v1/bulk/ccf/${ansattId.verdi}") {
                         contentType = APPLICATION_JSON
-                        content = """[{"brukerId":"$brukerId","type":"KOMPLETT_REGELTYPE"}]"""
+                        content = """[{"brukerId":"$brukerId","type":"KOMPLETT_REGELTYPE"},{"brukerId":"$avvistBrukerId","type":"KOMPLETT_REGELTYPE"}]"""
                     }.andExpect {
                         status { isMultiStatus() }
                         jsonPath("$.ansattId") { value(ansattId.verdi) }
-                        jsonPath("$.resultater[0].status") { value(204) }
                     }.andDo { handle(document("ccf-bulk")) }
                 }
             }
@@ -48,7 +71,8 @@ class CCFBulkTilgangControllerTest : TilgangControllerTestBase() {
             When("bulk/ccf/{ansattId}/{regelType} kalles med KJERNE_REGELTYPE") {
                 Then("returnerer 207") {
                     val kjerneSpecs = setOf(BrukerIdOgRegelsett(brukerId, KJERNE_REGELTYPE))
-                    every { regelTjeneste.bulkRegler(ansattId, kjerneSpecs) } returns respons
+                    val kjerneRespons = AggregertBulkRespons(ansattId, setOf(ok(brukerId)))
+                    every { regelTjeneste.bulkRegler(ansattId, kjerneSpecs) } returns kjerneRespons
                     mockMvc.post("/api/v1/bulk/ccf/${ansattId.verdi}/KJERNE_REGELTYPE") {
                         contentType = APPLICATION_JSON; content = """["$brukerId"]"""
                     }.andExpect { status { isMultiStatus() } }

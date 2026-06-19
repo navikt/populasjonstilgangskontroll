d0433a575356b0812d4e7ff28d9ee42b7344c2cd
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-19 21:20:16 +0200

    rich

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/enkelttilgang/EnkeltTilgangGyldig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/enkelttilgang/EnkeltTilgangGyldig.kt
@@ -12,7 +12,7 @@ import kotlin.reflect.KClass
 @Target(FIELD, VALUE_PARAMETER)
 @Retention(RUNTIME)
 annotation class EnkeltTilgangGyldig(
-    val message: String = "Enkelttilgang må være fra nå og maks 3 måneder frem i tid",
+    val message: String = "Enkelttilgang må være fra nå og maks 3 måneder frem i tid, og begrunnelse må være mellom 10 og 255 tegn",
     val groups: Array<KClass<*>> = [],
     val months: Long = 3,
     val min: Int = 10,

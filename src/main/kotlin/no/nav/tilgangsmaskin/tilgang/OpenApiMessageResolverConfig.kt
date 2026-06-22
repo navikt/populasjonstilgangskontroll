6ed2009e4489798819fb392d665976f8f0460c6a
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-22 13:29:55 +0200

    autoconf for message source

--- src/main/kotlin/no/nav/tilgangsmaskin/tilgang/OpenApiMessageResolverConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/tilgang/OpenApiMessageResolverConfig.kt
@@ -2,6 +2,7 @@ package no.nav.tilgangsmaskin.tilgang
 
 import io.swagger.v3.oas.models.Operation
 import no.nav.tilgangsmaskin.felles.NoCoverageAnalysis
+import org.slf4j.LoggerFactory.getLogger
 import org.springdoc.core.customizers.OpenApiCustomizer
 import org.springframework.context.MessageSource
 import org.springframework.context.annotation.Bean
@@ -11,6 +12,8 @@ import java.util.Locale.*
 @Configuration
 @NoCoverageAnalysis
 class OpenApiMessageResolverConfig(private val messageSource: MessageSource) {
+    private val log = getLogger(javaClass)
+
 
     @Bean
     fun openApiMessageCustomizer(): OpenApiCustomizer = OpenApiCustomizer { openApi ->
@@ -31,6 +34,7 @@ class OpenApiMessageResolverConfig(private val messageSource: MessageSource) {
     }
 
     private fun resolve(text: String?): String? {
+        log.info("Resolving $text from $messageSource")
         if (text.isNullOrBlank() || !text.startsWith(MSG_PREFIX)) return text
         val key = text.removePrefix(MSG_PREFIX)
         return messageSource.getMessage(key, null, key, getDefault())

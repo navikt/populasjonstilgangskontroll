f2e6a3dd4eac9795e4ba7ca59d2e1dd5b02e7a09
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-23 14:23:03 +0200

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ValkeyListener.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ValkeyListener.kt
@@ -1,7 +1,6 @@
 package no.nav.tilgangsmaskin.felles.cache
 
 import no.nav.tilgangsmaskin.felles.utils.LeaderAware
-import org.slf4j.LoggerFactory.getLogger
 import org.springframework.data.redis.annotation.RedisListener
 import org.springframework.stereotype.Component
 import kotlin.text.Charsets.UTF_8
@@ -9,17 +8,14 @@ import kotlin.text.Charsets.UTF_8
 @Component
 class ValkeyListener(erLeder: Boolean = true,
                      private vararg val oppfriskere: CacheOppfrisker) : LeaderAware(erLeder) {
-    private val log = getLogger(javaClass)
 
     @RedisListener(CHANNEL)
     fun onEvent(body: ByteArray) {
-        somLeder("cache innslag", {
-            with(CacheNøkkel(body.toString(UTF_8))) {
-                log.info("Valkey expired event {} på channel {}", this@with, CHANNEL)
-                oppfriskere.firstOrNull { it.cacheName == cacheName }?.run {
-                    oppfrisk(this@with)
+        val nøkkel = CacheNøkkel(body.toString(UTF_8))
+        somLeder("Håndterer utløpt cache-innslag for $nøkkel", {
+                oppfriskere.firstOrNull { it.cacheName == nøkkel.cacheName }?.run {
+                    oppfrisk(nøkkel)
                 }
-            }
         })
     }
 

60eb90ff897c4dee35091c2763523302faf51857
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-23 11:14:42 +0200

    prøv med RedisListener

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ValkeyListener.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ValkeyListener.kt
@@ -2,6 +2,7 @@ package no.nav.tilgangsmaskin.felles.cache
 
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.data.redis.annotation.RedisListener
+import org.springframework.data.redis.listener.Topic
 import org.springframework.stereotype.Component
 import kotlin.text.Charsets.UTF_8
 
@@ -11,11 +12,10 @@ class ValkeyListener {
     private val log = getLogger(javaClass)
 
     @RedisListener(CHANNEL)
-    fun onExpired(body: ByteArray) {
-        val key = body.toString(UTF_8)
-        val cachePrefix = key.substringBefore("::", missingDelimiterValue = "")
-        val rawKey = key.substringAfter("::", missingDelimiterValue = key)
-        log.info("Valkey expired key={}, cachePrefix={}, rawKey={}", key,cachePrefix, rawKey)
+    fun onEvent(body: ByteArray, topic: Topic) {
+        val theBody = body.toString(UTF_8)
+        val nøkkel = CacheNøkkel(theBody)
+        log.info("Valkey expired event {} på topic {}", nøkkel,topic.topic)
     }
     companion object {
         const val CHANNEL = "__keyevent@0__:expired"

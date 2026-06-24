782017713a8b750b32f8968f35590fa559495181
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-24 09:46:14 +0200

    log

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ValkeyEventListeningCacheOppfrisker.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ValkeyEventListeningCacheOppfrisker.kt
@@ -12,7 +12,7 @@ class ValkeyEventListeningCacheOppfrisker(erLeder: Boolean = true,
     @RedisListener(CHANNEL)
     fun onEvent(body: ByteArray) {
         val nøkkel = body.tilNøkkel()
-        somLeder("Håndterer utløpt cache-innslag for $nøkkel", {
+        somLeder("Håndterer oppfrisking for $nøkkel", {
                 oppfriskere.firstOrNull {
                     it.cacheName == nøkkel.cacheName
                 }?.run {

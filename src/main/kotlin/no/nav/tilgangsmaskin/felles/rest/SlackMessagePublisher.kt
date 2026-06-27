24b742ee831cd68bf0d7a01b4ebd5a76ec14f253
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-06-28 00:10:57 +0200

    slack api

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/SlackMessagePublisher.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/SlackMessagePublisher.kt
@@ -1,17 +1,15 @@
 package no.nav.tilgangsmaskin.felles.rest
 
-import com.slack.api.Slack
-import com.slack.api.model.block.Blocks.actions
+import com.slack.api.Slack.getInstance
 import com.slack.api.model.block.Blocks.asBlocks
-import com.slack.api.model.block.Blocks.divider
 import com.slack.api.model.block.Blocks.section
 import com.slack.api.model.block.LayoutBlock
-import com.slack.api.model.block.composition.BlockCompositions.markdownText
 import com.slack.api.model.block.composition.BlockCompositions.plainText
 import com.slack.api.webhook.Payload
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import org.springframework.web.client.RestClient
 
 /**
  * Publiserer meldinger til Slack via incoming webhook.
@@ -38,8 +36,14 @@ class SlackMessagePublisher(
         if (webhookUrl.isNotBlank()) {
             runCatching {
                 log.info("Sending Slack notification")
-                val response = Slack.getInstance().send(webhookUrl, payload)
-                log.info("Sent Slack notification, response: $response")
+
+                 RestClient.builder().build().post()
+                     .uri(webhookUrl)
+                     .body(payload)
+                     .retrieve()
+                     .toBodilessEntity()
+                //val response = getInstance().send(webhookUrl, payload)
+               // log.info("Sent Slack notification, response: $response")
             }.getOrElse {
                 log.error("Failed to send Slack notification", it)
             }

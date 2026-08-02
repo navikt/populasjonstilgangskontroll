100f848a6f43f7936c5db742bde4ec695cb2557b
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-02 19:12:40 +0200

    log renewals

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/security/LoggingOAuth2AuthorizationSuccessHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/security/LoggingOAuth2AuthorizationSuccessHandler.kt
@@ -1,11 +1,14 @@
 package no.nav.tilgangsmaskin.felles.security
 
+import org.slf4j.LoggerFactory
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.security.core.Authentication
 import org.springframework.security.oauth2.client.OAuth2AuthorizationSuccessHandler
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 
 class LoggingOAuth2AuthorizationSuccessHandler(
+    private val service: OAuth2AuthorizedClientService,
     private val delegate: OAuth2AuthorizationSuccessHandler
 ) : OAuth2AuthorizationSuccessHandler {
 
@@ -16,12 +19,22 @@ class LoggingOAuth2AuthorizationSuccessHandler(
         principal: Authentication,
         attributes: Map<String, Any>
     ) {
-        log.debug(
-            "OAuth2 authorization succeeded for clientRegistrationId={}, principalType={}, downstreamUri={}",
-            authorizedClient.clientRegistration.registrationId,
-            principal.javaClass.simpleName,
-            OAuth2DownstreamUriContext.currentUri() ?: "unknown"
-        )
-        delegate.onAuthorizationSuccess(authorizedClient, principal, attributes)
+        val registrationId = authorizedClient.clientRegistration.registrationId
+        val previous = service.loadAuthorizedClient<OAuth2AuthorizedClient>(registrationId, principal.name)
+        val prevToken = previous?.accessToken?.tokenValue
+        val newToken = authorizedClient.accessToken.tokenValue
+        val prevExp = previous?.accessToken?.expiresAt
+        val newExp = authorizedClient.accessToken.expiresAt
+
+        when {
+            previous == null ->
+                log.info("OAuth2 first authorization: clientRegistrationId={}, expiresAt={}", registrationId, newExp)
+            prevToken != newToken || prevExp != newExp ->
+                log.info("OAuth2 token renewed: clientRegistrationId={}, oldExpiresAt={}, newExpiresAt={}", registrationId, prevExp, newExp)
+            else ->
+                log.debug("OAuth2 authorization success without token change: clientRegistrationId={}", registrationId)
+        }
+
+        delegate.onAuthorizationSuccess(authorizedClient, principal, attributes) // saves client
     }
 }

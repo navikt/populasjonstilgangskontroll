4020cfcab39453e5f64968fe6c513bd0c469a1a2
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-02 17:28:28 +0200

    typo

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/security/SecurityConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/security/SecurityConfig.kt
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder

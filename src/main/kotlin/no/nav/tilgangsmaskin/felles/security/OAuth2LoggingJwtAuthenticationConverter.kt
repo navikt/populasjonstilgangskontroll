4749c3f425c58840354b3f5c4756d37aac46c798
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-04 14:16:09 +0200

    log mer kontekst

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2LoggingJwtAuthenticationConverter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2LoggingJwtAuthenticationConverter.kt
@@ -1,13 +1,12 @@
 package no.nav.tilgangsmaskin.felles.security
 
-import jakarta.servlet.http.HttpServletRequest
 import no.nav.tilgangsmaskin.felles.rest.Token.Companion.AZP_NAME
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.core.convert.converter.Converter
 import org.springframework.security.authentication.AbstractAuthenticationToken
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter
-import org.springframework.web.context.request.RequestContextHolder
+import org.springframework.web.context.request.RequestContextHolder.getRequestAttributes
 import org.springframework.web.context.request.ServletRequestAttributes
 
 class OAuth2LoggingJwtAuthenticationConverter(
@@ -17,17 +16,14 @@ class OAuth2LoggingJwtAuthenticationConverter(
     private val log = getLogger(javaClass)
 
     override fun convert(jwt: Jwt): AbstractAuthenticationToken {
-        val iss = jwt.issuer
         val exp = jwt.expiresAt
         val system = jwt.getClaimAsString(AZP_NAME)
-        val request = currentRequest()
+        val request = (getRequestAttributes() as? ServletRequestAttributes)?.request
         val url = request?.requestURI
         return runCatching { delegate.convert(jwt) }
-            .onSuccess { log.trace("JWT validert OK: system={}, iss={}, expiresAt={}, authorities={}, url={}", system, iss, exp, it.authorities, url) }
-            .onFailure { log.warn("JWT konvertering feilet: system=$system, iss=$iss, url=$url, feil=${it.message}", it) }
+            .onSuccess { log.trace("JWT validert OK: system={}, expiresAt={}, authorities={}, url={}", system, exp, it.authorities, url) }
+            .onFailure { log.warn("JWT konvertering feilet: system=$system, url=$url, feil=${it.message}", it) }
             .getOrThrow()
     }
 
-    private fun currentRequest(): HttpServletRequest? =
-        (RequestContextHolder.getRequestAttributes() as? ServletRequestAttributes)?.request
 }

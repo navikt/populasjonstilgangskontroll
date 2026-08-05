12ca9e45e4e072e14d753f636a8896fe25a06870
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-05 09:12:58 +0200

    test vergemål caching

--- src/test/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålTjenesteTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/ansatt/vergemål/VergemålTjenesteTest.kt
@@ -9,11 +9,17 @@ import io.kotest.matchers.shouldBe
 import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.ansatt.nom.NomTjeneste
 import no.nav.tilgangsmaskin.ansatt.vergemål.VergemålClient.Companion.VERGEMÅL_PATH
+import no.nav.tilgangsmaskin.ansatt.vergemål.VergemålConfig.Companion.VERGE_CACHE
+import no.nav.tilgangsmaskin.ansatt.vergemål.VergemålConfig.Companion.VERGEMÅL
 import no.nav.tilgangsmaskin.bruker.BrukerId
+import no.nav.tilgangsmaskin.felles.cache.CacheOperations
+import no.nav.tilgangsmaskin.felles.cache.CacheTestConfig
+import no.nav.tilgangsmaskin.felles.cache.getOne
 import no.nav.tilgangsmaskin.felles.rest.IrrecoverableRestException
 import no.nav.tilgangsmaskin.felles.rest.NotFoundRestException
 import no.nav.tilgangsmaskin.felles.rest.RecoverableRestException
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.boot.restclient.test.autoconfigure.RestClientTest
 import org.springframework.context.annotation.Import
 import org.springframework.http.HttpMethod.POST
@@ -21,6 +27,7 @@ import org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR
 import org.springframework.http.HttpStatus.NOT_FOUND
 import org.springframework.http.HttpStatus.UNAUTHORIZED
 import org.springframework.http.MediaType.APPLICATION_JSON
+import org.springframework.test.web.client.ExpectedCount.once
 import org.springframework.test.web.client.ExpectedCount.times
 import org.springframework.test.web.client.MockRestServiceServer
 import org.springframework.test.web.client.match.MockRestRequestMatchers.method
@@ -37,8 +44,11 @@ import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.test.context.ContextConfiguration
 import java.net.URI
 
+@TestConfiguration
+class VergemålTestConfig : CacheTestConfig(VERGEMÅL)
+
 @RestClientTest
-@Import(OAuth2ClientTestConfig::class)
+@Import(VergemålTestConfig::class, OAuth2ClientTestConfig::class)
 @ContextConfiguration(classes = [VergemålConfig::class, VergemålTjeneste::class])
 @EnableAutoConfiguration
 @ConfigurationPropertiesScan
@@ -56,7 +66,15 @@ class VergemålTjenesteTest : RestTjenesteTest() {
     @Autowired
     lateinit var server: MockRestServiceServer
 
+    @Autowired
+    lateinit var cache: CacheOperations
+
     init {
+        beforeEach {
+            server.reset()
+            cache.clear(VERGE_CACHE)
+        }
+
         afterEach { server.verify() }
 
         Given("oppslag av vergemål for ansatt") {
@@ -94,6 +112,39 @@ class VergemålTjenesteTest : RestTjenesteTest() {
                         .andRespond(withSuccess("[]", APPLICATION_JSON))
 
                     tjeneste.alle(ANSATT_ID).shouldBeEmpty()
+                }
+            }
+        }
+
+        Given("caching av vergemål") {
+            beforeEach {
+                every { nom.fnrForAnsatt(ANSATT_ID) } returns IDENT
+                every { nom.fnrForAnsatt(ANSATT_ID_2) } returns IDENT
+            }
+
+            When("samme ansatt slås opp to ganger") {
+                Then("REST kalles kun én gang og resultat finnes i cache") {
+                    server.expect(once(), requestTo(uri(cfg.baseUri)))
+                        .andExpect(method(POST))
+                        .andRespond(withSuccess(vergemålRespons(), APPLICATION_JSON))
+
+                    val første = tjeneste.alle(ANSATT_ID)
+                    val andre = tjeneste.alle(ANSATT_ID)
+
+                    første shouldBe setOf(BRUKER1, BRUKER2)
+                    andre shouldBe første
+                    cache.getOne<Set<BrukerId>>(VERGE_CACHE, ANSATT_ID.verdi) shouldBe første
+                }
+            }
+
+            When("to ulike ansatte slås opp") {
+                Then("REST kalles to ganger selv om NOM-ident er lik") {
+                    server.expect(times(2), requestTo(uri(cfg.baseUri)))
+                        .andExpect(method(POST))
+                        .andRespond(withSuccess(vergemålRespons(), APPLICATION_JSON))
+
+                    tjeneste.alle(ANSATT_ID) shouldBe setOf(BRUKER1, BRUKER2)
+                    tjeneste.alle(ANSATT_ID_2) shouldBe setOf(BRUKER1, BRUKER2)
                 }
             }
         }
@@ -145,10 +196,28 @@ class VergemålTjenesteTest : RestTjenesteTest() {
 
     companion object {
         private val ANSATT_ID = AnsattId("Z999999")
+        private val ANSATT_ID_2 = AnsattId("Z888888")
         private val IDENT = BrukerId("08526835670")
         private val BRUKER1 = BrukerId("20478606614")
         private val BRUKER2 = BrukerId("03508331575")
 
         private fun uri(base: URI) = fromUriString("$base$VERGEMÅL_PATH").build().toUri()
+
+        private fun vergemålRespons() = """
+            [
+              {
+                "vergehaver": "${BRUKER1.verdi}",
+                "verge": "${IDENT.verdi}",
+                "leserettigheter": ["DAG"],
+                "skriverettigheter": []
+              },
+              {
+                "vergehaver": "${BRUKER2.verdi}",
+                "verge": "${IDENT.verdi}",
+                "leserettigheter": ["PEN"],
+                "skriverettigheter": ["PEN"]
+              }
+            ]
+        """.trimIndent()
     }
 }

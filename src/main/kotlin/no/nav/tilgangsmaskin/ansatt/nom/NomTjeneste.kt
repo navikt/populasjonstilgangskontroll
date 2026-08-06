d5572358dad3fadb5eba1d3e68ebecfa0e3e084a
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-06 21:36:31 +0200

    tect chore

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomTjeneste.kt
@@ -3,13 +3,11 @@ package no.nav.tilgangsmaskin.ansatt.nom
 import io.micrometer.observation.annotation.Observed
 import no.nav.tilgangsmaskin.ansatt.AnsattId
 import no.nav.tilgangsmaskin.ansatt.nom.NomConfig.Companion.NOM
-import no.nav.tilgangsmaskin.felles.rest.RetryingWhenRecoverableRestService
 import org.springframework.cache.annotation.CacheEvict
 import org.springframework.cache.annotation.Cacheable
 import org.springframework.transaction.annotation.Transactional
 
 @Observed
-@RetryingWhenRecoverableRestService
 @Transactional
 class NomTjeneste(private val adapter: NomJPAAdapter) {
 
@@ -26,4 +24,3 @@ class NomTjeneste(private val adapter: NomJPAAdapter) {
     fun lagre(nomAnsattData: NomAnsattData) =
         adapter.upsert(nomAnsattData)
 }
-

04566ee976a218a8ec9f96a2c93438a3f8b07d2d
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-07 12:55:43 +0200

    send bare en gang

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/SlackApplicationReadyNotifier.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/SlackApplicationReadyNotifier.kt
@@ -1,19 +1,35 @@
 package no.nav.tilgangsmaskin.felles.rest
 
 import no.nav.tilgangsmaskin.felles.utils.MessagePublisher
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.event.EventListener
+import org.springframework.data.redis.core.StringRedisTemplate
 import org.springframework.stereotype.Component
+import java.time.Duration.ofDays
 
 @Component
 class SlackApplicationReadyNotifier(
     private val publisher: MessagePublisher,
+    private val valkey: StringRedisTemplate,
     @param:Value("\${info.name:\${spring.application.name:populasjonstilgangskontroll}}") private val appName: String,
     @param:Value("\${info.cluster:local}") private val cluster: String,
     @param:Value("\${info.image:unknown}") private val image: String,
 ) {
+    private val log = getLogger(javaClass)
+
     @EventListener(ApplicationReadyEvent::class)
-    fun onApplicationReady() =
-        publisher.info("Applikasjon klar", "$appName er startet i cluster '$cluster' med image '$image'")
+    fun onApplicationReady() {
+        val key = "startup-slack::$cluster::$appName::$image"
+        val firstPublisher = runCatching {
+            valkey.opsForValue().setIfAbsent(key, "sent", ofDays(30)) == true
+        }.onFailure {
+            log.warn("Kunne ikke reservere startup-slack nøkkel i Valkey", it)
+        }.getOrDefault(false)
+
+        if (firstPublisher) {
+            publisher.info("Applikasjon klar", "$appName er startet i cluster '$cluster' med image '$image'")
+        }
+    }
 }

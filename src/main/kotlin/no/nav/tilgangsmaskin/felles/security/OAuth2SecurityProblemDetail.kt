16535d0ceea34be4a2695e1042e33a593d997903
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-10 13:46:51 +0200

    chore

---
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2SecurityProblemDetail.kt
@@ -0,0 +1,13 @@
+package no.nav.tilgangsmaskin.felles.security
+
+import io.opentelemetry.api.trace.Span
+import no.nav.tilgangsmaskin.regler.motor.RegelMetadata.Companion.TYPE_URI
+import org.springframework.http.HttpStatus
+import org.springframework.http.ProblemDetail.forStatusAndDetail
+
+internal fun securityProblemDetail(status: HttpStatus, detail: String) =
+    forStatusAndDetail(status, detail).apply {
+        type = TYPE_URI
+        title = "${status.value()}"
+        properties = mapOf("traceId" to Span.current().spanContext.traceId)
+    }

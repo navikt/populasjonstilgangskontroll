e544c0b73c0a3c910f22d84308d45b4073748d64
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-10 09:37:00 +0200

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2SecurityBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2SecurityBeanConfig.kt
@@ -32,17 +32,19 @@ class OAuth2SecurityBeanConfig {
     fun securityFilterChain(
         http: HttpSecurity,
         authorizationManager: EnkeltTilgangAuthorizationManager,
-        accessDeniedHandler: AccessDeniedHandler,
-        authenticationEntryPoint: AuthenticationEntryPoint
+        deniedHandler: AccessDeniedHandler,
+        entryPoint: AuthenticationEntryPoint
     ) = http.authorizeHttpRequests { requests ->
             requests.requestMatchers(POST, "$PROD_BASE_PATH/overstyr").access(authorizationManager)
             requests.requestMatchers( *UNPROTECTED_ENDPOINTS).permitAll()
             requests.anyRequest().authenticated()
         }
-        .exceptionHandling { it.accessDeniedHandler(accessDeniedHandler) }
+        .exceptionHandling {
+            it.accessDeniedHandler(deniedHandler)
+        }
         .oauth2ResourceServer { oauth2 ->
             oauth2.jwt { it.jwtAuthenticationConverter(OAuth2LoggingJwtAuthenticationConverter()) }
-            oauth2.authenticationEntryPoint(authenticationEntryPoint)
+            oauth2.authenticationEntryPoint(entryPoint)
         }
         .statelessApiDefaults()
         .build()

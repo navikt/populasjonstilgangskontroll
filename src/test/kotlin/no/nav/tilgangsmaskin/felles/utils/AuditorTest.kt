14ec7c04b2c183eee263fe7052e6a8f975ca9437
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-12 12:07:50 +0200

    les jwt

--- src/test/kotlin/no/nav/tilgangsmaskin/felles/utils/AuditorTest.kt
+++ src/test/kotlin/no/nav/tilgangsmaskin/felles/utils/AuditorTest.kt
@@ -1,6 +1,7 @@
 package no.nav.tilgangsmaskin.felles.utils
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.mockk.clearMocks
 import io.mockk.mockk
 import io.mockk.verify
 import no.nav.tilgangsmaskin.felles.LocalAuditor
@@ -8,42 +9,47 @@ import no.nav.tilgangsmaskin.felles.SecureAuditor
 import org.slf4j.Logger
 
 class AuditorTest : BehaviorSpec({
+    val logger = mockk<Logger>(relaxed = true)
 
     Given("LocalAuditor") {
+        val auditor = LocalAuditor(logger)
+        beforeEach {
+            clearMocks(logger)
+        }
 
         When("info kalles med melding") {
             Then("logger melding via klassens logger") {
-                val logger = mockk<Logger>(relaxed = true)
-                LocalAuditor(logger).info("test melding")
+                auditor.info("test melding")
                 verify { logger.info("test melding") }
             }
         }
 
         When("info kalles med melding og throwable") {
             Then("logger melding med throwable") {
-                val logger = mockk<Logger>(relaxed = true)
                 val throwable = RuntimeException("feil")
-                LocalAuditor(logger).info("test melding", throwable)
+                auditor.info("test melding", throwable)
                 verify { logger.info("test melding", throwable) }
             }
         }
     }
 
     Given("SecureAuditor") {
+        val auditor = SecureAuditor(logger)
+        beforeEach {
+            clearMocks(logger)
+        }
 
         When("info kalles med sensitiv melding") {
             Then("logger melding via secureLog-loggeren") {
-                val logger = mockk<Logger>(relaxed = true)
-                SecureAuditor(logger).info("sensitiv melding")
+                auditor.info("sensitiv melding")
                 verify { logger.info("sensitiv melding") }
             }
         }
 
         When("info kalles med sensitiv melding og throwable") {
             Then("logger melding med throwable") {
-                val logger = mockk<Logger>(relaxed = true)
                 val throwable = RuntimeException("feil")
-                SecureAuditor(logger).info("sensitiv melding", throwable)
+                auditor.info("sensitiv melding", throwable)
                 verify { logger.info("sensitiv melding", throwable) }
             }
         }

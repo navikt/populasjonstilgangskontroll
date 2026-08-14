3309e839c23edd2f4e6d498fbdd8b068a683dfe7
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-14 14:34:27 +0200

    OI

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2SecurityBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2SecurityBeanConfig.kt
@@ -1,23 +1,17 @@
 package no.nav.tilgangsmaskin.felles.security
 
-import no.nav.boot.conditionals.ConditionalOnNotProd
 import no.nav.tilgangsmaskin.felles.rest.PROD_BASE_PATH
 import no.nav.tilgangsmaskin.felles.utils.cluster.ClusterConstants.DEV
 import org.springframework.beans.factory.ObjectProvider
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Profile
 import org.springframework.core.convert.converter.Converter
-import org.springframework.http.HttpMethod.POST
 import org.springframework.security.authentication.AbstractAuthenticationToken
-import org.springframework.security.authorization.AuthorityAuthorizationManager.hasRole
-import org.springframework.security.authorization.AuthorizationManagers.anyOf
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.http.SessionCreationPolicy.STATELESS
 import org.springframework.security.config.observation.SecurityObservationSettings
-import org.springframework.security.core.GrantedAuthority
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager
 import org.springframework.security.oauth2.client.OAuth2AuthorizationFailureHandler
@@ -30,7 +24,6 @@ import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpReq
 import org.springframework.security.oauth2.client.web.client.support.OAuth2RestClientHttpServiceGroupConfigurer.from
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter
 import org.springframework.security.web.AuthenticationEntryPoint
 import org.springframework.security.web.access.AccessDeniedHandler
 import org.springframework.stereotype.Component
@@ -52,7 +45,7 @@ class OAuth2SecurityBeanConfig {
                             deniedHandler: AccessDeniedHandler,
                             entryPoint: AuthenticationEntryPoint) =
         http.authorizeHttpRequests { requests ->
-            requests.requestMatchers(POST, "$PROD_BASE_PATH/overstyr").hasAnyRole(ENKELT, DEV)
+            requests.requestMatchers( "$PROD_BASE_PATH/overstyr").hasAnyRole(ENKELT, DEV)
             requests.requestMatchers( *UNPROTECTED_ENDPOINTS).permitAll()
             requests.anyRequest().authenticated()
         }

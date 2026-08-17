b0e1518fa4bb3e212438f95cb9e84745a5cd9877
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-17 11:10:23 +0200

    cache

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheController.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheController.kt
@@ -89,7 +89,7 @@ class CacheController(
             log.info("Tømte hele databasen og slettet {} nøkler", it)
         }
 
-    @GetMapping("vg", produces = [TEXT_HTML_VALUE])
+    @GetMapping("reset", produces = [TEXT_HTML_VALUE])
     @Operation(summary = SUMMARY_CACHE_VG, description = DESCRIPTION_CACHE_VG)
     fun vgKnapp(): ResponseEntity<String> =
         ResponseEntity.ok()

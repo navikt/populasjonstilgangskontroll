a23444fa1fcacb831b0e0967dee5ff5c17910bbb
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-21 23:26:48 +0200

    chore

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ResilientValkeySerializer.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/ResilientValkeySerializer.kt
@@ -3,17 +3,17 @@ package no.nav.tilgangsmaskin.felles.cache
 import org.slf4j.LoggerFactory.getLogger
 import org.springframework.data.redis.serializer.RedisSerializer
 
-class ResilientValkeySerializer(private val delegate: RedisSerializer<Any>
-) : RedisSerializer<Any> {
+class ResilientValkeySerializer(private val delegate: RedisSerializer<Any>) : RedisSerializer<Any> {
 
     private val log = getLogger(javaClass)
 
-    override fun serialize(t: Any?): ByteArray = delegate.serialize(t)
+    override fun serialize(t: Any?): ByteArray =
+        delegate.serialize(t)
 
-    override fun deserialize(bytes: ByteArray?): Any? =
-        runCatching { delegate.deserialize(bytes) }
-            .onFailure {
-                log.warn("Kunne ikke deserialisere cache-entry, behandler som miss", it)
-            }
-            .getOrNull()
+    override fun deserialize(bytes: ByteArray?) =
+        runCatching {
+            delegate.deserialize(bytes)
+        }.onFailure {
+            log.warn("Kunne ikke deserialisere cache-entry, behandler som miss", it)
+        }.getOrNull()
 }

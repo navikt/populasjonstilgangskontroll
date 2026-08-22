39e1182d740d17ad5edf18a9d0370a1b4f3264f3
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-22 10:37:21 +0200

    publiser uansett

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheBeanConfig.kt
@@ -70,8 +70,9 @@ class CacheBeanConfig(private val cf: RedisConnectionFactory,
         setConnectionFactory(cf)
         setRecoveryInterval(5_000) // retry subscription every 5s
         maxSubscriptionRegistrationWaitingTime = 30_000
+
         setErrorHandler {
-            log.warn("Redis listener failure, will retry in 5s", it)
+            log.warn("Redis listener feilet, retry om 5s", it)
         }
     }
 

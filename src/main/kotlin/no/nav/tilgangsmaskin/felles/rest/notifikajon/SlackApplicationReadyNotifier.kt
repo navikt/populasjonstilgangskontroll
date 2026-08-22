7528aa588dc83e792fa1d6500519cb3b973b487b
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-22 10:41:05 +0200

    publiser uansett

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/notifikajon/SlackApplicationReadyNotifier.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/notifikajon/SlackApplicationReadyNotifier.kt
@@ -19,7 +19,7 @@ class SlackApplicationReadyNotifier(
     @EventListener(ApplicationReadyEvent::class)
     fun onApplicationReady() =
         runCatching {
-            publisher.info("$pod er klar", "Startet  _${pod}_ for image _${image}_")
+            publisher.info("$pod startet", "Startet  _${pod}_ for image _${image}_")
             log.info("Startet $pod for image $image")
         }.onFailure {
             log.warn("Feilet ved sending av startup-notifikasjon", it)

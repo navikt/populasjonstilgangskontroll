42ca37cd0ca6cc169a3899c0633aa0a5af54cd8d
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-22 12:14:04 +0200

    publiser uansett

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/notifikajon/EnvironmentAwarePublishingNotifier.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/notifikajon/EnvironmentAwarePublishingNotifier.kt
@@ -1,11 +1,11 @@
 package no.nav.tilgangsmaskin.felles.rest.notifikajon
 
-import org.slf4j.LoggerFactory
+import org.slf4j.LoggerFactory.getLogger
 import org.springframework.core.env.Environment
 
 abstract class EnvironmentAwarePublishingNotifier(
     protected val publisher: MessagePublisher, env: Environment) {
     protected val pod = env.getProperty("hostname") ?: "localhost"
     protected val image = env.getRequiredProperty("nais.app.image")
-    protected val log = LoggerFactory.getLogger(javaClass)
+    protected val log = getLogger(javaClass)
 }

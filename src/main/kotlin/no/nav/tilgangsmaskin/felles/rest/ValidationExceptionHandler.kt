e2ca177507cad4328348b2b4d29594bf3ecd726a
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-08-28 16:28:16 +0200

    log info

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ValidationExceptionHandler.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/rest/ValidationExceptionHandler.kt
@@ -6,6 +6,7 @@ import org.springframework.http.MediaType.APPLICATION_PROBLEM_JSON
 import org.springframework.http.ResponseEntity
 import org.springframework.validation.FieldError
 import org.springframework.validation.ObjectError
+import org.springframework.web.ErrorResponseException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.method.annotation.HandlerMethodValidationException
@@ -36,6 +37,11 @@ class ValidationExceptionHandler {
         }
     )
 
+    @ExceptionHandler(ErrorResponseException::class)
+    fun handleErrorResponse(ex: ErrorResponseException): ResponseEntity<Any> =
+        ResponseEntity.status(ex.statusCode)
+            .contentType(APPLICATION_PROBLEM_JSON)
+            .body(ex.body)
 
     private fun badRequest(feil: List<Map<String, String>>): ResponseEntity<Map<String, Any>> =
         ResponseEntity.status(BAD_REQUEST)

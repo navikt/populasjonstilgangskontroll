71d8cee2cd1b061e20a9f96916574bba3b1d7ffc
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-11 09:38:46 +0200

    fjernet duplikat test

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2SecurityBeanConfig.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/security/OAuth2SecurityBeanConfig.kt
@@ -1,8 +1,6 @@
 package no.nav.tilgangsmaskin.felles.security
 
-import no.nav.tilgangsmaskin.felles.rest.PROD_BASE_PATH
 import no.nav.tilgangsmaskin.felles.utils.cluster.ClusterConstants.DEV
-import no.nav.tilgangsmaskin.regler.enkelttilgang.ENKELTTILGANG_PATH
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
@@ -35,7 +33,6 @@ class OAuth2SecurityBeanConfig {
                             deniedHandler: AccessDeniedHandler,
                             entryPoint: AuthenticationEntryPoint) =
         http.authorizeHttpRequests { requests ->
-            requests.requestMatchers(ENKELTTILGANG_PATH).hasRole(ENKELT)
             requests.requestMatchers( *UNPROTECTED_ENDPOINTS).permitAll()
             requests.anyRequest().authenticated()
         }

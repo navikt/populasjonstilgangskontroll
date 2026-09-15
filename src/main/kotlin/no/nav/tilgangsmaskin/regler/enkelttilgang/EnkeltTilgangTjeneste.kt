e775bbaf6e109535ae530cf005a2038d7f5a0257
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-15 09:02:37 +0200

    trenger ikke delay når vi har startup

--- src/main/kotlin/no/nav/tilgangsmaskin/regler/enkelttilgang/EnkeltTilgangTjeneste.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/regler/enkelttilgang/EnkeltTilgangTjeneste.kt
@@ -51,7 +51,7 @@ class EnkeltTilgangTjeneste(
     @Transactional
     fun registrerTilgang(ansattId: AnsattId, data: EnkeltTilgangData): Boolean =
         runCatching {
-            MDC.put(USER_ID, ansattId.verdi)
+            MDC.put("x" +USER_ID, ansattId.verdi)
             val enhetsnummer = enhetsNummerFor(ansattId)
             motor.kjerneregler(ansattTjeneste.ansatt(ansattId),
                 bruker.medNærmesteFamilie(data.brukerId.verdi))

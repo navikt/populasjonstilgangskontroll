413b4d836e011999f4aa474ca898cc9f92bd65f8
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-16 13:25:01 +0200

    knapp

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheFlushViewController.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheFlushViewController.kt
@@ -6,15 +6,13 @@ import no.nav.tilgangsmaskin.felles.utils.cluster.ClusterConstants.DEV
 import no.nav.tilgangsmaskin.tilgang.openapi.MSG
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-
-private const val SUMMARY_CACHE_VG = "${MSG}openapi.dev.cache.vg.summary"
-private const val DESCRIPTION_CACHE_VG = "${MSG}openapi.dev.cache.vg.description"
+import org.springframework.web.bind.annotation.RequestMapping
 
 
-@Controller("/${DEV}/cache")
+@Controller("Flush the cache")
+@RequestMapping("/dev/cache")
 class CacheFlushViewController {
 
     @GetMapping("flush")
-    @Operation(summary = SUMMARY_CACHE_VG, description = DESCRIPTION_CACHE_VG)
     fun flushAnsatt(): String = "dev/cache/flush"
 }

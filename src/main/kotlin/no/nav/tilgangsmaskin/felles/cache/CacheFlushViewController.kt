071024c6b7038f48a2421172e55093c6e68932ce
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-16 13:37:53 +0200

    knapp

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheFlushViewController.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheFlushViewController.kt
@@ -1,15 +1,10 @@
 package no.nav.tilgangsmaskin.felles.cache
 
-import io.swagger.v3.oas.annotations.Operation
-import no.nav.tilgangsmaskin.felles.rest.DevController
-import no.nav.tilgangsmaskin.felles.utils.cluster.ClusterConstants.DEV
-import no.nav.tilgangsmaskin.tilgang.openapi.MSG
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
-
-@Controller("Flush the cache")
+@Controller
 @RequestMapping("/dev/cache")
 class CacheFlushViewController {
 

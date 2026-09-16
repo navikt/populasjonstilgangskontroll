15b3d2b693b2d9f6c7c70c7df533534b544a45cb
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-16 15:32:32 +0200

    flush

--- src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheFlushController.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/felles/cache/CacheFlushController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 @RequestMapping("/cache")
-class CacheFlushViewController {
+class CacheFlushController {
 
     @GetMapping("flush")
     fun flushAnsatt(): String = "cache/flush"

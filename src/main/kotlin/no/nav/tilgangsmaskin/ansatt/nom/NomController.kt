dd98cd7200e702e7951ba7559fc64edee7a10a32
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-24 10:17:31 +0200

    la til nom controller i dev

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomController.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomController.kt
@@ -1,19 +1,15 @@
 package no.nav.tilgangsmaskin.ansatt.nom
 
-import io.swagger.v3.oas.annotations.Operation
 import no.nav.tilgangsmaskin.ansatt.AnsattId
-import no.nav.tilgangsmaskin.ansatt.AnsattTjeneste
-import no.nav.tilgangsmaskin.ansatt.entraproxy.EntraProxyTjeneste
 import no.nav.tilgangsmaskin.felles.rest.DevController
 import no.nav.tilgangsmaskin.felles.utils.cluster.ClusterConstants.DEV
-import no.nav.tilgangsmaskin.tilgang.openapi.MSG
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 
 
 
 @DevController(
-    value = ["/${DEV}/nom/"],
+    value = ["/${DEV}/nom/leder"],
     name = "NomController")
 class NomController(
     private val nom: NomTjeneste) {

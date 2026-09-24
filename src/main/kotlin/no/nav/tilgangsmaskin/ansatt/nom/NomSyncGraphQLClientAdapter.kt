bd713c6905f338a20e8fee39fbfdf26034f5dcf4
Jan Olav Eide <jan.olav.eide@gmail.com>
2026-09-24 14:13:09 +0200

    var

--- src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomSyncGraphQLClientAdapter.kt
+++ src/main/kotlin/no/nav/tilgangsmaskin/ansatt/nom/NomSyncGraphQLClientAdapter.kt
@@ -19,7 +19,7 @@ class NomSyncGraphQLClientAdapter(
 
     fun leder(ansattId: String): Any =
         runCatching {
-            query<Leder>(LEDER_QUERY, ident(ansattId))
+            query<Ledere>(LEDER_QUERY, ident(ansattId))
         }.getOrThrow()
 
     private inline fun <reified T : Any> query(query: Pair<String, String>, vars: Map<String, String>) =
